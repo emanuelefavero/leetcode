2fe2dbc63ca894ddfb8e2199b699d066d4297ed7
emanuelefavero <emafaux89@gmail.com>
2023-09-23 20:29:36 +0200

    add array/running-sum-of-1d-array alternative solution

--- array/running-sum-of-1d-array.js
+++ array/running-sum-of-1d-array.js
@@ -24,3 +24,19 @@ function runningSum(nums) {
 console.log(runningSum([1, 2, 3, 4])) // [1, 3, 6, 10]
 console.log(runningSum([1, 1, 1, 1, 1])) // [1, 2, 3, 4, 5]
 console.log(runningSum([3, 1, 2, 10, 1])) // [3, 4, 6, 16, 17]
+
+// --------------------------------------------------------------------------
+// ALTERNATE SOLUTIONS
+
+// O(n) time, O(n) space
+function runningSum2(nums) {
+  let result = []
+  let sum = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+    result.push(sum)
+  }
+
+  return result
+}
