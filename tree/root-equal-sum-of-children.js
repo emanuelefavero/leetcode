2b612dffc05599309d5cd84501f3a647bbcf1bea
emanuelefavero <emafaux89@gmail.com>
2023-09-23 19:14:05 +0200

    add tree/root-equal-sum-of-children

---
+++ tree/root-equal-sum-of-children.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef TreeNode
+ * @property {number} val
+ * @property {TreeNode} left
+ * @property {TreeNode} right
+ */
+
+// * INPUT
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+
+let root = new TreeNode(10, new TreeNode(4), new TreeNode(6))
+let root2 = new TreeNode(10, new TreeNode(3), new TreeNode(2))
+
+/**
+ * You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+ * Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+ *
+ * @param {TreeNode} root
+ * @returns {boolean}
+ */
+
+// * SOLUTION O(1)
+function checkTree(root) {
+  return root.val === root.left.val + root.right.val
+}
+
+console.log(checkTree(root)) // true
+console.log(checkTree(root2)) // false
