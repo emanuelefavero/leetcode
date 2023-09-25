1ad869283e5947cde2a034bb5a3b962aa2493857
emanuelefavero <emafaux89@gmail.com>
2023-09-25 06:34:14 +0200

    add jsDoc and comments to easy/fizz-buzz

--- easy/fizz-buzz.js
+++ easy/fizz-buzz.js
@@ -1,3 +1,13 @@
+/**
+ * Given an integer n, return a string array answer (1-indexed) where:
+ * - answer[i] == 'FizzBuzz' if i is divisible by 3 and 5.
+ * - answer[i] == 'Fizz' if i is divisible by 3.
+ * - answer[i] == 'Buzz' if i is divisible by 5.
+ * - answer[i] == i if non of the above conditions are true.
+ *
+ * @param {number} n
+ * @returns {string[]}
+ */
 function fizzBuzz(n) {
   let answer = []
 
@@ -11,4 +21,4 @@ function fizzBuzz(n) {
   return answer
 }
 
-console.log(fizzBuzz(15))
+console.log(fizzBuzz(15)) // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
