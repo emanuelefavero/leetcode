fefd2844f28f20d1cefc2db7d88a8f90936822c7
emanuelefavero <emafaux89@gmail.com>
2023-09-26 21:42:58 +0200

    refactor hashing/ransom-note

--- hashing/ransom-note.js
+++ hashing/ransom-note.js
@@ -1,16 +1,16 @@
+// TODO: Add jsDoc
+// TODO: Add comments
+// TODO: Add solution2
+
 function canConstruct(ransomNote, magazine) {
   let hashTable = {}
 
-  for (let i = 0; i < magazine.length; i++) {
-    let char = magazine[i]
-
+  for (let char of magazine) {
     if (!hashTable[char]) hashTable[char] = 1
     else hashTable[char]++
   }
 
-  for (let i = 0; i < ransomNote.length; i++) {
-    let char = ransomNote[i]
-
+  for (let char of ransomNote) {
     if (!hashTable[char] || hashTable[char] === 0) return false
     else hashTable[char]--
   }
@@ -21,3 +21,26 @@ function canConstruct(ransomNote, magazine) {
 console.log(canConstruct('a', 'b')) // false
 console.log(canConstruct('aa', 'ab')) // false
 console.log(canConstruct('aa', 'aab')) // true
+
+// -------------------------------
+// SOLUTION 2
+
+/*
+function canConstruct(ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) return false
+
+  let hashTable = new Map()
+
+  for (let char of magazine) {
+    if (!hashTable.has(char)) hashTable.set(char, 1)
+    else hashTable.set(char, hashTable.get(char) + 1)
+  }
+
+  for (let char of ransomNote) {
+    if (!hashTable.has(char) || hashTable.get(char) === 0) return false
+    else hashTable.set(char, hashTable.get(char) - 1)
+  }
+
+  return true
+}
+*/
