26e88eb3e7330bf92f3d713376f37ae697221d77
emanuelefavero <emafaux89@gmail.com>
2023-09-27 12:19:16 +0200

    add jsDoc and comments to array/even-number-of-digits

--- array/even-number-of-digits.js
+++ array/even-number-of-digits.js
@@ -1,11 +1,19 @@
+/**
+ * * Given an array nums of integers, return how many of them contain an even number of digits
+ * @param {number[]} nums
+ * @returns {number}
+ */
+
+// O(n) time | O(1) space
 function findNumbers(nums) {
   let count = 0
 
   for (let num of nums) {
+    // if the length of the number is even, increment count
     if (String(num).length % 2 === 0) count++
   }
 
   return count
 }
 
-console.log(findNumbers([22, 1, 234, 33]))
+console.log(findNumbers([22, 1, 234, 33])) // 2
