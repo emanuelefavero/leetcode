73f61b47018cb742848f43b78ec0818c00bd6dc0
emanuelefavero <emafaux89@gmail.com>
2023-09-27 11:57:40 +0200

    add solution 2 to array/max-consecutives-ones

--- array/max-consecutive-ones.js
+++ array/max-consecutive-ones.js
@@ -1,4 +1,24 @@
 function findMaxConsecutiveOnes(nums) {
+  let currentSum = 0
+  let maxSum = 0
+
+  for (let n of nums) {
+    if (n === 1) {
+      currentSum++
+    } else {
+      maxSum = Math.max(maxSum, currentSum)
+      currentSum = 0
+    }
+  }
+
+  maxSum = Math.max(maxSum, currentSum)
+
+  return maxSum
+}
+
+// -------------------------------
+// SOLUTION 2
+function findMaxConsecutiveOnes2(nums) {
   let result = []
   let consecutiveOnes = 0
 
@@ -16,5 +36,9 @@ function findMaxConsecutiveOnes(nums) {
   return Math.max(...result)
 }
 
-console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
-console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 0]))
+// -------------------------------
+
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])) // 3
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 0])) // 2
+console.log(findMaxConsecutiveOnes2([1, 1, 0, 1, 1, 1])) // 3
+console.log(findMaxConsecutiveOnes2([1, 1, 0, 1, 1, 0])) // 2
