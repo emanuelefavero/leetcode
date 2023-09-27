ed4b7093c4596ef8a447586e55b134de6b250e9d
emanuelefavero <emafaux89@gmail.com>
2023-09-27 13:01:29 +0200

    add solution 2 to array/squares-of-a-sorted-array

--- array/squares-of-a-sorted-array.js
+++ array/squares-of-a-sorted-array.js
@@ -1,9 +1,26 @@
+// not in-place
 function sortedSquares(nums) {
+  let squares = []
+
+  for (let i = 0; i < nums.length; i++) {
+    squares.push(nums[i] * nums[i])
+  }
+
+  return squares.sort((a, b) => a - b)
+}
+
+// -------------------------------
+// SOLUTION 2 - in-place
+
+function sortedSquares2(nums) {
   for (let i = 0; i < nums.length; i++) {
     nums[i] = nums[i] * nums[i]
   }
 
   return nums.sort((a, b) => a - b)
 }
+
+// -------------------------------
+// TESTS
 
 console.log(sortedSquares([-4, -1, 0, 3, 10]))
