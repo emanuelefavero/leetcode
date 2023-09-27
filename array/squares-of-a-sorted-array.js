652bf112052389df161f7252e1889cbad6c0e068
emanuelefavero <emafaux89@gmail.com>
2023-09-27 12:59:05 +0200

    add array/squares-of-a-sorted-array

---
+++ array/squares-of-a-sorted-array.js
@@ -0,0 +1,9 @@
+function sortedSquares(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] * nums[i]
+  }
+
+  return nums.sort((a, b) => a - b)
+}
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]))
