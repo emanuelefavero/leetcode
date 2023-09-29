2ff2dea5a03cad7a508752dadf1ea3f216ca8f71
emanuelefavero <emafaux89@gmail.com>
2023-09-29 19:45:21 +0200

    add jsDoc to array/duplicate-zeros

--- array/duplicate-zeros.js
+++ array/duplicate-zeros.js
@@ -1,9 +1,11 @@
-// TODO: jsDoc and comments
 // TODO: Add link to README
 
-// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
-
-// Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+/**
+ * * Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right
+ *
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
 
 function duplicateZeros(arr) {
   for (let i = 0; i < arr.length; i++) {
