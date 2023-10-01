7c5494a2d78a8b0b588ab8e89f71bc2ed9835156
emanuelefavero <emafaux89@gmail.com>
2023-10-01 11:50:04 +0200

    add array/check-if-n-and-its-double-exist

---
+++ array/check-if-n-and-its-double-exist.js
@@ -0,0 +1,20 @@
+// Given an array of integers arr, return true if there are two distinct indices i and j in the array such that arr[i] == 2 * arr[j], or arr[j] == 2 * arr[i], or both
+
+function checkIfExist(arr) {
+  let set = new Set()
+
+  for (let i = 0; i < arr.length; i++) {
+    if (set.has(arr[i] * 2) || set.has(arr[i] / 2)) return true
+
+    set.add(arr[i])
+  }
+
+  return false
+}
+
+// ---------------------
+// TESTS
+
+console.log(checkIfExist([10, 2, 5, 3])) // true
+console.log(checkIfExist([2, 5, 3, 10])) // true
+console.log(checkIfExist([3, 1, 7, 11])) // false
