db9c8e67b581bce3ef6056a20084e34661a40d82
emanuelefavero <emafaux89@gmail.com>
2023-10-02 16:00:28 +0200

    add array/replace-elements-with-greatest

---
+++ array/replace-elements-with-greatest.js
@@ -0,0 +1,24 @@
+// Replace Elements With Greatest Element on Right Side
+
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+function replaceElements(arr) {
+  let max = -1
+
+  // loop backwards so we can keep track of the max number to the right
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let current = arr[i]
+
+    arr[i] = max
+    max = Math.max(max, current)
+  }
+
+  return arr
+}
+
+// ------------------
+// TESTS
+
+console.log(replaceElements([17, 18, 5, 4, 6, 1]))
