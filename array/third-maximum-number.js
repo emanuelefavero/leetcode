254c3894f3c55cfafd74645df51ae2631c8939fc
emanuelefavero <emafaux89@gmail.com>
2023-10-03 13:28:36 +0200

    add comments to array/third-maximum-number

--- array/third-maximum-number.js
+++ array/third-maximum-number.js
@@ -1,21 +1,23 @@
 // Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
 
-function thirdMax(nums) {
-  nums.sort((a, b) => a - b)
+// TIP: This solution is easy to understand but is not very efficient memory-wise
 
+// O(n log n) time | O(n) space
+function thirdMax(nums) {
+  nums.sort((a, b) => a - b) // sort nums
+
+  // remove duplicates
   let set = new Set()
   for (let i = 0; i < nums.length; i++) {
     set.add(nums[i])
   }
 
-  let result = [...set]
+  let result = [...set] // convert set to array
 
+  // return third max if it exists, otherwise return max
   if (result.length >= 3) return result[result.length - 3]
   else return result[result.length - 1]
 }
 
-let nums = [
-  3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8,
-  5, 6,
-]
-console.log(thirdMax(nums))
+let nums = [3, 1, 2, 2, 4, 6, 5]
+console.log(thirdMax(nums)) // 4
