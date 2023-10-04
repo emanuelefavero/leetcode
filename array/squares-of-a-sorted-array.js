bccb43ef9699c39a39cc6ef5d68fdb2abd58df49
emanuelefavero <emafaux89@gmail.com>
2023-10-04 08:28:50 +0200

    add solution 4 to array/squares-of-a-sorted-array.js

--- array/squares-of-a-sorted-array.js
+++ array/squares-of-a-sorted-array.js
@@ -1,11 +1,12 @@
 /**
  * * Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order
+ *
  * @param {number[]} nums
  * @return {number[]}
  */
 
 // O(n log n) time | O(n) space
-function sortedSquares3(nums) {
+function sortedSquares(nums) {
   let result = nums.map((n) => n * n) // square each number
 
   return result.sort((a, b) => a - b) // sort the array
@@ -27,7 +28,7 @@ function sortedSquares2(nums) {
 // SOLUTION 3 - not in-place
 
 // O(n log n) time | O(n) space
-function sortedSquares(nums) {
+function sortedSquares3(nums) {
   let squares = []
 
   for (let i = 0; i < nums.length; i++) {
@@ -38,8 +39,39 @@ function sortedSquares(nums) {
 }
 
 // -------------------------------
+// SOLUTION 4 - O(n) solution (two-pointer technique)
+
+// TIP: This solution is faster than the previous three solutions but is harder to read
+
+// O(n) time | O(n) space
+function sortedSquares4(nums) {
+  let squares = []
+  let left = 0
+  let right = nums.length - 1
+
+  // iterate through the array from right to left
+  for (let i = nums.length - 1; i >= 0; i--) {
+    // compare the squares of the left and right pointers
+    let leftSquare = nums[left] * nums[left]
+    let rightSquare = nums[right] * nums[right]
+
+    // add the larger square to the beginning of the array
+    if (leftSquare > rightSquare) {
+      squares[i] = leftSquare
+      left++
+    } else {
+      squares[i] = rightSquare
+      right--
+    }
+  }
+
+  return squares
+}
+
+// -------------------------------
 // TESTS
 
 console.log(sortedSquares([-4, -1, 0, 3, 10])) // [0,1,9,16,100]
 console.log(sortedSquares2([-4, -1, 0, 3, 10])) // [0,1,9,16,100]
 console.log(sortedSquares3([-4, -1, 0, 3, 10])) // [0,1,9,16,100]
+console.log(sortedSquares4([-4, -1, 0, 3, 10])) // [0,1,9,16,100]
