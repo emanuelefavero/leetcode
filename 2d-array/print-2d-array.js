201f5435a93ae5c64fb90f7f72baa4aaf4b6ee6d
emanuelefavero <emafaux89@gmail.com>
2023-10-05 21:24:53 +0200

    add jsDoc and comments to 2d-array/print-2d-array

--- 2d-array/print-2d-array.js
+++ 2d-array/print-2d-array.js
@@ -1,3 +1,14 @@
+// TODO: Commit - add jsDoc and comments to 2d-array/print-2d-array
+// TODO: Add 2d-array/print-2d-array link to README
+
+/**
+ * * Print 2d array
+ *
+ * @param {number[]} array
+ */
+
+// O(n) time | O(1) space - even if we have nested loops, we are still only iterating through each element once
+
 function print2dArray(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array[i].length; j++) {
@@ -6,9 +17,12 @@ function print2dArray(array) {
   }
 }
 
+// -------------------------
+// TESTS
+
 let array2d = [
   [1, 2],
   [3, 4],
 ]
 
-console.log(print2dArray(array2d))
+console.log(print2dArray(array2d)) // 1, 2, 3, 4
