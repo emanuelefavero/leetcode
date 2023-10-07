0485ad13d165322fe4cc223cf31e2052a003db5b
emanuelefavero <emafaux89@gmail.com>
2023-10-07 21:43:01 +0200

    add jsDoc to easy/add-two-integers

--- easy/add-two-integers.js
+++ easy/add-two-integers.js
@@ -1,9 +1,18 @@
 /**
- * @param {number} num1
- * @param {number} num2
- * @return {number}
+ *  * Add Two Integers
+ *
+ * @param {number} num1 - first number
+ * @param {number} num2 - second number
+ * @return {number} The sum of the two numbers
  */
 
+// O(1) time | O(1) space
 function sum(num1, num2) {
   return num1 + num2
 }
+
+// -------------------------
+// TESTS
+
+console.log(sum(1, 2)) // 3
+console.log(sum(2, 3)) // 5
