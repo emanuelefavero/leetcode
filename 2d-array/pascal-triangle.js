5cfa7fd6d7d2e0003889db3e1cc57bcc03c1c0f1
emanuelefavero <emafaux89@gmail.com>
2023-10-09 18:55:35 +0200

    add jsDoc to 2d-array/pascal-triangle

--- 2d-array/pascal-triangle.js
+++ 2d-array/pascal-triangle.js
@@ -1,9 +1,12 @@
-// TODO: Add jsDoc
-
-// Given an integer numRows, return the first numRows of Pascal's triangle.
-
-// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
-// https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif
+/**
+ * * Given an integer numRows, return the first numRows of Pascal's triangle.
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+ * @see https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif
+ *
+ * @param {number} numRows
+ * @returns {number[][]}
+ */
 
 // O(n^2) time | O(n^2) space
 function generate(numRows) {
