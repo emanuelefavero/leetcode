59abb82a98895c113965218bbcb651063599c1e1
emanuelefavero <emafaux89@gmail.com>
2023-10-10 01:23:12 +0200

    add solution 2 to string/longest-common-prefix

--- string/longest-common-prefix.js
+++ string/longest-common-prefix.js
@@ -1,3 +1,6 @@
+// TODO: Add link to README
+// TODO: Add jsDoc and comments
+
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 // If there is no common prefix, return an empty string "".
@@ -22,5 +25,32 @@ function longestCommonPrefix(strs) {
   return prefix
 }
 
+// -------------------------
+// SOLUTION 2
+
+// TIP: This solution uses sorting and the fact that the longest common prefix is the prefix of the first and last string in the array
+
+function longestCommonPrefix2(strs) {
+  let prefix = ''
+  strs.sort() // sort the array
+
+  // loop through the first string
+  for (let i = 0; i < strs[0].length; i++) {
+    // if the character at the current index of the first string is equal to the character at the current index of the last string, add it to the prefix
+    if (strs[0][i] === strs[strs.length - 1][i]) prefix += strs[0][i]
+    else return prefix // otherwise, return the prefix
+  }
+
+  return prefix
+}
+
+// -------------------------
+// TESTS
+
+// 1
 console.log(longestCommonPrefix(['flower', 'flow', 'flight'])) // 'fl'
 console.log(longestCommonPrefix(['dog', 'racecar', 'car'])) // ''
+
+// 2
+console.log(longestCommonPrefix2(['flower', 'flow', 'flight'])) // 'fl'
+console.log(longestCommonPrefix2(['dog', 'racecar', 'car'])) // ''
