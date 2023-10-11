ce9b9fbdf488846171294a680d433b1940bbd504
emanuelefavero <emafaux89@gmail.com>
2023-10-11 20:11:31 +0200

    add jsDoc to array/partition-i

--- array/array-partition-i.js
+++ array/array-partition-i.js
@@ -1,4 +1,9 @@
-// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum
+/**
+ * * Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum
+ *
+ * @param {number[]} nums
+ * @returns {number}
+ */
 
 /*
 EXAMPLE:
@@ -11,9 +16,13 @@ Explanation: All possible pairings (ignoring the ordering of elements) are:
 So the maximum possible sum is 4.
 */
 
-function arrayPairSum(nums) {
-  nums.sort((a, b) => a - b)
+// TIP: You can sort the array and then add the first element of each pair
 
+// O(n * log(n)) time - O(1) space
+function arrayPairSum(nums) {
+  nums.sort((a, b) => a - b) // sort
+
+  // add the first element of each pair by looping through the array 2 at a time
   let sum = 0
   for (let i = 0; i < nums.length; i += 2) {
     sum += nums[i]
@@ -21,5 +30,8 @@ function arrayPairSum(nums) {
 
   return sum
 }
+
+// -------------------------
+// TESTS
 
 console.log(arrayPairSum([1, 4, 3, 2])) // 4
