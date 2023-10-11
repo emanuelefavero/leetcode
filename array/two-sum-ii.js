5122b9dd91323a92095a4124ce7beb0f681e9088
emanuelefavero <emafaux89@gmail.com>
2023-10-12 00:11:04 +0200

    add solution 2 to array/two-sum-ii

--- array/two-sum-ii.js
+++ array/two-sum-ii.js
@@ -1,3 +1,4 @@
+// O(n) time - O(1) space
 function twoSum(numbers, target) {
   let hashTable = {}
 
@@ -10,4 +11,28 @@ function twoSum(numbers, target) {
   }
 }
 
+// -------------------------
+// SOLUTION 2
+
+// O(n) time - O(1) space
+function twoSum2(numbers, target) {
+  let left = 0
+  let right = numbers.length - 1
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right]
+
+    // if the sum is equal to the target, return the indices
+    if (sum === target) return [left + 1, right + 1]
+    // if the sum is less than the target, move the left pointer to the right
+    else if (sum < target) left++
+    // if the sum is greater than the target, move the right pointer to the left
+    else right--
+  }
+}
+
+// -------------------------
+// TESTS
+
 console.log(twoSum([2, 7, 11, 15], 9)) // [1, 2]
+console.log(twoSum2([2, 7, 11, 15], 9)) // [1, 2]
