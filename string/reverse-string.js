fd4263a6baf22e9b1fdc778e74bd239c8c8f518b
emanuelefavero <emafaux89@gmail.com>
2023-10-11 19:52:51 +0200

    add jsDoc to string/reverse-string

--- string/reverse-string.js
+++ string/reverse-string.js
@@ -1,9 +1,13 @@
-// TODO: jsDoc and comments
+/**
+ * * Write a function that reverses a string. The input string is given as an array of characters s
+ *
+ * You must do this by modifying the input array in-place with O(1) extra memory
+ *
+ * @param {string[]} s
+ * @returns {void}
+ */
 
-// Write a function that reverses a string. The input string is given as an array of characters s
-
-// You must do this by modifying the input array in-place with O(1) extra memory
-
+// O(n) time - O(1) space
 function reverseString(s) {
   let left = 0
   let right = s.length - 1
@@ -17,6 +21,9 @@ function reverseString(s) {
 // -------------------------
 // SOLUTION 2
 
+// TIP: Using JavaScript built-in functions
+
+// O(n) time - O(1) space
 function reverseString2(s) {
   s.reverse()
 }
