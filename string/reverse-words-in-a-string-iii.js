8222431231c98fd1617a9f4213cd2c9a803781fd
emanuelefavero <emafaux89@gmail.com>
2023-10-12 08:42:25 +0200

    add string/reverse-words-in-a-string-iii solution 2

--- string/reverse-words-in-a-string-iii.js
+++ string/reverse-words-in-a-string-iii.js
@@ -27,8 +27,26 @@ function reverseWords(s) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution uses JavaScript built in methods
+
+function reverseWords2(s) {
+  return s
+    .split(' ') // split string into array of words
+    .filter((word) => word) // remove empty strings
+    .map((word) => word.split('').reverse().join('')) // reverse each word
+    .join(' ')
+    .trim()
+}
+
+// -------------------------
 // TESTS
 
 console.log(reverseWords('the sky is blue')) // 'eht yks si eulb'
 console.log(reverseWords('  hello world  ')) // '  olleh dlrow  '
 console.log(reverseWords('a good   example')) // 'a doog   elpmaxe'
+
+console.log(reverseWords2('the sky is blue')) // 'eht yks si eulb'
+console.log(reverseWords2('  hello world  ')) // '  olleh dlrow  '
+console.log(reverseWords2('a good   example')) // 'a doog   elpmaxe'
