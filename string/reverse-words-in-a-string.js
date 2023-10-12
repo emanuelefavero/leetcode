4353a644878cc7b0f23ddb33a0307ed5edb19b33
emanuelefavero <emafaux89@gmail.com>
2023-10-12 08:25:31 +0200

    add string/reverse-words-in-a-string solution 3

--- string/reverse-words-in-a-string.js
+++ string/reverse-words-in-a-string.js
@@ -33,12 +33,24 @@ function reverseWords2(s) {
 }
 
 // -------------------------
+// SOLUTION 3
+
+function reverseWords3(s) {
+  let trimmed = s.trim().split(' ') // trim and split string into array of words
+  let result = []
+
+  // loop through the array backwards
+  for (let i = trimmed.length - 1; i >= 0; i--) {
+    // add the word to the result if it is not empty
+    if (trimmed[i]) result.push(trimmed[i])
+  }
+
+  return result.join(' ')
+}
+
+// -------------------------
 // TESTS
 
 console.log(reverseWords('the sky is blue')) // 'blue is the sky'
-console.log(reverseWords('  hello world  ')) // 'world hello'
-console.log(reverseWords('a good   example')) // 'example good a'
-
-console.log(reverseWords2('the sky is blue')) // 'blue is the sky'
 console.log(reverseWords2('  hello world  ')) // 'world hello'
-console.log(reverseWords2('a good   example')) // 'example good a'
+console.log(reverseWords3('a good   example')) // 'example good a'
