3467cbe4e074b4f265e68d2d6fc40ebc0d2052f5
emanuelefavero <emafaux89@gmail.com>
2023-10-12 08:21:29 +0200

    add string/reverse-words-in-a-string solution 2

--- string/reverse-words-in-a-string.js
+++ string/reverse-words-in-a-string.js
@@ -18,6 +18,27 @@ function reverseWords(s) {
   return result.reverse().join(' ').trim()
 }
 
+// -------------------------
+// SOLUTION 2
+
+// TIP: This solution uses JavaScript built in methods
+
+function reverseWords2(s) {
+  return s
+    .split(' ') // split string into array of words
+    .filter((word) => word) // remove empty strings
+    .reverse()
+    .join(' ')
+    .trim()
+}
+
+// -------------------------
+// TESTS
+
 console.log(reverseWords('the sky is blue')) // 'blue is the sky'
 console.log(reverseWords('  hello world  ')) // 'world hello'
 console.log(reverseWords('a good   example')) // 'example good a'
+
+console.log(reverseWords2('the sky is blue')) // 'blue is the sky'
+console.log(reverseWords2('  hello world  ')) // 'world hello'
+console.log(reverseWords2('a good   example')) // 'example good a'
