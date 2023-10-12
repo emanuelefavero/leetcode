cb12d5b579015c3098b73b9457e6f18ee0291e20
emanuelefavero <emafaux89@gmail.com>
2023-10-12 05:37:44 +0200

    change file location of 2d-array/pascal-triangle-ii back to array/

--- array/pascal-triangle-ii.js
+++ array/pascal-triangle-ii.js
@@ -1,4 +1,4 @@
-// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle
+// Given an integer rowIndex, return the rowIndex th (0-indexed) row of the Pascal's triangle
 
 // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 // https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1171/
