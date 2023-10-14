81a2ebff9d920e095adfd005f048fdd7c25591d0
emanuelefavero <emafaux89@gmail.com>
2023-10-14 02:36:54 +0200

    add addAtTail method to linked-list/design-linked-list

---
+++ linked-list/design-linked-list.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class MyLinkedList {
+  constructor() {
+    this.head = null
+    this.length = 0
+  }
+
+  get(index) {}
+
+  addAtHead(val) {}
+
+  addAtTail(val) {
+    let newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode
+    } else {
+      let current = this.head
+
+      while (current.next) {
+        current = current.next
+      }
+
+      current.next = newNode
+    }
+
+    this.length++
+  }
+
+  addAtIndex(index, val) {}
+
+  deleteAtIndex(index) {}
+}
+
+let obj = new MyLinkedList()
+obj.addAtTail(2)
+obj.addAtTail(3)
+// obj.addAtHead(1)
+
+console.log(obj)
