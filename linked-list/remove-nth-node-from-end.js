67c4d91f02f2c07436699c183e24e376fe77aab5
emanuelefavero <emafaux89@gmail.com>
2023-10-14 10:38:51 +0200

    add linked-list/remove-nth-node-from-end

---
+++ linked-list/remove-nth-node-from-end.js
@@ -0,0 +1,48 @@
+// TODO: Add jsDoc
+// TODO: Add solution 2?
+
+/**
+ * @typedef {Object} ListNode
+ * @property {*} val
+ */
+
+class ListNode {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+// * Remove Nth Node From End of List
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+function removeNthFromEnd(head, n) {
+  let slow = head
+  let fast = head
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next
+  }
+
+  // if the node to remove is the head, return the next node
+  if (!fast) return head.next
+
+  while (fast.next) {
+    slow = slow.next
+    fast = fast.next
+  }
+
+  slow.next = slow.next.next
+
+  return head
+}
+
+// -----------------------------
+// TESTS
+
+let head = new ListNode(1)
+head.next = new ListNode(2)
+head.next.next = new ListNode(3)
+head.next.next.next = new ListNode(4)
+
+console.log(removeNthFromEnd(head, 2)) // 1 -> 2 -> 4
