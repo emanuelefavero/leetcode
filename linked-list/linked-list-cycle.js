78ef89eb18fd33bf60509aea8bb9a313f1b6fa45
emanuelefavero <emafaux89@gmail.com>
2023-10-14 04:24:07 +0200

    add linked-list/linked-list-cycle

---
+++ linked-list/linked-list-cycle.js
@@ -0,0 +1,30 @@
+class ListNode {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+function hasCycle(head) {
+  if (!head || !head.next) return false
+
+  let slow = head
+  let fast = head.next
+
+  while (fast && fast.next) {
+    if (slow === fast) return true
+
+    fast = fast.next.next
+    slow = slow.next
+  }
+
+  return false
+}
+
+let head = new ListNode(1)
+head.next = new ListNode(2)
+head.next.next = new ListNode(3)
+head.next.next.next = new ListNode(4)
+head.next.next.next.next = head.next // cycle to node with val 2
+
+console.log(hasCycle(head)) // true
