d570db0c85f30f19cae8c11c4c824082014647ac
emanuelefavero <emafaux89@gmail.com>
2023-10-16 06:39:32 +0200

    add solution 2 to linked-list/merge-two-sorted-lists

--- linked-list/merge-two-sorted-lists.js
+++ linked-list/merge-two-sorted-lists.js
@@ -32,6 +32,27 @@ function mergeTwoLists(list1, list2) {
   return head.next
 }
 
+// -----------------------------
+// SOLUTION 2
+
+// TIP: This solution uses recursion
+
+function mergeTwoLists2(list1, list2) {
+  if (!list1) return list2
+  if (!list2) return list1
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists2(list1.next, list2)
+    return list1
+  } else {
+    list2.next = mergeTwoLists2(list1, list2.next)
+    return list2
+  }
+}
+
+// -----------------------------
+// TESTS
+
 let list1 = new ListNode(1)
 list1.next = new ListNode(2)
 list1.next.next = new ListNode(4)
@@ -41,3 +62,4 @@ list2.next = new ListNode(3)
 list2.next.next = new ListNode(4)
 
 console.log(mergeTwoLists(list1, list2)) // 1 -> 1 -> 2 -> 3 -> 4 -> 4
+// console.log(mergeTwoLists2(list1, list2)) // 1 -> 1 -> 2 -> 3 -> 4 -> 4
