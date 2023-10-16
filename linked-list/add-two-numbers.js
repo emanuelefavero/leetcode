31259f8abae45483b7ed242d408b8e06b0968bf0
emanuelefavero <emafaux89@gmail.com>
2023-10-16 07:46:58 +0200

    add solution 2 to linked-list/add-two-numbers

--- linked-list/add-two-numbers.js
+++ linked-list/add-two-numbers.js
@@ -40,6 +40,34 @@ function addTwoNumbers(l1, l2) {
   return result
 }
 
+// -----------------------------
+// SOLUTION 2
+
+// TIP: This solution uses recursion and carry but is less easy and readable
+
+function addTwoNumbers2(l1, l2, carry) {
+  // BASE CASE
+  if (!l1 && !l2 && !carry) return null
+
+  let total = carry || 0 // 0 if no carry
+
+  // Add remaining values if any
+  if (l1) total += l1.val
+  if (l2) total += l2.val
+
+  // Create new node with total % 10 - % 10 converts to single digit base 10
+  let newNode = new ListNode(total % 10)
+
+  // Recursively call function with next nodes
+  newNode.next = addTwoNumbers2(
+    l1 ? l1.next : null,
+    l2 ? l2.next : null,
+    total > 9 ? 1 : 0 // 1 if total > 9
+  )
+
+  return newNode // return linked list with total
+}
+
 let l1 = new ListNode(2)
 l1.next = new ListNode(4)
 l1.next.next = new ListNode(3)
@@ -48,4 +76,4 @@ let l2 = new ListNode(5)
 l2.next = new ListNode(6)
 l2.next.next = new ListNode(4)
 
-console.log(addTwoNumbers(l1, l2)) // 708
+console.log(addTwoNumbers2(l1, l2)) // 708
