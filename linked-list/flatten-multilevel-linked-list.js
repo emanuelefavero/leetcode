c23c95d9a2e0230874eb747db2e65a67f8f1381d
emanuelefavero <emafaux89@gmail.com>
2023-10-16 12:38:37 +0200

    add jsDoc to linked-list/flatten-multilevel-linked-list

--- linked-list/flatten-multilevel-linked-list.js
+++ linked-list/flatten-multilevel-linked-list.js
@@ -15,22 +15,21 @@ class ListNode {
   }
 }
 
-// You are given a doubly linked head, which contains nodes that have a next pointer, a previous pointer, and an additional child pointer. This child pointer may or may not point to a separate doubly linked head, also containing these special nodes. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure as shown in the example below.
-
-// Given the head of the first level of the head, flatten the head so that all the nodes appear in a single-level, doubly linked head. Let current be a node with a child head. The nodes in the child head should appear after current and before current.next in the flattened head.
-
-// Return the head of the flattened head. The nodes in the head must have all of their child pointers set to null.
+/**
+ * * Flatten Multilevel Linked List (Doubly Linked List)
+ * Given a doubly linked head, flatten the head so that all the nodes appear in a single-level, doubly linked head. Return the head of the flattened head
+ *
+ * @see https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/description/
+ * @param {ListNode} head
+ * @returns {ListNode}
+ */
 
 /*
 EXAMPLE:
 Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
 Output: [1,2,3,7,8,11,12,9,10,4,5,6]
 Explanation: The multilevel linked head in the input is shown.
-
-@see https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/description/
 */
-
-// TODO: Add jsDoc and clean up comments
 
 // TIP: This solution outputs a linked list (like the original problem)
 
