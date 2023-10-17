7e9d7a57eade90d62d28f75a1a68640b59eff454
emanuelefavero <emafaux89@gmail.com>
2023-10-17 22:32:36 +0200

    add jsDoc to linked-list/rotate-list

--- linked-list/rotate-list.js
+++ linked-list/rotate-list.js
@@ -9,16 +9,23 @@ class ListNode {
   }
 }
 
-// TODO: Add jsDoc
-
-// Rotate List
-// Given the head of a linked list, rotate the list to the right by k places.
+/**
+ * * Rotate List
+ *
+ * Given the head of a linked list, rotate the list to the right by k places.
+ *
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
 
 /*
+EXAMPLE:
 Input: head = [1,2,3,4,5], k = 2
 Output: [4,5,1,2,3]
 */
 
+// O(n) time | O(1) space
 function rotateRight(head, k) {
   if (!head) return null
 
