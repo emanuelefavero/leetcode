8a152adb2a9368f3da3f6822e383f9c9959ddd97
emanuelefavero <emafaux89@gmail.com>
2023-10-18 07:31:46 +0200

    add hash-table/design-hash-set boilerplate

---
+++ hash-table/design-hash-set.js
@@ -0,0 +1,28 @@
+// Design a HashSet without using any built-in hash table libraries.
+
+/* Implement MyHashSet class:
+  - void add(key) Inserts the value key into the HashSet.
+  - bool contains(key) Returns whether the value key exists in the HashSet or not.
+  - void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+*/
+
+class MyHashSet {
+  constructor() {}
+
+  add(key) {}
+
+  remove(key) {}
+
+  contains(key) {}
+}
+
+// -----------------------------
+// TESTS
+
+let obj = new MyHashSet()
+obj.add(1)
+obj.add(2)
+obj.remove(1)
+
+console.log(obj.contains(1)) // false
+console.log(obj.contains(2)) // true
