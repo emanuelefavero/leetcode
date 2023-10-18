25169dfbc7636d0736456090c8167b01218902a5
emanuelefavero <emafaux89@gmail.com>
2023-10-18 18:38:24 +0200

    add solution 2 to hash-table/single-number

--- hash-table/single-number.js
+++ hash-table/single-number.js
@@ -25,6 +25,24 @@ function singleNumber(nums) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// O(n) time | O(n) space
+function singleNumber2(nums) {
+  let result = 0 // initialize result with 0 so that XORing with the first number in the array will return that number
+
+  for (let i = 0; i < nums.length; i++) {
+    // XOR all the numbers in the array
+    result = result ^ nums[i] // ^ is the XOR operator
+
+    // TIP: This operation is commonly used in algorithms to find non-repeating or unique elements in an array. By XORing all the elements together, duplicates cancel out, and you're left with the unique element.(The XOR operator returns a 1, or true, if both the bits are different, and a 0, or false, if both the bits are the same)
+  }
+
+  return result
+}
+
+// -----------------------------
 // TESTS
 
 console.log(singleNumber([2, 2, 1, 3, 3])) // 1
+console.log(singleNumber2([2, 2, 1, 3, 3])) // 1
