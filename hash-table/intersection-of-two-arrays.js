b982894f56ba93598f7ece804c37d00098858f6f
emanuelefavero <emafaux89@gmail.com>
2023-10-18 18:12:38 +0200

    add jsDoc to hash-table/intersection-of-two-arrays

--- hash-table/intersection-of-two-arrays.js
+++ hash-table/intersection-of-two-arrays.js
@@ -1,4 +1,10 @@
-// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+/**
+ * * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order
+ *
+ * @param {number[]} nums1
+ * @param {number} nums2
+ * @returns {number[]}
+ */
 
 function intersection(nums1, nums2) {
   let set = new Set(nums1)
@@ -12,5 +18,8 @@ function intersection(nums1, nums2) {
 
   return [...result]
 }
+
+// -----------------------------
+// TESTS
 
 console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]))
