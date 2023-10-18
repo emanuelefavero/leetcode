cb6ca1feb0e720b251a8dca10cfd5845194c61bd
emanuelefavero <emafaux89@gmail.com>
2023-10-18 10:14:55 +0200

    add jsDoc to hash-table/contains-duplicate

--- hash-table/contains-duplicate.js
+++ hash-table/contains-duplicate.js
@@ -1,4 +1,9 @@
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct
+/**
+ * * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct
+ *
+ * @param {number[]} nums
+ * @returns {boolean}
+ */
 
 function containsDuplicate(nums) {
   let set = new Set()
