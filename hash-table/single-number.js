a99f269c44007c92fdda766bd3fa76264959ab5f
emanuelefavero <emafaux89@gmail.com>
2023-10-18 17:57:57 +0200

    add hash-table/single-number

--- hash-table/single-number.js
+++ hash-table/single-number.js
@@ -1,6 +1,22 @@
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one
 
-function singleNumber(nums) {}
+function singleNumber(nums) {
+  let hashTable = {}
+
+  for (let num of nums) {
+    if (hashTable[num]) {
+      hashTable[num]++
+    } else {
+      hashTable[num] = 1
+    }
+  }
+
+  for (let key in hashTable) {
+    if (hashTable[key] < 2) {
+      return key
+    }
+  }
+}
 
 // -----------------------------
 // TESTS
