71bb5ecbecdf6e6db1101828c558e0df1047197e
emanuelefavero <emafaux89@gmail.com>
2023-10-18 08:57:19 +0200

    add jsDoc to hash-table/design-hash-set

--- hash-table/design-hash-set.js
+++ hash-table/design-hash-set.js
@@ -1,23 +1,39 @@
-// Design a HashSet without using any built-in hash table libraries.
+// * Design a HashSet without using any built-in hash table libraries
 
-/* Implement MyHashSet class:
-  - void add(key) Inserts the value key into the HashSet.
-  - bool contains(key) Returns whether the value key exists in the HashSet or not.
-  - void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
-*/
+/**
+ * @typedef {Object} MyHashSet
+ */
 
 class MyHashSet {
   constructor() {
     this.set = {}
   }
 
+  /**
+   * Inserts the value key into the HashSet
+   * @param {*} key
+   * @returns {void}
+   */
+
   add(key) {
     this.set[key] = true
   }
 
+  /**
+   * Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing
+   * @param {*} key
+   * @returns {void}
+   */
+
   remove(key) {
     delete this.set[key]
   }
+
+  /**
+   * Returns whether the value key exists in the HashSet or not
+   * @param {*} key
+   * @returns {boolean}
+   */
 
   contains(key) {
     return this.set[key] === true
