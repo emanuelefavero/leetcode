31a0699971337b937eb76fa75075bd5d8722cf07
emanuelefavero <emafaux89@gmail.com>
2023-10-20 17:25:50 +0200

    add solution 2 to hash-table/first-unique-character-in-a-string

--- hash-table/first-unique-character-in-a-string.js
+++ hash-table/first-unique-character-in-a-string.js
@@ -22,7 +22,27 @@ function firstUniqChar(s) {
 }
 
 // -------------------------------
+// SOLUTION 2
+
+// TIP: This solution uses indexOf and lastIndexOf to check if the current character is unique (it uses less space but could be a bit harder to understand)
+
+function firstUniqChar2(s) {
+  for (let i = 0; i < s.length; i++) {
+    // check if the index of the current character is the same as the last index of the current character
+    // TIP: If the index is different, it means the character is repeated and it's not unique
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i
+  }
+
+  return -1
+}
+
+// -------------------------------
 // TESTS
 
+// Solution 1
 console.log(firstUniqChar('aabbc')) // 4
 console.log(firstUniqChar('aabb')) // -1
+
+// Solution 2
+console.log(firstUniqChar2('aabbc')) // 4
+console.log(firstUniqChar2('aabb')) // -1
