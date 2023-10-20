2fb8aca74703c22a0a1fb3049662b41a8896145c
emanuelefavero <emafaux89@gmail.com>
2023-10-20 17:16:23 +0200

    add hash-table/first-unique-character-in-a-string

---
+++ hash-table/first-unique-character-in-a-string.js
@@ -0,0 +1,22 @@
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1
+
+function firstUniqChar(s) {
+  let map = new Map()
+
+  for (let i = 0; i < s.length; i++) {
+    if (!map.has(s[i])) map.set(s[i], 1)
+    else map.set(s[i], map.get(s[i]) + 1)
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) < 2) return i
+  }
+
+  return -1
+}
+
+// -------------------------------
+// TESTS
+
+console.log(firstUniqChar('aabbc'))
+console.log(firstUniqChar('aabb'))
