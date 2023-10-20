dc01bb15d63767b6b2dde21e0e4802ce33e4bf81
emanuelefavero <emafaux89@gmail.com>
2023-10-20 07:34:46 +0200

    add jsDoc to hash-table/isomorphic-strings

--- hash-table/isomorphic-strings.js
+++ hash-table/isomorphic-strings.js
@@ -1,10 +1,10 @@
-// Given two strings s and t, determine if they are isomorphic.
-
-// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+// * Given two strings s and t, determine if they are isomorphic
 
 /*
+TIP: Two strings s and t are isomorphic if the characters in s can be replaced to get t
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself
+
 EXAMPLE:
 
 s = egg, t = add
@@ -19,14 +19,23 @@ r ?
 FALSE
 */
 
+/**
+ * @param {string} s
+ * @param {string} t
+ * @returns {boolean}
+ */
+
+// O(n) time | O(n) space
 function isIsomorphic(s, t) {
   let hashTable = new Map()
   let hashTable2 = new Map()
 
   for (let i = 0; i < s.length; i++) {
+    // fill the hash tables with the characters of the strings
     if (!hashTable.has(s[i])) hashTable.set(s[i], t[i])
     if (!hashTable2.has(t[i])) hashTable2.set(t[i], s[i])
 
+    // check if any of the characters are already mapped to a different character
     if (hashTable.get(s[i]) !== t[i] || hashTable2.get(t[i]) !== s[i])
       return false
   }
