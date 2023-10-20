a20b81fd0c2e34ce4c29a76f9746cafb0e6ddb59
emanuelefavero <emafaux89@gmail.com>
2023-10-20 17:49:35 +0200

    add solution 2 to hash-table/intersection-of-two-arrays-ii

--- hash-table/intersection-of-two-arrays-ii.js
+++ hash-table/intersection-of-two-arrays-ii.js
@@ -30,7 +30,35 @@ function intersect(nums1, nums2) {
 }
 
 // -------------------------------
+// SOLUTION 2
+
+// TIP: This solution is more memory efficient but slower
+
+// O(n^2) time | O(n) space
+function intersect2(nums1, nums2) {
+  let result = []
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) {
+        result.push(nums1[i])
+        // remove the element from nums2 to avoid duplicates
+        nums2.splice(j, 1)
+        break // break the inner loop
+      }
+    }
+  }
+
+  return result
+}
+
+// -------------------------------
 // TESTS
 
+// Solution 1
 console.log(intersect([1, 2, 2, 1], [2, 2])) // [2, 2]
 console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])) // [4, 9]
+
+// Solution 2
+console.log(intersect2([1, 2, 2, 1], [2, 2])) // [2, 2]
+console.log(intersect2([4, 9, 5], [9, 4, 9, 8, 4])) // [4, 9]
