a8ca6bc9b4f1b1532408d1812fae6d6734ca7d19
emanuelefavero <emafaux89@gmail.com>
2023-10-22 05:10:40 +0200

    add solution 2 to hash-table/valid-sudoku

--- hash-table/valid-sudoku.js
+++ hash-table/valid-sudoku.js
@@ -19,19 +19,20 @@ function isValidSudoku(board) {
     for (let j = 0; j < 9; j++) {
       let _row = board[i][j]
       let _col = board[j][i]
+      // TIP: This is the formula to get the row and column of the 3x3 box
       let _box =
         board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
 
-      if (_row != '.') {
+      if (_row !== '.') {
         if (row.has(_row)) return false
         row.add(_row)
       }
-      if (_col != '.') {
+      if (_col !== '.') {
         if (col.has(_col)) return false
         col.add(_col)
       }
 
-      if (_box != '.') {
+      if (_box !== '.') {
         if (box.has(_box)) return false
         box.add(_box)
       }
@@ -41,8 +42,50 @@ function isValidSudoku(board) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+function isValidSudoku2(board) {
+  // rows
+  for (let i = 0; i < 9; i++) {
+    let nums = new Set()
+
+    for (let j = 0; j < 9; j++) {
+      if (nums.has(board[i][j])) return false
+      else if (board[i][j] !== '.') nums.add(board[i][j])
+    }
+  }
+
+  // columns
+  for (let i = 0; i < 9; i++) {
+    let nums = new Set()
+
+    for (let j = 0; j < 9; j++) {
+      if (nums.has(board[j][i])) return false
+      else if (board[j][i] !== '.') nums.add(board[j][i])
+    }
+  }
+
+  // boxes
+  for (let i = 0; i < 9; i++) {
+    let nums = new Set()
+
+    for (let j = 0; j < 9; j++) {
+      // TIP: This is the formula to get the row and column of the 3x3 box
+      let row = Math.floor(i / 3) * 3 + Math.floor(j / 3)
+      let column = (j % 3) + 3 * (i % 3)
+
+      if (nums.has(board[row][column])) return false
+      else if (board[row][column] !== '.') nums.add(board[row][column])
+    }
+  }
+
+  return true
+}
+
+// -----------------------------
 // TESTS
 
+// Solution 1
 console.log(
   isValidSudoku([
     ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
@@ -71,3 +114,32 @@ console.log(
   ])
 ) // false
 // Explanation: Same as above, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid
+
+// Solution 2
+console.log(
+  isValidSudoku2([
+    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+    ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+  ])
+) // true
+
+console.log(
+  isValidSudoku2([
+    ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+    ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+  ])
+) // false
