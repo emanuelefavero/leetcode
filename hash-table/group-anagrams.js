8ce41276b046c78b04d890393ddc1bd2d6f6f599
emanuelefavero <emafaux89@gmail.com>
2023-10-22 02:01:34 +0200

    add hash-table/group-anagrams

---
+++ hash-table/group-anagrams.js
@@ -0,0 +1,27 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// TIP: An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+function groupAnagrams(strs) {
+  let map = new Map()
+
+  for (string of strs) {
+    let sortedString = string.split('').sort().join('')
+
+    if (map.has(sortedString)) {
+      map.get(sortedString).push(string)
+    } else {
+      map.set(sortedString, [string])
+    }
+  }
+
+  return [...map.values()]
+}
+
+// -----------------------------
+// TESTS
+
+console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+// [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+console.log(groupAnagrams([''])) // [[""]]
