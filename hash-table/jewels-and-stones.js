9650350ccc5b7dbb9ca3122be9c497cc9f0c142f
emanuelefavero <emafaux89@gmail.com>
2023-10-22 09:40:38 +0200

    add jsDoc to hash-table/jewels-and-stones

--- hash-table/jewels-and-stones.js
+++ hash-table/jewels-and-stones.js
@@ -1,7 +1,14 @@
-// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels
+// * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels
 
 // Letters are case sensitive, so "a" is considered a different type of stone from "A"
 
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @returns {number}
+ */
+
+// O(n) time | O(n) space
 function numJewelsInStones(jewels, stones) {
   let set = new Set(...jewels)
   let count = 0
