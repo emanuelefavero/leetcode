d5c7eb0d75f7cef49cde4614d72dec10d5dbfa92
emanuelefavero <emafaux89@gmail.com>
2023-10-23 01:17:35 +0200

    add hash-table/insert-delete-get-random

--- hash-table/insert-delete-get-random.js
+++ hash-table/insert-delete-get-random.js
@@ -7,3 +7,50 @@ bool remove(int val) Removes an item val from the set if present. Returns true i
 int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
 You must implement the functions of the class such that each function works in average O(1) time complexity.
 */
+
+// TODO: jsDoc
+
+class RandomizedSet {
+  constructor() {
+    this.set = {}
+  }
+
+  // Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+  insert(val) {
+    if (!this.set[val]) {
+      this.set[val] = true
+      return true
+    } else return false
+  }
+
+  // Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+  remove(val) {
+    if (!this.set[val]) {
+      return false
+    } else {
+      delete this.set[val]
+      return true
+    }
+  }
+
+  // Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned
+  getRandom() {
+    let keys = Object.keys(this.set)
+    let randomIndex = Math.floor(Math.random() * keys.length)
+    return keys[randomIndex]
+  }
+}
+
+// --------------------------
+// TESTS
+
+let obj = new RandomizedSet()
+obj.insert(1) // true, 1 was inserted successfully
+obj.remove(2) // false, 2 is not in the set
+obj.insert(2) // true
+console.log(obj.getRandom()) // 1 or 2
+obj.remove(1) // true
+obj.insert(2) // false
+console.log(obj.getRandom()) // 2
+
+console.log(obj)
