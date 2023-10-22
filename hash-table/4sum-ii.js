9a7ca98fccc298605f49687e6e2ce6a5366d9812
emanuelefavero <emafaux89@gmail.com>
2023-10-22 13:55:11 +0200

    add jsDoc to hash-table/4sum-ii

--- hash-table/4sum-ii.js
+++ hash-table/4sum-ii.js
@@ -1,7 +1,17 @@
-// Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
-// 0 <= i, j, k, l < n
-// nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+/**
+ * * Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+ *
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @returns {number}
+ */
 
+// O(n^2) time | O(n^2) space
 function fourSumCount(nums1, nums2, nums3, nums4) {
   let map = new Map()
   let count = 0
