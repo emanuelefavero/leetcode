a95cd43681a711a3325e2b080fa909cceb3d5b78
emanuelefavero <emafaux89@gmail.com>
2023-10-22 10:20:08 +0200

    add hash-table/longest-without-repeating-chars solution 2

--- hash-table/longest-without-repeating-chars.js
+++ hash-table/longest-without-repeating-chars.js
@@ -28,16 +28,40 @@ function lengthOfLongestSubstring(s) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+function lengthOfLongestSubstring2(s) {
+  let longest = 0
+  let current = []
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i]
+
+    if (!current.includes(char)) {
+      current.push(char)
+
+      if (current.length > longest) longest = current.length
+    } else {
+      // If the character is in the current array, remove all the characters before the character and add the character to the end of the array
+      current = current.slice(current.indexOf(char) + 1)
+      current.push(char)
+    }
+  }
+
+  return longest
+}
+
+// -----------------------------
 // TESTS
 
-console.log(lengthOfLongestSubstring('abcabcbb')) // 3
+console.log(lengthOfLongestSubstring2('abcabcbb')) // 3
 // Explanation: The answer is 'abc', with the length of 3
 
-console.log(lengthOfLongestSubstring('bbbbb')) // 1
+console.log(lengthOfLongestSubstring2('bbbbb')) // 1
 // Explanation: The answer is 'b', with the length of 1
 
-console.log(lengthOfLongestSubstring('pwwkew')) // 3
+console.log(lengthOfLongestSubstring2('pwwkew')) // 3
 // Explanation: The answer is 'wke', with the length of 3
 
-console.log(lengthOfLongestSubstring('aab')) // 2
+console.log(lengthOfLongestSubstring2('aab')) // 2
 // Explanation: The answer is 'ab', with the length of 2
