778241ba032578179abc2ec214b4930918ad540a
emanuelefavero <emafaux89@gmail.com>
2023-10-24 17:32:02 +0200

    add jsDoc to queue/open-the-lock

--- queue/open-the-lock.js
+++ queue/open-the-lock.js
@@ -1,5 +1,3 @@
-// * Open the Lock
-
 /*
 You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
 
@@ -32,6 +30,14 @@ Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], tar
 Output: -1
 Explanation: We cannot reach the target without getting stuck.
 */
+
+/**
+ * * Open the Lock
+ *
+ * @param {string[]} deadends
+ * @param {string} target
+ * @returns {number}
+ */
 
 function openLock(deadends, target) {
   const deadendsSet = new Set(deadends)
