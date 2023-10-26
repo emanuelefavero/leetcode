8fcf5dc60e84c73b9998f2dae89ca94482f929e1
emanuelefavero <emafaux89@gmail.com>
2023-10-27 01:16:41 +0200

    add stack/evaluate-reverse-poly-notation solution 2

--- stack/evaluate-reverse-poly-notation.js
+++ stack/evaluate-reverse-poly-notation.js
@@ -16,11 +16,11 @@ The answer and all the intermediate calculations can be represented in a 32-bit 
 */
 
 function operation(a, b, operator) {
+  // TIP: Math.ceil() is used to round towards 0 when the result is negative
+
   if (operator === '+') return a + b
   if (operator === '-') return a - b
   if (operator === '*') return a * b
-
-  // Math.ceil() is used to round towards 0 when the result is negative
   if (operator === '/') return a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b)
 }
 
@@ -43,16 +43,64 @@ function evalRPN(tokens) {
 }
 
 // ----------------------------
+// SOLUTION 2
+
+// TODO: add link to README
+// TODO: add jsDoc (add @see tag, see up top)
+
+function evalRPN2(tokens) {
+  let stack = []
+  let operation = {
+    // NOTE: You can use an arrow function as a value in an object
+    // TIP: Math.ceil() is used to round towards 0 when the result is negative
+
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => (a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b)),
+  }
+
+  for (let value of tokens) {
+    if (value in operation) {
+      let top = stack.pop()
+      let second = stack.pop()
+
+      // NOTE: You can pass arguments to a function inside an object
+      // TIP: put the second operand first in the operation for the algorithm to work
+      stack.push(operation[value](second, top))
+    } else {
+      stack.push(Number(value))
+    }
+  }
+
+  return stack.pop()
+}
+
+// ----------------------------
 // TESTS
 
-console.log(evalRPN(['2', '1', '+', '3', '*'])) // 9
+console.log(evalRPN2(['2', '1', '+', '3', '*'])) // 9
 // Explanation: ((2 + 1) * 3) = 9
 
-console.log(evalRPN(['4', '13', '5', '/', '+'])) // 6
+console.log(evalRPN2(['4', '13', '5', '/', '+'])) // 6
 // Explanation: (4 + (13 / 5)) = 6
 
-console.log(evalRPN(['3', '4', '-'])) // -1
+console.log(evalRPN2(['3', '4', '-'])) // -1
 
 console.log(
-  evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])
+  evalRPN2([
+    '10',
+    '6',
+    '9',
+    '3',
+    '+',
+    '-11',
+    '*',
+    '/',
+    '*',
+    '17',
+    '+',
+    '5',
+    '+',
+  ])
 ) // 22
