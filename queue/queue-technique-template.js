73e7f24e623e2352e7fefd0867bf1a418f1d37ac
emanuelefavero <emafaux89@gmail.com>
2023-10-26 06:16:00 +0200

    add template 2 to queue/queue-technique-template

--- queue/queue-technique-template.js
+++ queue/queue-technique-template.js
@@ -15,3 +15,27 @@ function print(array) {
 }
 
 print([1, 2, 3, 4, 5]) // 1 2 3 4 5
+
+// ------------------------
+// TEMPLATE 2
+
+// TIP: This is a template for a breadth first search (BFS) on a binary tree
+
+function BFS(array) {
+  let queue = [array[0]]
+
+  while (queue.length) {
+    let queueLength = queue.length
+
+    for (let i = 0; i < queueLength; i++) {
+      let current = queue.shift()
+
+      console.log(current) // DO SOMETHING WITH CURRENT
+
+      if (current.left) queue.push(current.left)
+      if (current.right) queue.push(current.right)
+    }
+  }
+}
+
+BFS([1, 2, 3, 4, 5]) // 1 2 3 4 5
