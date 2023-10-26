01fc7d539445ec62fb28bc35a6a9dfd32bb190d2
emanuelefavero <emafaux89@gmail.com>
2023-10-26 11:57:46 +0200

    refactor stack/daily-temperatures

--- stack/daily-temperatures.js
+++ stack/daily-temperatures.js
@@ -2,20 +2,17 @@
 
 function dailyTemperatures(temperatures) {
   let stack = []
-  let result = []
+  let result = new Array(temperatures.length).fill(0)
 
   for (let i = 0; i < temperatures.length; i++) {
     while (
       stack.length &&
       temperatures[i] > temperatures[stack[stack.length - 1]]
     ) {
-      let top = stack.pop() // pop and get the index of the top element
-      result[top] = i - top // calculate the difference between the indices
+      let index = stack.pop()
+      result[index] = i - index
     }
-
     stack.push(i)
-
-    result[i] = 0
   }
 
   return result
@@ -27,3 +24,6 @@ function dailyTemperatures(temperatures) {
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
 // [1, 1, 4, 2, 1, 1, 0, 0]
 console.log(dailyTemperatures([30, 40, 50, 60])) // [1, 1, 1, 0]
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
+// [1 ,1 ,4 ,2 ,1 ,1 ,0 ,0]
