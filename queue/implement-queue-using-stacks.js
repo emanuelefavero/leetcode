b4129fcb46d4ebfe4815a3a238dedfe6b0e1e138
emanuelefavero <emafaux89@gmail.com>
2023-10-30 07:25:03 +0100

    add jsDoc to stack/implement-queue-using-stacks

--- queue/implement-queue-using-stacks.js
+++ queue/implement-queue-using-stacks.js
@@ -1,6 +1,8 @@
-// Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
-
-// TODO: add jsDoc
+/**
+ * * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty)
+ *
+ * @typedef {Object} MyQueue
+ */
 
 class MyQueue {
   constructor() {
@@ -8,11 +10,16 @@ class MyQueue {
     this.stack2 = []
   }
 
+  /**
+   * @param {number} x
+   */
+
   push(x) {
     this.stack1.push(x)
   }
 
   // NOTE: This is the key to the solution, before calling pop or peek we need to migrate the elements from stack1 to stack2 so that the first element in stack2 is the first element in the queue
+
   _migrate() {
     if (!this.stack2.length) {
       while (this.stack1.length) {
@@ -21,15 +28,27 @@ class MyQueue {
     }
   }
 
+  /**
+   * @returns {number}
+   */
+
   pop() {
     this._migrate()
     return this.stack2.pop()
   }
 
+  /**
+   * @returns {number}
+   */
+
   peek() {
     this._migrate()
     return this.stack2[this.stack2.length - 1]
   }
+
+  /**
+   * @returns {boolean}
+   */
 
   empty() {
     return !this.stack1.length && !this.stack2.length
