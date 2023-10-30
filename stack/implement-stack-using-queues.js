b1c5657f68bb26d3f2c7ff4f55e097a2301c38b5
emanuelefavero <emafaux89@gmail.com>
2023-10-30 09:36:11 +0100

    add jsDoc to stack/implement-stack-using-queues methods

--- stack/implement-stack-using-queues.js
+++ stack/implement-stack-using-queues.js
@@ -1,7 +1,7 @@
 /**
  * * Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty)
  *
- * @typedef {Object} MyQueue
+ * @typedef {Object} MyStack
  */
 
 class MyStack {
@@ -10,9 +10,17 @@ class MyStack {
     this.queue2 = []
   }
 
+  /**
+   * @param {number} x
+   */
+
   push(x) {
     this.queue1.push(x)
   }
+
+  /**
+   * @returns {number}
+   */
 
   pop() {
     while (this.queue1.length > 1) {
@@ -21,25 +29,34 @@ class MyStack {
 
     let popped = this.queue1.shift()
 
-    // swap queues
+    // swap queues - we need to do this because we need to keep the queue in the same state as it was before calling pop
     ;[this.queue1, this.queue2] = [this.queue2, this.queue1]
 
     return popped
   }
+
+  /**
+   * @returns {number}
+   */
 
   top() {
     while (this.queue1.length > 1) {
       this.queue2.push(this.queue1.shift())
     }
 
+    // get the top element and push it to the other queue
     let top = this.queue1[0]
     this.queue2.push(this.queue1.shift())
 
-    // swap queues
+    // swap queues - we need to do this because we need to keep the queue in the same state as it was before calling top
     ;[this.queue1, this.queue2] = [this.queue2, this.queue1]
 
     return top
   }
+
+  /**
+   * @returns {boolean}
+   */
 
   empty() {
     return !this.queue1.length
