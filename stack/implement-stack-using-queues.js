b1339d0d4516ae5b94970bf0c24889015ba0f466
emanuelefavero <emafaux89@gmail.com>
2023-10-30 09:30:56 +0100

    fix stack/implement-stack-using-queues methods

--- stack/implement-stack-using-queues.js
+++ stack/implement-stack-using-queues.js
@@ -4,8 +4,6 @@
  * @typedef {Object} MyQueue
  */
 
-// NOTE: Create a stack using two queues. The queues can't use the pop method, only shift and push
-
 class MyStack {
   constructor() {
     this.queue1 = []
@@ -16,26 +14,35 @@ class MyStack {
     this.queue1.push(x)
   }
 
-  _migrate() {
-    if (!this.queue2.length) {
-      while (this.queue1.length) {
-        this.queue2.push(this.queue1.shift())
-      }
-    }
-  }
-
   pop() {
-    this._migrate()
-    return this.queue2.pop()
+    while (this.queue1.length > 1) {
+      this.queue2.push(this.queue1.shift())
+    }
+
+    let popped = this.queue1.shift()
+
+    // swap queues
+    ;[this.queue1, this.queue2] = [this.queue2, this.queue1]
+
+    return popped
   }
 
   top() {
-    this._migrate()
-    return this.queue2[this.queue2.length - 1]
+    while (this.queue1.length > 1) {
+      this.queue2.push(this.queue1.shift())
+    }
+
+    let top = this.queue1[0]
+    this.queue2.push(this.queue1.shift())
+
+    // swap queues
+    ;[this.queue1, this.queue2] = [this.queue2, this.queue1]
+
+    return top
   }
 
   empty() {
-    return !this.queue1.length && !this.queue2.length
+    return !this.queue1.length
   }
 }
 
@@ -49,8 +56,9 @@ stack.push(2)
 stack.push(3)
 stack.push(4)
 
-console.log(stack.pop()) // 1
-console.log(stack.top()) // 2
+console.log(stack.pop()) // 4
+console.log(stack.top()) // 3
 console.log(stack.empty()) // false
 
 console.log(stack)
+// MyStack { queue1: [ 1, 2, 3 ], queue2: [] }
