236d7aeb60ef41734452b954a29a3334dcb36548
emanuelefavero <emafaux89@gmail.com>
2023-11-01 02:49:40 +0100

    add jsDoc to matrix/flood-fill

--- matrix/flood-fill.js
+++ matrix/flood-fill.js
@@ -1,5 +1,24 @@
 // * Flood Fill
 
+/*
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+*/
+
+/**
+ * @param {number[][]} image - 2D array of integers
+ * @param {number} sr - starting row
+ * @param {number} sc - starting column
+ * @param {number} color - color to fill
+ * @returns {number[][]}
+ */
+
+// O(n) time | O(n) space
 function floodFill(image, sr, sc, color) {
   let row = image.length
   let column = image[0].length
