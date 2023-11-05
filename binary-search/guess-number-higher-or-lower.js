c9a1d7783343233b26788dea67223add12f06a0d
emanuelefavero <emafaux89@gmail.com>
2023-11-06 00:47:30 +0100

    add binary-search/guess-number-higher-or-lower

---
+++ binary-search/guess-number-higher-or-lower.js
@@ -0,0 +1,47 @@
+// * Guess Number Higher or Lower
+
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+*/
+
+// NOTE: simulate guess function (this function is provided by LeetCode, we don't need to implement it)
+
+function guess(n) {
+  const pick = 6
+
+  if (n > pick) return -1
+  else if (n < pick) return 1
+  else return 0
+}
+
+// TODO: add link to README
+// TODO: add jsDoc
+
+function guessNumber(n) {
+  let left = 1
+  let right = n
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+
+    if (guess(middle) === 0) return middle
+    else if (guess(middle) === 1) left = middle + 1
+    else if (guess(middle) === -1) right = middle - 1
+  }
+}
+
+// -------------------------
+// TESTS
+
+console.log(guessNumber(10)) // 6
