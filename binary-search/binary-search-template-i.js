b2f52891ac5ed81a589a5cca9ade34ab836e29cf
emanuelefavero <emafaux89@gmail.com>
2023-11-05 09:36:23 +0100

    add binary-search/binary-search-template-i

---
+++ binary-search/binary-search-template-i.js
@@ -0,0 +1,24 @@
+// * Binary Search Template I
+
+function binarySearch(nums, target) {
+  if (!nums.length) return -1
+
+  let left = 0
+  let right = nums.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+
+    if (nums[middle] === target) return middle
+    else if (nums[middle] < target) left = middle + 1
+    else right = middle - 1
+  }
+
+  return -1
+}
+
+// ------------------------
+// TESTS
+
+console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9)) // 4, the index of 9 is 4
+console.log(binarySearch([-1, 0, 3, 5, 9, 12], 2)) // -1, 2 does not exist in nums
