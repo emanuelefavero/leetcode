422867b9056a79673a626d8f10f6bf537a021a3e
emanuelefavero <emafaux89@gmail.com>
2023-11-07 10:40:11 +0100

    add binary-search/search-in-rotated-sorted-array comments

--- binary-search/search-in-rotated-sorted-array.js
+++ binary-search/search-in-rotated-sorted-array.js
@@ -1,3 +1,17 @@
+// * Search in Rotated Sorted Array
+
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity
+*/
+
+// TIP: We will use binary search to solve this problem efficiently
+
 function search(nums, target) {
   let left = 0
   let right = nums.length - 1
@@ -26,5 +40,8 @@ function search(nums, target) {
 
   return -1
 }
+
+// -------------------------
+// TESTS
 
 console.log(search([4, 5, 6, 7, 0, 1, 2], 6)) // 2 (six is at index 2)
