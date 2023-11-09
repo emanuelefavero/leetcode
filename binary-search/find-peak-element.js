32aac7ae31fb9121c89937e43181273b9f0cae2e
emanuelefavero <emafaux89@gmail.com>
2023-11-09 16:49:47 +0100

    add binary-search/find-peak-element jsDoc

--- binary-search/find-peak-element.js
+++ binary-search/find-peak-element.js
@@ -1,3 +1,23 @@
+// * Find Peak Element
+
+/*
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+*/
+
+// TODO: commit jsDoc
+
+/**
+ * @param {number[]} nums
+ * @returns {number} index of peak element
+ */
+
+// O(log n) time | O(1) space
 function findPeakElement(nums) {
   let left = 0
   let right = nums.length - 1
@@ -15,6 +35,7 @@ function findPeakElement(nums) {
 // ------------------------
 // SOLUTION 2 - linear search
 
+// O(n) time | O(1) space
 function findPeakElement2(nums) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > nums[i + 1]) return i
@@ -30,4 +51,4 @@ console.log(findPeakElement([1, 2, 3, 1])) // 2
 console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4])) // 5
 
 console.log(findPeakElement2([1, 2, 3, 1])) // 2
-console.log(findPeakElement2([1, 2, 1, 3, 5, 6, 4])) // 5
+console.log(findPeakElement2([1, 2, 1, 3, 5, 6, 4])) // 1, the index of 2
