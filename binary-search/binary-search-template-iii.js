eeca8129defc7f900d323d8859918ca5a48f2692
emanuelefavero <emafaux89@gmail.com>
2023-11-11 17:15:59 +0100

    add binary-search/binary-search-template-iii

---
+++ binary-search/binary-search-template-iii.js
@@ -0,0 +1,26 @@
+function binarySearch(nums, target) {
+  if (!nums.length) return -1
+
+  let left = 0
+  let right = nums.length - 1
+
+  while (left + 1 < right) {
+    let middle = Math.floor((left + right) / 2)
+
+    if (nums[middle] === target) return middle
+    else if (nums[middle] < target) left = middle
+    else right = middle
+  }
+
+  // Post-processing
+  // End condition: left + 1 = right
+  if (nums[left] === target) return left
+  if (nums[right] === target) return right
+  return -1
+}
+
+// ------------------------
+// TESTS
+
+console.log(binarySearch([1, 2, 3, 4, 5], 3)) // 2, index of 3
+console.log(binarySearch([1, 2, 3, 4, 5], 6)) // -1, not found
