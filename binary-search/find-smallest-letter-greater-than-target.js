6de164c6ed47853b82eea075c27cb8b89cd0b41d
emanuelefavero <emafaux89@gmail.com>
2023-11-13 17:16:18 +0100

    add binary-search/find-smallest-letter-greater-than-target solution 2

--- binary-search/find-smallest-letter-greater-than-target.js
+++ binary-search/find-smallest-letter-greater-than-target.js
@@ -1,6 +1,27 @@
 // * Find Smallest Letter Greater Than Target
 
+/*
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+*/
+
+// TODO: add jsDoc
+
 function nextGreatestLetter(letters, target) {
+  for (let letter of letters) {
+    if (letter.charCodeAt(0) > target.charCodeAt(0)) return letter
+  }
+
+  return letters[0]
+}
+
+// ---------------------
+// SOLUTION 2
+
+// TIP: This solution uses binary search
+
+function nextGreatestLetter2(letters, target) {
   let left = 0
   let right = letters.length - 1
 
@@ -14,6 +35,15 @@ function nextGreatestLetter(letters, target) {
   return letters[left % letters.length] // TIP: This is to handle the case where the target is greater than the last letter in the array
 }
 
+// ---------------------
+// TESTS
+
+// 1
 console.log(nextGreatestLetter(['c', 'f', 'j'], 'a')) // 'c'
 console.log(nextGreatestLetter(['c', 'f', 'j'], 'c')) // 'f'
 console.log(nextGreatestLetter(['x', 'x', 'y', 'y'], 'z')) // 'x'
+
+// 2
+console.log(nextGreatestLetter2(['c', 'f', 'j'], 'a')) // 'c'
+console.log(nextGreatestLetter2(['c', 'f', 'j'], 'c')) // 'f'
+console.log(nextGreatestLetter2(['x', 'x', 'y', 'y'], 'z')) // 'x'
