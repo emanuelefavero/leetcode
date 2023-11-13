eb355a3389e1eb01641e2085cf3aebb12dfab918
emanuelefavero <emafaux89@gmail.com>
2023-11-13 14:32:59 +0100

    add binary-search/valid-perfect-square solution 3

--- binary-search/valid-perfect-square.js
+++ binary-search/valid-perfect-square.js
@@ -21,6 +21,29 @@ function isPerfectSquare2(num) {
 }
 
 // ---------------------
+// SOLUTION 3
+
+// TIP: This solution uses binary search
+
+function isPerfectSquare3(num) {
+  let left = 1
+  let right = num
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+
+    // If the square of mid is equal to num, we have found the answer
+    if (mid * mid === num) return true
+    // If the square of mid is greater than num, we must move left
+    else if (mid * mid > num) right = mid - 1
+    // If the square of mid is less than num, we must move right
+    else left = mid + 1
+  }
+
+  return false
+}
+
+// ---------------------
 // TESTS
 
 // 1
@@ -31,6 +54,10 @@ function isPerfectSquare2(num) {
 // console.log(isPerfectSquare(2)) // false
 // console.log(isPerfectSquare(14)) // false
 
-// 2
-console.log(isPerfectSquare2(16)) // true
-console.log(isPerfectSquare2(14)) // false
+// // 2
+// console.log(isPerfectSquare2(16)) // true
+// console.log(isPerfectSquare2(14)) // false
+
+// 3
+console.log(isPerfectSquare3(16)) // true
+console.log(isPerfectSquare3(14)) // false
