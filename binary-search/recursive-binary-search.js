6762036f90bf2c4db378c666c29581103e15b055
emanuelefavero <emafaux89@gmail.com>
2023-11-13 12:41:00 +0100

    add binary-search/recursive-binary-search

---
+++ binary-search/recursive-binary-search.js
@@ -0,0 +1,22 @@
+// * Recursive Binary Search
+
+// TIP: This is an implementation of the binary search algorithm using recursion
+
+function binarySearch(nums, target, left = 0, right = nums.length - 1) {
+  if (left > right) return -1 // BASE CASE
+
+  let middle = Math.floor((left + right) / 2)
+
+  if (nums[middle] === target) return middle
+  else if (nums[middle] < target) {
+    return binarySearch(nums, target, middle + 1, right)
+  } else {
+    return binarySearch(nums, target, left, middle - 1)
+  }
+}
+
+// ------------------------
+// TESTS
+
+console.log(binarySearch([1, 2, 3, 4, 5], 3)) // 2, index of 3
+console.log(binarySearch([1, 2, 3, 4, 5], 6)) // -1, not found
