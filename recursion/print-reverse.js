1314a8d49161edf6fe250d78ed12ed721dd69469
emanuelefavero <emafaux89@gmail.com>
2023-11-17 10:26:29 +0100

    add recursion/print-reverse solution 3

--- recursion/print-reverse.js
+++ recursion/print-reverse.js
@@ -30,12 +30,25 @@ function printReverse(string, index = 0, output = '') {
 // TIP: This solution returns a reversed string instead of printing it
 
 // O(n) time | O(n) space
-function printReverse(string, index = 0, output = '') {
+function printReverse2(string, index = 0, output = '') {
   if (index >= string.length) return output
 
   output = string[index] + output
 
-  return printReverse(string, index + 1, output)
+  return printReverse2(string, index + 1, output)
+}
+
+// ---------------------
+// SOLUTION 3
+
+function printReverse3(string) {
+  let output = ''
+
+  for (let i = string.length; i <= 0; i--) {
+    output += string[i]
+  }
+
+  return output
 }
 
 // ---------------------
@@ -46,5 +59,9 @@ printReverse('Hello') // o, l, l, e, H
 printReverse('') // ''
 
 // 2
-console.log(printReverse('Hello')) // olleH
-console.log(printReverse('')) // ''
+console.log(printReverse2('Hello')) // olleH
+console.log(printReverse2('')) // ''
+
+// 3
+console.log(printReverse3('Hello')) // olleH
+console.log(printReverse3('')) // ''
