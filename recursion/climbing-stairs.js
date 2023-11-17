3289d9b374e49c518da59790368371ffff83aee4
emanuelefavero <emafaux89@gmail.com>
2023-11-17 04:34:51 +0100

    add recursion/climbing-stairs solution 2

--- recursion/climbing-stairs.js
+++ recursion/climbing-stairs.js
@@ -23,8 +23,28 @@ function climbStairs(n, memo = {}) {
 }
 
 // -----------------------
+// SOLUTION 2
+
+function climbStairs2(n) {
+  let result = [1, 1]
+
+  for (let i = 2; i <= n; i++) {
+    // ? add previous and current numbers up until n
+    result.push(result[i - 1] + result[i - 2])
+  }
+
+  return result[n]
+}
+
+// -----------------------
 // TESTS
 
+// 1
 console.log(climbStairs(2)) // 2, 1 + 1 = 2, 2 = 2
 console.log(climbStairs(3)) // 3, 1 + 1 + 1 = 3, 1 + 2 = 3, 2 + 1 = 3
 console.log(climbStairs(4)) // 5, 1 + 1 + 1 + 1 = 4, 1 + 1 + 2 = 4, 1 + 2 + 1 = 4, 2 + 1 + 1 = 4, 2 + 2 = 4
+
+// 2
+console.log(climbStairs2(2)) // 2, 1 + 1 = 2, 2 = 2
+console.log(climbStairs2(3)) // 3, 1 + 1 + 1 = 3, 1 + 2 = 3, 2 + 1 = 3
+console.log(climbStairs2(4)) // 5, 1 + 1 + 1 + 1 = 4, 1 + 1 + 2 = 4, 1 + 2 + 1 = 4, 2 + 1 + 1 = 4, 2 + 2 = 4
