b73d7b2a3c8d3fc03145788a04fb72ed8f2dfe56
emanuelefavero <emafaux89@gmail.com>
2023-11-17 22:48:40 +0100

    add recursion/merge-two-sorted-lists solution 2

--- recursion/merge-two-sorted-lists.js
+++ recursion/merge-two-sorted-lists.js
@@ -37,6 +37,35 @@ function mergeTwoLists(list1, list2) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: This solution uses iteration
+// TIP: In my opinion this solution is easier to understand
+
+// O(n + m) time | O(1) space
+function mergeTwoLists2(list1, list2) {
+  let head = new ListNode()
+  let current = head
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1
+      list1 = list1.next
+    } else {
+      current.next = list2
+      list2 = list2.next
+    }
+
+    current = current.next
+  }
+
+  // add possible remaining nodes
+  current.next = list1 || list2 || null
+
+  return head.next // ? head.next because head is a dummy node
+}
+
+// -----------------------------
 // TESTS
 
 let list1 = new ListNode(1)
@@ -47,4 +76,6 @@ let list2 = new ListNode(1)
 list2.next = new ListNode(3)
 list2.next.next = new ListNode(4)
 
-console.log(mergeTwoLists(list1, list2)) // 1 -> 1 -> 2 -> 3 -> 4 -> 4
+// NOTE: Run one solution at a time
+// console.log(mergeTwoLists(list1, list2)) // 1 -> 1 -> 2 -> 3 -> 4 -> 4
+console.log(mergeTwoLists2(list1, list2)) // 1 -> 1 -> 2 -> 3 -> 4 -> 4
