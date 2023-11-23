1b7563aa2d817f20a4aa807e6b8d524e9a819dce
emanuelefavero <emafaux89@gmail.com>
2023-11-23 08:58:12 +0100

    add trie/map-sum-pairs jsDoc and comments

--- trie/map-sum-pairs.js
+++ trie/map-sum-pairs.js
@@ -1,12 +1,45 @@
+// * Map Sum Pairs
+
+/*
+Design a map that allows you to do the following:
+
+Maps a string key to a given value.
+Returns the sum of the values that have a key with a prefix equal to a given string.
+Implement the MapSum class:
+
+MapSum() Initializes the MapSum object.
+void insert(String key, int val) Inserts the key-val pair into the map. If the key already existed, the original key-value pair will be overridden to the new one.
+int sum(string prefix) Returns the sum of all the pairs' value whose key starts with the prefix.
+*/
+
+// TIP: This problem can also be solved with a Trie but it is much simpler to use a Map
+
+/**
+ * @typedef {Object} MapSum
+ */
+
 class MapSum {
   constructor() {
     this.map = {}
   }
 
+  /**
+   * @param {string} key
+   * @param {number} val
+   * @return {void}
+   */
+
+  // O(1)
   insert(key, val) {
     this.map[key] = val
   }
 
+  /**
+   * @param {string} prefix
+   * @return {number}
+   */
+
+  // O(n)
   sum(prefix) {
     let sum = 0
 
