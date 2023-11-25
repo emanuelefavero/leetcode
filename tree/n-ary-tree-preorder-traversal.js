81e6c4ac3bd9e71adfb5263ab70c290957a91055
emanuelefavero <emafaux89@gmail.com>
2023-11-25 07:51:20 +0100

    add tree/n-ary-tree-preorder-traversal solution 2

--- tree/n-ary-tree-preorder-traversal.js
+++ tree/n-ary-tree-preorder-traversal.js
@@ -29,6 +29,8 @@ class Node {
  * @returns {number[]}
  */
 
+// NOTE: Recursive solution
+
 // O(n) time | O(n) space
 function preorder(root, result = []) {
   if (!root) return result
@@ -43,6 +45,30 @@ function preorder(root, result = []) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// NOTE: Iterative solution
+
+function preorder2(root) {
+  let result = []
+  let stack = [root]
+
+  while (stack.length) {
+    let current = stack.pop()
+    if (!current) continue // we need this to avoid pushing null values into the result array
+
+    result.push(current.val)
+
+    // for preorder, we need to push the children in reverse order so that the first child is at the top of the stack
+    for (let i = current.children.length - 1; i >= 0; i--) {
+      stack.push(current.children[i])
+    }
+  }
+
+  return result
+}
+
+// -----------------------------
 // TESTS
 
 let root = new Node(1)
@@ -50,3 +76,4 @@ root.children = [new Node(3), new Node(2), new Node(4)]
 root.children[0].children = [new Node(5), new Node(6)]
 
 console.log(preorder(root)) // [1, 3, 5, 6, 2, 4]
+console.log(preorder2(root)) // [1, 3, 5, 6, 2, 4]
