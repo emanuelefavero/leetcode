3ac8b7b22b630de41da0482dbe7d4b4a1c3878d1
emanuelefavero <emafaux89@gmail.com>
2023-11-27 12:17:49 +0100

    add tree/search-in-a-binary-search-tree solution 2

--- tree/search-in-a-binary-search-tree.js
+++ tree/search-in-a-binary-search-tree.js
@@ -21,6 +21,21 @@ function searchBST(root, val) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+function searchBST2(root, val) {
+  let current = root
+
+  while (current) {
+    if (current.val === val) return current
+    else if (current.val < val) current = current.right
+    else current = current.left
+  }
+
+  return null
+}
+
+// -----------------------------
 // TESTS
 
 let root = new TreeNode(4)
@@ -29,5 +44,10 @@ root.right = new TreeNode(7)
 root.left.left = new TreeNode(1)
 root.left.right = new TreeNode(3)
 
+// 1
 console.log(searchBST(root, 2)) // [2,1,3]
 console.log(searchBST(root, 5)) // []
+
+// 2
+console.log(searchBST2(root, 2)) // [2,1,3]
+console.log(searchBST2(root, 5)) // []
