8db195db7e68394bf066cf27353e56ba1f83149e
emanuelefavero <emafaux89@gmail.com>
2023-11-27 08:37:50 +0100

    add tree/validate-binary-search-tree

--- tree/validate-binary-search-tree.js
+++ tree/validate-binary-search-tree.js
@@ -9,3 +9,31 @@ The left subtree of a node contains only nodes with keys less than the node's ke
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
 */
+
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val || 0
+    this.left = left || null
+    this.right = right || null
+  }
+}
+
+function isValidBST(root, min = -Infinity, max = Infinity) {
+  if (!root) return true
+
+  if (root.val <= min || root.val >= max) return false
+
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  )
+}
+
+// -----------------------------
+// TESTS
+
+let root = new TreeNode(2)
+root.left = new TreeNode(1)
+root.right = new TreeNode(3)
+
+console.log(isValidBST(root)) // true
