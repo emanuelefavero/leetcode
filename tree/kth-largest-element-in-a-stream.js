eda1f10daa92a9a4f6da302dd8ea4af92c00a30b
emanuelefavero <emafaux89@gmail.com>
2023-11-29 05:52:44 +0100

    add tree/kth-largest-element-in-a-stream jsDoc

--- tree/kth-largest-element-in-a-stream.js
+++ tree/kth-largest-element-in-a-stream.js
@@ -9,12 +9,26 @@ KthLargest(int k, int[] nums) Initializes the object with the integer k and the 
 int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
 */
 
+// TIP: This is a brute force solution. The optimal solution uses a min heap. (@see https://leetcode.com/problems/kth-largest-element-in-a-stream/solutions)
+
+/**
+ * @typedef {Object} KthLargest
+ * @property {number} k
+ * @property {number[]} nums
+ */
+
 class KthLargest {
   constructor(k, nums) {
     this.k = k
     this.nums = nums
   }
 
+  /**
+   * @param {number} val
+   * @returns {number}
+   */
+
+  // O(n log n) time | O(n) space
   add(val) {
     this.nums.push(val)
     return this.nums.sort((a, b) => b - a)[this.k - 1]
@@ -24,6 +38,7 @@ class KthLargest {
 // -----------------------------
 // TESTS
 
+// 1
 let kthLargest = new KthLargest(3, [4, 5, 8, 2])
 console.log(kthLargest.add(3)) // 4
 console.log(kthLargest.add(5)) // 5, since [2,4,5,5,8] is the stream at this point
