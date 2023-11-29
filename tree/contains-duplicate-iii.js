4c7336d6c6ec14ab1c2d23c2563ea8553a1abf6d
emanuelefavero <emafaux89@gmail.com>
2023-11-29 09:37:55 +0100

    add tree/contains-duplicate-iii solution 2

--- tree/contains-duplicate-iii.js
+++ tree/contains-duplicate-iii.js
@@ -23,8 +23,51 @@ function containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: This is a more efficient solution using bucket sort (it also can be solved using BST, but the code is more complex)
+// TIP: Bucket sort, also known as bin sort, is a sorting algorithm that divides an array's elements into several buckets. The buckets are then sorted one at a time, either using a different sorting algorithm or by recursively applying the bucket sorting algorithm
+// @see https://www.geeksforgeeks.org/bucket-sort-2
+
+function containsNearbyAlmostDuplicate2(nums, indexDiff, valueDiff) {
+  let buckets = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    // get bucket number
+    let bucket = Math.floor(nums[i] / (valueDiff + 1))
+
+    // check if bucket already exists
+    if (buckets.has(bucket)) return true
+
+    // check if adjacent buckets have values
+    if (
+      buckets.has(bucket - 1) &&
+      Math.abs(nums[i] - buckets.get(bucket - 1)) <= valueDiff
+    )
+      return true
+
+    // check if adjacent buckets have values
+    if (
+      buckets.has(bucket + 1) &&
+      Math.abs(nums[i] - buckets.get(bucket + 1)) <= valueDiff
+    )
+      return true
+
+    // add value to bucket
+    buckets.set(bucket, nums[i])
+
+    // remove value from bucket if it's out of range
+    if (i >= indexDiff)
+      buckets.delete(Math.floor(nums[i - indexDiff] / (valueDiff + 1)))
+  }
+
+  return false
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 console.log(containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0)) // true
 /*
 EXPLANATION: 
@@ -37,3 +80,7 @@ abs(nums[i] - nums[j]) <= valueDiff --> abs(1 - 1) <= 0
 
 console.log(containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3)) // false
 // EXPLANATION: After trying all the possible pairs (i, j), we cannot satisfy the three conditions, so we return false.
+
+// 2
+console.log(containsNearbyAlmostDuplicate2([1, 2, 3, 1], 3, 0)) // true
+console.log(containsNearbyAlmostDuplicate2([1, 5, 9, 1, 5, 9], 2, 3)) // false
