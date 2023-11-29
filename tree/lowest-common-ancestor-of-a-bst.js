7999c1f52fbdd9840e4d2927bf4f62deae8eb901
emanuelefavero <emafaux89@gmail.com>
2023-11-29 07:42:12 +0100

    add tree/lowest-common-ancestor-of-a-bst solution

--- tree/lowest-common-ancestor-of-a-bst.js
+++ tree/lowest-common-ancestor-of-a-bst.js
@@ -12,6 +12,16 @@ class TreeNode {
   }
 }
 
+function lowestCommonAncestor(root, p, q) {
+  if (!root) return null
+
+  if (p.val < root.val && q.val < root.val)
+    return lowestCommonAncestor(root.left, p, q)
+  else if (p.val > root.val && q.val > root.val)
+    return lowestCommonAncestor(root.right, p, q)
+  else return root
+}
+
 // -----------------------------
 // TESTS
 
