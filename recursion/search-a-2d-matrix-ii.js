e8e6ed9394f529fc6ae7274d4f440139aca85030
emanuelefavero <emafaux89@gmail.com>
2023-12-01 02:28:40 +0100

    add recursion/search-a-2d-matrix-ii

--- recursion/search-a-2d-matrix-ii.js
+++ recursion/search-a-2d-matrix-ii.js
@@ -4,3 +4,51 @@
 
 // Integers in each row are sorted in ascending from left to right.
 // Integers in each column are sorted in ascending from top to bottom.
+
+// TIP: This solution uses a binary search approach
+
+function searchMatrix(matrix, target) {
+  let row = 0
+  let column = matrix[0].length - 1
+
+  while (row < matrix.length && column >= 0) {
+    if (matrix[row][column] === target) return true
+
+    if (matrix[row][column] < target) {
+      row++
+    } else {
+      column--
+    }
+  }
+
+  return false
+}
+
+// -----------------------------
+// TESTS
+
+console.log(
+  searchMatrix(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    5
+  )
+) // true
+
+console.log(
+  searchMatrix(
+    [
+      [1, 4, 7, 11, 15],
+      [2, 5, 8, 12, 19],
+      [3, 6, 9, 16, 22],
+      [10, 13, 14, 17, 24],
+      [18, 21, 23, 26, 30],
+    ],
+    20
+  )
+) // false
