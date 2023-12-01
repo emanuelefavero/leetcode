129b54feea4f0cb763cd3f288785ec314c652e74
emanuelefavero <emafaux89@gmail.com>
2023-12-01 08:21:42 +0100

    add recursion/n-queens-ii jsDoc

--- recursion/n-queens-ii.js
+++ recursion/n-queens-ii.js
@@ -4,10 +4,19 @@
 
 // Given an integer n, return the number of distinct solutions to the n-queens puzzle.
 
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+
+// TIP: This solution uses a backtracking approach @see https://leetcode.com/explore/learn/card/recursion-ii/472/backtracking/2654
+
+// O(n!) time - O(n) space
 function totalNQueens(n) {
   let occupiedColumns = new Set()
   let positiveDiagonals = new Set()
   let negativeDiagonals = new Set()
+
   let solutionCount = 0
 
   function computePositionForRow(row) {
