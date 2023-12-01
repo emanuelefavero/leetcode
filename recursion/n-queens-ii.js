6bdb22ebaf3916a34ab7d4538816c106d82b15b6
emanuelefavero <emafaux89@gmail.com>
2023-12-01 07:52:53 +0100

    add recursion/n-queens-ii

--- recursion/n-queens-ii.js
+++ recursion/n-queens-ii.js
@@ -3,3 +3,47 @@
 // The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
 
 // Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+function totalNQueens(n) {
+  let occupiedColumns = new Set()
+  let positiveDiagonals = new Set()
+  let negativeDiagonals = new Set()
+  let solutionCount = 0
+
+  function computePositionForRow(row) {
+    if (row === n) {
+      solutionCount++
+      return
+    }
+
+    for (let col = 0; col < n; col++) {
+      if (
+        occupiedColumns.has(col) ||
+        positiveDiagonals.has(row + col) ||
+        negativeDiagonals.has(row - col)
+      ) {
+        continue
+      }
+
+      occupiedColumns.add(col)
+      positiveDiagonals.add(row + col)
+      negativeDiagonals.add(row - col)
+
+      computePositionForRow(row + 1)
+
+      occupiedColumns.delete(col)
+      positiveDiagonals.delete(row + col)
+      negativeDiagonals.delete(row - col)
+    }
+  }
+
+  computePositionForRow(0)
+
+  return solutionCount
+}
+
+// -----------------------------
+// TESTS
+
+console.log(totalNQueens(4)) // 2
+console.log(totalNQueens(1)) // 1
