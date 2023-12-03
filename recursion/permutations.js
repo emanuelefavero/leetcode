0ed566043bfcbe85b65ebf6ec1a93e54fc1f9757
emanuelefavero <emafaux89@gmail.com>
2023-12-03 09:25:30 +0100

    add recursion/permutations jsDoc

--- recursion/permutations.js
+++ recursion/permutations.js
@@ -2,6 +2,12 @@
 
 // Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order
 
+/**
+ * @param {number[]} nums
+ * @returns {number[][]}
+ */
+
+// O(n!) time (factorial) - O(n) space
 function permute(nums) {
   let result = []
 
