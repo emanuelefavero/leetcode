b8d93bf51119cc4fe031811e9b5520eeb2a42bbe
emanuelefavero <emafaux89@gmail.com>
2023-12-05 00:02:54 +0100

    add recursion/the-skyline-problem solution

--- recursion/the-skyline-problem.js
+++ recursion/the-skyline-problem.js
@@ -15,7 +15,38 @@ The skyline should be represented as a list of "key points" sorted by their x-co
 Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...,[2 3],[4 5],[7 5],[11 5],[12 7],...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...,[2 3],[4 5],[12 7],...]
 */
 
-function getSkyline(buildings) {}
+// TODO jsDoc
+
+function getSkyline(buildings) {
+  let heights = []
+
+  for (let [start, end, height] of buildings) {
+    heights.push([start, 0 - height])
+    heights.push([end, height])
+  }
+
+  // sort heights
+  heights.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]))
+
+  let result = []
+  let maxHeap = [0]
+  let maxHeight = 0
+
+  // loop through heights
+  for (let [pos, height] of heights) {
+    if (height < 0) maxHeap.push(0 - height)
+    else maxHeap.splice(maxHeap.indexOf(height), 1)
+
+    let currentMaxHeight = Math.max(...maxHeap)
+
+    if (currentMaxHeight !== maxHeight) {
+      result.push([pos, currentMaxHeight])
+      maxHeight = currentMaxHeight
+    }
+  }
+
+  return result
+}
 
 // -----------------------------
 // TESTS
