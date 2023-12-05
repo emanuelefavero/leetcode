3151f3f1e183e19187100c1e5130c53a0ba9366a
emanuelefavero <emafaux89@gmail.com>
2023-12-05 07:12:37 +0100

    add dynamic-programming/counting-bits jsDoc

--- dynamic-programming/counting-bits.js
+++ dynamic-programming/counting-bits.js
@@ -1,7 +1,15 @@
-// * Counting Bits
+/**
+ * * Counting Bits
+ *
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i
+ *
+ * @param {number} n
+ * @returns {number[]}
+ */
 
-// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+// TIP: This is not a dynamic programming problem but it os one of the top answers on LeetCode
 
+// O(n) time | O(n) space
 function countBits(n) {
   let ans = []
 
@@ -22,5 +30,6 @@ function countBits(n) {
 // -----------------------------
 // TESTS
 
+// 1
 console.log(countBits(2)) // [0,1,1]
 console.log(countBits(5)) // [0,1,1,2,1,2]
