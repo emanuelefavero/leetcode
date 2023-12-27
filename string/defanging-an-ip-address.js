c9904005cd6763e22e037379769a27afcd2999d0
emanuelefavero <emafaux89@gmail.com>
2023-12-27 16:34:50 +0100

    add string/defanging-an-ip-address

---
+++ string/defanging-an-ip-address.js
@@ -0,0 +1,15 @@
+// * Defanging an IP Address
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+function defangIPaddr(address) {
+  return address.replace(/\./g, '[.]')
+}
+
+// -----------------------------
+// TESTS
+
+console.log(defangIPaddr('1.1.1.1')) // '1[.]1[.]1[.]1'
+console.log(defangIPaddr('255.100.50.0')) // '255[.]100[.]50[.]0'
