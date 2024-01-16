c6d331633c47fe5bb75716306e0400b31cea1cb0
emanuelefavero <emafaux89@gmail.com>
2024-01-16 05:20:35 +0100

    add array/build-array-from-permutation jsDoc

--- array/build-array-from-permutation.js
+++ array/build-array-from-permutation.js
@@ -1,3 +1,15 @@
+// * Build Array from Permutation
+
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+/**
+ * @param {number[]} nums
+ * @returns {number[]}
+ */
+
+// O(n) time | O(n) space
 function buildArray(nums) {
   let result = []
 
