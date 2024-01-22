b61258764369226ac0ccf1f675a6beb7e70e52e9
emanuelefavero <emafaux89@gmail.com>
2024-01-22 22:10:32 +0100

    add string/split-a-string-in-balanced-strings solution 2

--- string/split-a-string-in-balanced-strings.js
+++ string/split-a-string-in-balanced-strings.js
@@ -11,6 +11,23 @@ Return the maximum number of balanced strings you can obtain.
 
 function balancedStringSplit(s) {
   let count = 0
+  let balanced = 0
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'L') balanced++
+    else balanced--
+
+    if (balanced === 0) count++
+  }
+
+  return count
+}
+
+// ---------------------------------------------------
+// SOLUTION 2
+
+function balancedStringSplit2(s) {
+  let count = 0
   let l = 0
   let r = 0
 
@@ -27,10 +44,13 @@ function balancedStringSplit(s) {
 // ---------------------------------------------------
 // TESTS
 
+// 1
 console.log(balancedStringSplit('RLRRLLRLRL')) // 4
 // Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
 console.log(balancedStringSplit('RLRRRLLRLL')) // 2
 // Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
 // Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
 
-console.log(balancedStringSplit('LLLLRRRR')) // 1
+// 2
+console.log(balancedStringSplit2('RLRRLLRLRL')) // 4
+console.log(balancedStringSplit2('RLRRRLLRLL')) // 2
