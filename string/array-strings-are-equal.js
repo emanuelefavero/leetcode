1a6e94237e4aeb6cb42005f1647453f930d43edf
emanuelefavero <emafaux89@gmail.com>
2024-01-26 18:10:11 +0100

    add string/array-strings-are-equal solution 2

--- string/array-strings-are-equal.js
+++ string/array-strings-are-equal.js
@@ -11,6 +11,29 @@ function arrayStringsAreEqual(word1, word2) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+function arrayStringsAreEqual2(word1, word2) {
+  let string1 = ''
+  let string2 = ''
+
+  for (s of word1) {
+    for (char of s) {
+      string1 += char
+    }
+  }
+
+  for (s of word2) {
+    for (char of s) {
+      string2 += char
+    }
+  }
+
+  return string1 === string2
+}
+
+// -----------------------------
 // TESTS
 
-console.log(arrayStringsAreEqual(['ab', 'c'], ['a', 'bc']))
+console.log(arrayStringsAreEqual(['ab', 'c'], ['a', 'bc'])) // true
+console.log(arrayStringsAreEqual2(['ab', 'c'], ['a', 'bc'])) // true
