847a3acf72d34b72dc5872d15e95ec491370a98d
emanuelefavero <emafaux89@gmail.com>
2024-01-26 18:17:59 +0100

    add string/array-strings-are-equal jsDoc

--- string/array-strings-are-equal.js
+++ string/array-strings-are-equal.js
@@ -1,11 +1,18 @@
-// * Check if Two String Arrays are Equivalent
-
-// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
-
-// A string is represented by an array if the array elements concatenated in order forms the string.
+/**
+ * * Check if Two String Arrays are Equivalent
+ *
+ * Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise
+ *
+ * A string is represented by an array if the array elements concatenated in order forms the string
+ *
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @returns {boolean}
+ */
 
 // TIP: This solution uses JavaScript built in methods
 
+// O(n) time | O(n) space
 function arrayStringsAreEqual(word1, word2) {
   return word1.join('') === word2.join('')
 }
@@ -13,6 +20,7 @@ function arrayStringsAreEqual(word1, word2) {
 // -----------------------------
 // SOLUTION 2
 
+// O(n) time | O(n) space
 function arrayStringsAreEqual2(word1, word2) {
   let string1 = ''
   let string2 = ''
