1d0bdae02411063dba3f60189cad95219f53cdd0
emanuelefavero <emafaux89@gmail.com>
2024-01-30 04:19:51 +0100

    add hash-table/number-of-good-pairs solution 2

--- hash-table/number-of-good-pairs.js
+++ hash-table/number-of-good-pairs.js
@@ -14,5 +14,34 @@ function numIdenticalPairs(nums) {
   return count
 }
 
+// -----------------------------
+// SOLUTION 2
+
+// TIP: This solution uses an hash table
+
+function numIdenticalPairs2(nums) {
+  let count = 0
+  let hash = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    // if the number is already in the hash table, add the value to the count
+    if (hash[nums[i]]) {
+      count += hash[nums[i]]
+      hash[nums[i]]++
+    } else {
+      hash[nums[i]] = 1
+    }
+  }
+
+  return count
+}
+
+// -----------------------------
+// TESTS
+
+// 1
 console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3])) // 4
 // Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+// 2
+console.log(numIdenticalPairs2([1, 2, 3, 1, 1, 3])) // 4
