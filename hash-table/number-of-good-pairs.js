31d5be85872dc089e5dff9bc74785ef8c198422b
emanuelefavero <emafaux89@gmail.com>
2024-01-30 04:14:35 +0100

    add hash-table/number-of-good-pairs

---
+++ hash-table/number-of-good-pairs.js
@@ -0,0 +1,18 @@
+// * Number of Good Pairs
+
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+function numIdenticalPairs(nums) {
+  let count = 0
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) count++
+    }
+  }
+  return count
+}
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3])) // 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
