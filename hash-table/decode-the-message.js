6ebd3408f7b6507b00aef82c6af5ffe5f863f4c5
emanuelefavero <emafaux89@gmail.com>
2024-02-06 10:06:43 +0100

    add hash-table/decode-the-message jsDoc

--- hash-table/decode-the-message.js
+++ hash-table/decode-the-message.js
@@ -1,3 +1,23 @@
+// * Decode the Message
+
+/*
+You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+Align the substitution table with the regular English alphabet.
+Each letter in message is then substituted using the table.
+Spaces ' ' are transformed to themselves.
+For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+Return the decoded message.
+*/
+
+/**
+ * @param {string} key - The key used for decoding the message
+ * @param {string} message - The message to be decoded
+ * @returns {string} - The decoded message
+ */
+
+// O(n) time | O(1) space
 function decodeMessage(key, message) {
   let result = ''
 
@@ -20,6 +40,9 @@ function decodeMessage(key, message) {
 
   return result
 }
+
+// ------------------------------------------
+// TESTS
 
 console.log(
   decodeMessage(
