4f56f8cd22bd9d5b7f2abfc00c8bd12176bdac96
emanuelefavero <emafaux89@gmail.com>
2024-02-12 20:05:34 +0100

    add dynamic-programming/tribonacci jsDoc

--- dynamic-programming/n-th-tribonacci-number.js
+++ dynamic-programming/n-th-tribonacci-number.js
@@ -1,13 +1,17 @@
-// *  N-th Tribonacci Number
-
 // The Tribonacci sequence Tn is defined as follows:
-
 // T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0
-
 // Given n, return the value of Tn
+
+/**
+ * *  N-th Tribonacci Number
+ *
+ * @param {number} n
+ * @returns {number}
+ */
 
 // NOTE: Do not use this solution for large values of n, it's not efficient
 
+// O(3^n) time | O(n) space
 function tribonacci(n) {
   if (n === 2) return 1
   if (n <= 1) return n
