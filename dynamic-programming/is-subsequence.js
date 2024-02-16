9f60e683cb45e1194b48dc4884d40ecb3e59b71c
emanuelefavero <emafaux89@gmail.com>
2024-02-16 06:14:11 +0100

    add dynamic-programming/is-subsequence solution

--- dynamic-programming/is-subsequence.js
+++ dynamic-programming/is-subsequence.js
@@ -4,9 +4,22 @@
 
 // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-// TODO write solution
+// TIP: This solution uses two pointers to iterate over the strings and compare the characters. At the end of the loop, if the first pointer is equal to the length of the first string, it means that all the characters were found in the second string in the same order
 
-function isSubsequence(s, t) {}
+function isSubsequence(s, t) {
+  let i = 0
+  let j = 0
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++ // Only increment the first pointer if the characters are equal
+    }
+
+    j++ // Always increment the second pointer
+  }
+
+  return i === s.length
+}
 
 // -----------------------------
 // TESTS
