3879a1b1c02b1f4c507f8024fdd825f7692d5ef5
emanuelefavero <emafaux89@gmail.com>
2024-02-20 08:59:05 +0100

    add math/find-the-maximum-achievable-number solution 2

--- math/find-maximum-achievable-number.js
+++ math/find-maximum-achievable-number.js
@@ -22,9 +22,28 @@ function theMaximumAchievableX(num, t) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// O(t) time | O(1) space
+function theMaximumAchievableX2(num, t) {
+  let x = t
+
+  while (t > 0) {
+    num++
+    t--
+  }
+
+  num += x
+
+  return num
+}
+
+// -------------------------
 // TESTS
 
 console.log(theMaximumAchievableX(4, 1)) // 6
 // Explanation: The maximum achievable number is x = 6; it can become equal to num after performing this operation:
 // 1- Decrease x by 1, and increase num by 1. Now, x = 5 and num = 5.
 // It can be proven that there is no achievable number larger than 6.
+
+console.log(theMaximumAchievableX2(4, 1)) // 6
