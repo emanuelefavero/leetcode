c0d5b01d5c17bb1e17dfcb34d36234da2f1788db
emanuelefavero <emafaux89@gmail.com>
2024-02-28 07:10:24 +0100

    add sorting/count-pairs

---
+++ sorting/count-pairs.js
@@ -0,0 +1,24 @@
+// * COunt Pairs Whose Sum is Less than Target
+
+// Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+
+// TIP: This solution does not use sorting, but is easier to understand and implement
+
+function countPairs(nums, target) {
+  let count = 0
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] < target) {
+        count++
+      }
+    }
+  }
+  return count
+}
+
+// -----------------------
+// TESTS
+
+console.log(countPairs([-1, 1, 2, 3, 1], 2))
+console.log(countPairs([-6, 2, 5, -2, -7, -1, 3], -2))
