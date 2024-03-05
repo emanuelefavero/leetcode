9ed2136edf73d06ab61ab0cd8971c736ffbad5cd
emanuelefavero <emafaux89@gmail.com>
2024-03-05 03:34:17 +0100

    add sorting/max-product-difference

---
+++ sorting/max-product-difference.js
@@ -0,0 +1,21 @@
+// * Maximum Product Difference Between Two Pairs
+
+/*
+The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+
+For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+
+Return the maximum such product difference.
+*/
+
+function maxProductDifference(nums) {
+  nums.sort((a, b) => a - b)
+
+  return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1]
+}
+
+// -----------------------
+// TESTS
+
+console.log(maxProductDifference([5, 6, 2, 7, 4])) // 34
