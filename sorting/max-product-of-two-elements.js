eaa001374b907622b1c144584c68ed96c5d2e02d
emanuelefavero <emafaux89@gmail.com>
2024-03-06 15:46:14 +0100

    add sorting/max-product-of-two-elements jsDoc

--- sorting/max-product-of-two-elements.js
+++ sorting/max-product-of-two-elements.js
@@ -1,7 +1,13 @@
-// * Maximum Product of Two Elements in an Array
+/**
+ * * Maximum Product of Two Elements in an Array
+ *
+ * Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1)
+ *
+ * @param {number[]} nums
+ * @returns {number}
+ */
 
-// Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1)
-
+// O(n log n) time | O(1) space
 function maxProduct(nums) {
   nums.sort((a, b) => a - b)
 
