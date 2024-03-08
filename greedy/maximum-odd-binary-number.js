f24d01d82709e0a5819b693d0891954a3ff96c04
emanuelefavero <emafaux89@gmail.com>
2024-03-08 21:08:29 +0100

    add greedy/maximum-odd-binary-number jsDoc

--- greedy/maximum-odd-binary-number.js
+++ greedy/maximum-odd-binary-number.js
@@ -12,6 +12,12 @@ Note that the resulting string can have leading zeros.
 
 // TIP: We need to put all ones to the left and all zeros to the right except one 1 at the end (e.g. 110001)
 
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+
+// O(n) time | O(1) space
 function maximumOddBinaryNumber(s) {
   let count1 = 0
   let count0 = 0
@@ -27,5 +33,6 @@ function maximumOddBinaryNumber(s) {
 // -----------------------------
 // TESTS
 
+// 1
 console.log(maximumOddBinaryNumber('010'))
 console.log(maximumOddBinaryNumber('0101'))
