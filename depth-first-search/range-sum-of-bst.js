9566d4bb9afccf85cb77829af1379c20b7ac51bf
emanuelefavero <emafaux89@gmail.com>
2024-03-19 11:28:20 +0100

    add depth-first-search/range-sum-of-bst solution 2

--- depth-first-search/range-sum-of-bst.js
+++ depth-first-search/range-sum-of-bst.js
@@ -47,6 +47,27 @@ function rangeSumBST(root, low, high, sum = 0) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+function rangeSumBST2(root, low, high) {
+  let set = new Set()
+
+  const helper = (node) => {
+    if (!node) return false
+    helper(node.left)
+    helper(node.right)
+
+    if (node.val >= low && node.val <= high) {
+      set.add(node.val)
+    }
+  }
+
+  helper(root)
+
+  return [...set].reduce((acc, curr) => acc + curr, 0)
+}
+
+// -------------------------
 // TESTS
 
 let root = new TreeNode(10)
@@ -56,4 +77,7 @@ root.left.left = new TreeNode(3)
 root.left.right = new TreeNode(7)
 root.right.right = new TreeNode(18)
 
-console.log(rangeSumBST(root, 7, 15))
+console.log(rangeSumBST(root, 7, 15)) // 32
+console.log(rangeSumBST2(root, 7, 15)) // 32
+
+// Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
