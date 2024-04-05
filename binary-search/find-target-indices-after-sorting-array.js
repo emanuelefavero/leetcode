c10e8f1c04280bfdc497d34ffa45e190f199b8a8
emanuelefavero <emafaux89@gmail.com>
2024-04-05 23:27:46 +0200

    add binary-search/find-target-indices-after-sorting-array jsDoc

--- binary-search/find-target-indices-after-sorting-array.js
+++ binary-search/find-target-indices-after-sorting-array.js
@@ -8,6 +8,13 @@ A target index is an index i such that nums[i] == target.
 Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number[]}
+ */
+
+// O(nlogn) time complexity
 function targetIndices(nums, target) {
   nums.sort((a, b) => a - b)
 
