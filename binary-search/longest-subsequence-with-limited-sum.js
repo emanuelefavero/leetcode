1fa95db647a8c5b2f4292b07cc8dc6b0a44e0cc4
emanuelefavero <emafaux89@gmail.com>
2024-04-06 23:04:22 +0200

    add binary-search/longest-subsequence-with-limited-sum

---
+++ binary-search/longest-subsequence-with-limited-sum.js
@@ -0,0 +1,38 @@
+// * Longest Subsequence With Limited Sum
+
+/*
+You are given an integer array nums of length n, and an integer array queries of length m.
+
+Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+*/
+
+// TIP: This is not a binary search solution but is the most simple and common
+
+function answerQueries(nums, queries) {
+  nums.sort((a, b) => a - b)
+
+  let result = []
+
+  for (let query of queries) {
+    let count = 0
+    let sum = 0
+
+    for (let i = 0; i < nums.length; i++) {
+      if (sum + nums[i] <= query) {
+        sum += nums[i]
+        count++
+      }
+    }
+
+    result.push(count)
+  }
+
+  return result
+}
+
+// ---------------------
+// TESTS
+
+console.log(answerQueries([4, 5, 2, 1], [3, 10, 21])) // [2, 3, 4]
