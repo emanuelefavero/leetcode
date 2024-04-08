aab13a72dcd9a0596e958ed5dceae5c29f71ba40
emanuelefavero <emafaux89@gmail.com>
2024-04-08 17:05:06 +0200

    add binary-search/find-the-distance-value jsDoc

--- binary-search/find-the-distance-value.js
+++ binary-search/find-the-distance-value.js
@@ -6,8 +6,16 @@ Given two integer arrays arr1 and arr2, and the integer d, return the distance v
 The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| < d.
 */
 
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @returns {number}
+ */
+
 // TIP: This problem can also be solved with binary search but is not common to do so
 
+// O(n^2) time | O(1) space
 function findTheDistanceValue(arr1, arr2, d) {
   let count = 0
 
