0727a184499aa8c33c2f3b022dcd62ab81615bd5
emanuelefavero <emafaux89@gmail.com>
2024-04-14 18:25:23 +0200

    add array/shuffle-the-array jsDoc

--- array/shuffle-the-array.js
+++ array/shuffle-the-array.js
@@ -6,6 +6,13 @@ Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,.
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @returns {number[]}
+ */
+
+// O(n) time | O(n) space
 function shuffle(nums, n) {
   let left = 0
   let right = n
@@ -24,6 +31,7 @@ function shuffle(nums, n) {
 // -------------------------
 // SOLUTION 2
 
+// O(n) time | O(n) space
 function shuffle2(nums, n) {
   let result = []
 
