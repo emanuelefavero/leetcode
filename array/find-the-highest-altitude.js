f04b841ba2664c8c9436279ac3633b1ec7d883b5
emanuelefavero <emafaux89@gmail.com>
2024-05-06 13:46:30 +0200

    add array/find-the-highest-altitude jsDoc

--- array/find-the-highest-altitude.js
+++ array/find-the-highest-altitude.js
@@ -6,6 +6,12 @@ There is a biker going on a road trip. The road trip consists of n + 1 points at
 You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
 */
 
+/**
+ * @param {number[]} gain
+ * @returns {number}
+ */
+
+// O(n) time | O(1) space
 function largestAltitude(gain) {
   let sum = 0
   let max = 0
