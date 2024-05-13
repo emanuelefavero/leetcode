507d31931b0c02931117c72616986846e3b48e7e
emanuelefavero <emafaux89@gmail.com>
2024-05-13 12:08:11 +0200

    add 2d-array/matrix-diagonal-sum

---
+++ 2d-array/matrix-diagonal-sum.js
@@ -0,0 +1,31 @@
+// * Matrix Diagonal Sum
+
+// Given a square matrix mat, return the sum of the matrix diagonals.
+
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+function diagonalSum(mat) {
+  let sum = 0
+
+  for (let i = 0; i < mat.length; i++) {
+    sum += mat[i][i]
+    if (i !== mat.length - 1 - i) {
+      sum += mat[i][mat.length - 1 - i]
+    }
+  }
+
+  return sum
+}
+
+// -------------------------
+// TESTS
+
+console.log(
+  diagonalSum([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+) // 25
+// Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+// Notice that element mat[1][1] = 5 is counted only once.
