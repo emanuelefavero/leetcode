3db4a6e2a4118b7515a240f1f258f4dc8e8fd51e
emanuelefavero <emafaux89@gmail.com>
2024-05-15 00:04:02 +0200

    refactor array/sum-odd-length-subarrays

--- array/sum-odd-length-subarrays.js
+++ array/sum-odd-length-subarrays.js
@@ -10,10 +10,11 @@ function sumOddLengthSubarrays(arr) {
   let sum = 0
 
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j += 2) {
-      for (let k = i; k <= j; k++) {
-        sum += arr[k]
-      }
+    let currentSum = 0
+    for (let j = i; j < arr.length; j++) {
+      currentSum += arr[j]
+
+      if ((j - i) % 2 === 0) sum += currentSum
     }
   }
 
