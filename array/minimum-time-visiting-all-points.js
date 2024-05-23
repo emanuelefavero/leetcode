61cf2623c7a1eca819100c4b2c13f6d4a41f3b77
emanuelefavero <emafaux89@gmail.com>
2024-05-23 23:05:45 +0200

    add array/minimum-time-visiting-all-points

---
+++ array/minimum-time-visiting-all-points.js
@@ -0,0 +1,44 @@
+// * Minimum Time Visiting All Points
+
+/*
+On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum result in seconds to visit all the points in the order given by points.
+
+You can move according to these rules:
+
+In 1 second, you can either:
+move vertically by one unit,
+move horizontally by one unit, or
+move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+You have to visit the points in the same order as they appear in the array.
+You are allowed to pass through points that appear later in the order, but these do not count as visits.
+*/
+
+function minTimeToVisitAllPoints(points) {
+  let result = 0
+
+  for (let i = 1; i < points.length; i++) {
+    result += Math.max(
+      Math.abs(points[i][0] - points[i - 1][0]),
+      Math.abs(points[i][1] - points[i - 1][1])
+    )
+  }
+
+  return result
+}
+
+// -------------------------
+// TESTS
+
+console.log(
+  minTimeToVisitAllPoints([
+    [1, 1],
+    [3, 4],
+    [-1, 0],
+  ])
+) // 7
+/*
+Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]   
+Time from [1,1] to [3,4] = 3 seconds 
+Time from [3,4] to [-1,0] = 4 seconds
+Total result = 7 seconds
+*/
