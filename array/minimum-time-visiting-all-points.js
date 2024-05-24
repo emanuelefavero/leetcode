f70d8d8b0f91087cb36aa45a707801d8ce099fb8
emanuelefavero <emafaux89@gmail.com>
2024-05-24 13:57:05 +0200

    refactor array/minimum-time-visiting-all-points

--- array/minimum-time-visiting-all-points.js
+++ array/minimum-time-visiting-all-points.js
@@ -22,11 +22,10 @@ You are allowed to pass through points that appear later in the order, but these
 function minTimeToVisitAllPoints(points) {
   let result = 0
 
-  for (let i = 1; i < points.length; i++) {
-    result += Math.max(
-      Math.abs(points[i][0] - points[i - 1][0]),
-      Math.abs(points[i][1] - points[i - 1][1])
-    )
+  for (let i = 0; i < points.length - 1; i++) {
+    let xPoint = Math.abs(points[i][0] - points[i + 1][0])
+    let yPoint = Math.abs(points[i][1] - points[i + 1][1])
+    result += Math.max(xPoint, yPoint)
   }
 
   return result
