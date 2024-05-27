c2c8a976fb41f7ae5a1d6c2d172296e5e0357cbb
emanuelefavero <emafaux89@gmail.com>
2024-05-27 17:21:40 +0200

    add array/count-good-triplets

---
+++ array/count-good-triplets.js
@@ -0,0 +1,40 @@
+// * Count Good Triplets
+
+/*
+Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+
+Return the number of good triplets.
+*/
+
+function countGoodTriplets(arr, a, b, c) {
+  let count = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (
+          Math.abs(arr[i] - arr[j]) <= a &&
+          Math.abs(arr[j] - arr[k]) <= b &&
+          Math.abs(arr[i] - arr[k]) <= c
+        ) {
+          count++
+        }
+      }
+    }
+  }
+
+  return count
+}
+
+// -----------------------------
+// TESTS
+
+console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3)) // 4
