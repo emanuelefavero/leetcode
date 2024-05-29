cd1dd112be6eedff4260dc94b7d0ef248d7b454b
emanuelefavero <emafaux89@gmail.com>
2024-05-29 20:57:09 +0200

    add array/sort-the-people

---
+++ array/sort-the-people.js
@@ -0,0 +1,31 @@
+// *  Sort the People
+
+/*
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+*/
+
+function sortPeople(names, heights) {
+  let result = []
+
+  for (let i = 0; i < names.length; i++) {
+    let person = { name: names[i], height: heights[i] }
+    result.push(person)
+  }
+
+  result.sort((a, b) => b.height - a.height)
+
+  for (let i = 0; i < result.length; i++) {
+    result[i] = result[i].name
+  }
+
+  return result
+}
+
+// -------------------------
+// TESTS
+
+console.log(sortPeople(['Mary', 'John', 'Emma'], [180, 165, 170]))
