1686a64c2fd4561535678526379e7377110884be
emanuelefavero <emafaux89@gmail.com>
2024-05-30 15:08:38 +0200

    add array/destination-city

---
+++ array/destination-city.js
@@ -0,0 +1,37 @@
+// * Destination City
+
+/*
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+*/
+
+// TIP: The destination city never appears on the left of the array so we will create a set with all left cities and then check which right city does not appear on the left by checking which right city is not found in the set
+
+function destCity(paths) {
+  let set = new Set()
+
+  // Add all left cities to the set
+  for (let i = 0; i < paths.length; i++) {
+    let leftCity = paths[i][0]
+    set.add(leftCity)
+  }
+
+  // Check which right city does not appear on the left
+  for (let i = 0; i < paths.length; i++) {
+    let rightCity = paths[i][1]
+    if (!set.has(rightCity)) return rightCity
+  }
+}
+
+// -----------------------------
+// TESTS
+
+console.log(
+  destCity([
+    ['London', 'New York'],
+    ['New York', 'Lima'],
+    ['Lima', 'Sao Paulo'],
+  ])
+) // Sao Paulo
+// Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
