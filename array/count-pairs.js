5b9ad314e180e30e6e478d9177473759695638ee
emanuelefavero <emafaux89@gmail.com>
2024-05-31 18:06:08 +0200

    add array/count-pairs link jsDoc

--- array/count-pairs.js
+++ array/count-pairs.js
@@ -2,6 +2,14 @@
 
 // Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
 
+/**
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number}
+ */
+
+// O(n^2) time | O(1) space
 function countPairs(nums, k) {
   let count = 0
 
