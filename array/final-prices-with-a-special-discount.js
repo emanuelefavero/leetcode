5840d82c4bb2b9c30d23916f3f39d745e24514fb
emanuelefavero <emafaux89@gmail.com>
2024-06-24 14:16:49 +0200

    add array/final-prices-with-a-special-discount jsDoc

--- array/final-prices-with-a-special-discount.js
+++ array/final-prices-with-a-special-discount.js
@@ -8,6 +8,12 @@ There is a special discount for items in the shop. If you buy the ith item, then
 Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
 */
 
+/**
+ * @param {number[]} prices
+ * @returns {number[]}
+ */
+
+// O(n^2) time | O(n) space
 function finalPrices(prices) {
   let result = []
 
