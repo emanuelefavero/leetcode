6c52494f24b62b8a9025e7fdfd2f04aa0fcdb909
emanuelefavero <emafaux89@gmail.com>
2024-06-27 18:06:32 +0200

    add easy/add-two-promises solution 2

--- easy/add-two-promises.js
+++ easy/add-two-promises.js
@@ -10,11 +10,26 @@ async function addTwoPromises(promise1, promise2) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+async function addTwoPromises2(promise1, promise2) {
+  const [a, b] = await Promise.all([promise1, promise2])
+  return a + b
+}
+
+// -------------------------
 // TESTS
 
+// 1
 addTwoPromises(
   new Promise((resolve) => setTimeout(() => resolve(2), 20)),
   new Promise((resolve) => setTimeout(() => resolve(5), 60))
 ).then(console.log)
 
 // Explanation: The two input promises resolve with the values of 2 and 5 respectively. The returned promise should resolve with a value of 2 + 5 = 7. The time the returned promise resolves is not judged for this problem.
+
+// 2
+addTwoPromises2(
+  new Promise((resolve) => setTimeout(() => resolve(2), 20)),
+  new Promise((resolve) => setTimeout(() => resolve(5), 60))
+).then(console.log)
