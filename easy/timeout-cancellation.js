d1f6601edbbfcb3f86de00e31c864328da1c9515
emanuelefavero <emafaux89@gmail.com>
2024-07-02 18:02:32 +0200

    add easy/timeout-cancellation jsDoc

--- easy/timeout-cancellation.js
+++ easy/timeout-cancellation.js
@@ -11,6 +11,14 @@ Initially, the execution of the function fn should be delayed by t milliseconds.
 If, before the delay of t milliseconds, the function cancelFn is invoked, it should cancel the delayed execution of fn. Otherwise, if cancelFn is not invoked within the specified delay t, fn should be executed with the provided args as arguments.
 */
 
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @returns {Function}
+ */
+
+// O(n) time | O(1) space
 function cancellable(fn, args, t) {
   let timer = setTimeout(() => fn(...args), t)
 
