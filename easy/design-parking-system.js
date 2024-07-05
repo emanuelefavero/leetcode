52474027548ad0ef30286e0f5c59449c7321bd11
emanuelefavero <emafaux89@gmail.com>
2024-07-05 14:52:21 +0200

    add easy/design-parking-system jsDoc

--- easy/design-parking-system.js
+++ easy/design-parking-system.js
@@ -1,3 +1,20 @@
+// * Design Parking System
+
+/*
+Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+Implement the ParkingSystem class:
+
+ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+*/
+
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+
 class ParkingSystem {
   constructor(big, medium, small) {
     this.parking = {
@@ -7,6 +24,12 @@ class ParkingSystem {
     }
   }
 
+  /**
+   * @param {number} carType
+   * @returns {boolean}
+   */
+
+  // O(1) time | O(1) space
   addCar(carType) {
     if (this.parking[carType]) {
       this.parking[carType]--
