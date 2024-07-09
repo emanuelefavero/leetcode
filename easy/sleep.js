576f15d1e3ddedb95d9c79ab5fc22860b03b67ae
emanuelefavero <emafaux89@gmail.com>
2024-07-09 19:33:26 +0200

    add easy/sleep jsDoc

--- easy/sleep.js
+++ easy/sleep.js
@@ -2,6 +2,12 @@
 
 // Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 
+/**
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+
+// O(n) - time | O(1) - space
 async function sleep(ms) {
   return new Promise((res) => setTimeout(res, ms))
 }
