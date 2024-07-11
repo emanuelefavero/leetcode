788aa0ed273a28c277813a79eb1ea0000f018acc
emanuelefavero <emafaux89@gmail.com>
2024-07-11 22:24:07 +0200

    add string/reverse-prefix-of-word

---
+++ string/reverse-prefix-of-word.js
@@ -0,0 +1,34 @@
+// * Reverse Prefix of Word
+
+/*
+Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+Return the resulting string.
+*/
+
+function reverse(string) {
+  return string.split('').reverse().join('')
+}
+
+function reversePrefix(word, ch) {
+  let right = 0
+
+  for (let char of word) {
+    if (char === ch) break
+    right++
+  }
+
+  if (right >= word.length) return word
+
+  return reverse(word.slice(0, right + 1)) + word.slice(right + 1)
+}
+
+// -------------------------
+// TESTS
+
+console.log(reversePrefix('abcdefd', 'd')) // 'dcbaefd'
+// Explanation: The first occurrence of "d" is at index 3.
+// Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+console.log(reversePrefix('abcd', 'z')) // 'abcd'
