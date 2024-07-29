639ee288e1ce6a783c94e0795a7db37ba9f226b6
emanuelefavero <emafaux89@gmail.com>
2024-07-29 19:21:52 +0200

    add string/valid-anagram

---
+++ string/valid-anagram.js
@@ -0,0 +1,30 @@
+// * Valid Anagram
+
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
+
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false
+
+  let map = {}
+
+  for (let char of s) {
+    map[char] = map[char] + 1 || 1
+  }
+
+  for (let char of t) {
+    if (!map[char] || map[char] === 0) return false
+    map[char]--
+  }
+
+  return true
+}
+
+// -------------------------
+// TESTS
+
+console.log(isAnagram('anagram', 'nagaram')) // true
+console.log(isAnagram('rat', 'car')) // false
