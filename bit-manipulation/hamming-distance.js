fae583cee911d331d11e182a1a412039145c07c2
emanuelefavero <emafaux89@gmail.com>
2024-08-06 05:11:33 +0200

    refactor bit-manipulation/hamming-distance

--- bit-manipulation/hamming-distance.js
+++ bit-manipulation/hamming-distance.js
@@ -5,22 +5,8 @@
 // Given two integers x and y, return the Hamming distance between them.
 
 function hammingDistance(x, y) {
-  let xBinary = x.toString(2)
-  let yBinary = y.toString(2)
-  let counter = 0
-
-  for (let i = 0; i < xBinary.length; i++) {
-    let xChar = xBinary[i]
-    let yChar = yBinary[i]
-
-    if (xChar === undefined || yChar === undefined) break
-    if (xChar !== yChar) counter++
-  }
-
-  let lengthDifference = Math.abs(xBinary.length - yBinary.length)
-  counter += lengthDifference
-
-  return counter
+  // TIP: XOR operator (^) will return 1 if the bits are different and 0 if they are the same
+  return (x ^ y).toString(2).replace(/0/g, '').length
 }
 
 // ----------------------
