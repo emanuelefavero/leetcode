2c2387c3be00c4bb3ca27603725428e0895ae85b
emanuelefavero <emafaux89@gmail.com>
2024-08-08 09:31:49 +0200

    add string/merge-strings-alternately solution 2

--- string/merge-strings-alternately.js
+++ string/merge-strings-alternately.js
@@ -22,8 +22,23 @@ function mergeAlternately(word1, word2) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+function mergeAlternately2(word1, word2) {
+  let result = ''
+
+  for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+    if (i < word1.length) result += word1[i]
+    if (i < word2.length) result += word2[i]
+  }
+
+  return result
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(mergeAlternately('abc', 'pqr')) // 'apbqcr'
 /*
 Explanation: The merged string will be merged as so:
@@ -32,3 +47,7 @@ word2:    p   q   r
 merged: a p b q c r
 */
 console.log(mergeAlternately('ab', 'pqrs')) // 'apbqrs'
+
+// 2
+console.log(mergeAlternately2('abc', 'pqr')) // 'apbqcr'
+console.log(mergeAlternately2('ab', 'pqrs')) // 'apbqrs'
