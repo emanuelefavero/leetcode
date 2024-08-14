679fb34e99d261c5a1b25dd20ef15f97fbc977aa
emanuelefavero <emafaux89@gmail.com>
2024-08-14 08:45:34 +0200

    add easy/apply-transform-over-each-element solution 2

--- easy/apply-transform-over-each-element.js
+++ easy/apply-transform-over-each-element.js
@@ -19,8 +19,23 @@ function map(arr, fn) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: Use the built-in Array.map method
+
+function map2(arr, fn) {
+  return arr.map(fn)
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(map([1, 2, 3], (n) => n + 1)) // [2, 3, 4]
 console.log(map([1, 2, 3], (n, i) => n + i)) // [1, 3, 5]
 console.log(map([10, 20, 30], () => 42)) // [42, 42, 42]
+
+// 2
+console.log(map2([1, 2, 3], (n) => n + 1)) // [2, 3, 4]
+console.log(map2([1, 2, 3], (n, i) => n + i)) // [1, 3, 5]
+console.log(map2([10, 20, 30], () => 42)) // [42, 42, 42]
