e924f4378dec92d4da486457d594115c0803f0ad
emanuelefavero <emafaux89@gmail.com>
2024-08-16 06:35:07 +0200

    add array/minimum-average-of-smallest-and-largest

---
+++ array/minimum-average-of-smallest-and-largest.js
@@ -0,0 +1,42 @@
+// * Minimum Average of Smallest and Largest Elements
+
+/*
+You have an array of floating point numbers averages which is initially empty. You are given an array nums of n integers where n is even.
+
+You repeat the following procedure n / 2 times:
+
+Remove the smallest element, minElement, and the largest element maxElement, from nums.
+Add (minElement + maxElement) / 2 to averages.
+Return the minimum element in averages.
+*/
+
+function minimumAverage(nums) {
+  let averages = []
+
+  nums.sort((a, b) => a - b)
+
+  while (nums.length) {
+    averages.push((nums.shift() + nums.pop()) / 2)
+  }
+
+  averages.sort((a, b) => a - b)
+
+  return averages[0]
+}
+
+// -----------------------
+// TESTS
+
+console.log(minimumAverage([7, 8, 3, 4, 15, 13, 4, 1])) // 5.5
+/*
+Explanation:
+
+step	nums	              averages
+0	    [7,8,3,4,15,13,4,1]	[]
+1	    [7,8,3,4,13,4]	    [8]
+2	    [7,8,4,4]	          [8,8]
+3   	[7,4]	              [8,8,6]
+4	    []	                [8,8,6,5.5]
+
+The smallest element of averages, 5.5, is returned.
+*/
