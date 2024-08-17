425728ab5772c142369f897ddc3be57aac83bf16
emanuelefavero <emafaux89@gmail.com>
2024-08-17 19:31:58 +0200

    add math/count-the-digits-that-divide-a-number solution 2

--- math/count-the-digits-that-divide-a-number.js
+++ math/count-the-digits-that-divide-a-number.js
@@ -15,9 +15,28 @@ function countDigits(num) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+function countDigits2(num) {
+  let str = num.toString()
+  let count = 0
+
+  for (let i = 0; i < str.length; i++) {
+    let digit = parseInt(str[i])
+    if (num % digit === 0) count++
+  }
+
+  return count
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(countDigits(7)) // 1
 // Explanation: 7 divides itself, hence the answer is 1.
 console.log(countDigits(121)) // 2
 console.log(countDigits(1248)) // 4
+
+// 2
+console.log(countDigits2(7)) // 1
