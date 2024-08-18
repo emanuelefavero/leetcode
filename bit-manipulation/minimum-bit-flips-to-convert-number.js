8a93fc501c03300d69146399c7650b4fc62237b4
emanuelefavero <emafaux89@gmail.com>
2024-08-19 01:42:25 +0200

    add bit-manipulation/minimum-bit-flips-to-convert-number

---
+++ bit-manipulation/minimum-bit-flips-to-convert-number.js
@@ -0,0 +1,26 @@
+// * Minimum Bit Flips to Convert Number
+
+/*
+A bit flip of a number x is choosing a bit in the binary representation of x and flipping it from either 0 to 1 or 1 to 0.
+
+For example, for x = 7, the binary representation is 111 and we may choose any bit (including any leading zeros not shown) and flip it. We can flip the first bit from the right to get 110, flip the second bit from the right to get 101, flip the fifth bit from the right (a leading zero) to get 10111, etc.
+Given two integers start and goal, return the minimum number of bit flips to convert start to goal.
+*/
+
+// TIP: XOR (^) operator to find the difference between two numbers, then count the number of 1s in the result
+
+function minBitFlips(start, goal) {
+  return (start ^ goal).toString(2).replace(/0/g, '').length
+}
+
+// ----------------------
+// TESTS
+
+console.log(minBitFlips(10, 7)) // 3
+/*
+Explanation: The binary representation of 10 and 7 are 1010 and 0111 respectively. We can convert 10 to 7 in 3 steps:
+- Flip the first bit from the right: 1010 -> 1011.
+- Flip the third bit from the right: 1011 -> 1111.
+- Flip the fourth bit from the right: 1111 -> 0111.
+It can be shown we cannot convert 10 to 7 in less than 3 steps. Hence, we return 3.
+*/
