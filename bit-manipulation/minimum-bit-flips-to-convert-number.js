5e9b9bd59524de091451facbeebb98c24a877e79
emanuelefavero <emafaux89@gmail.com>
2024-08-19 01:42:52 +0200

    add bit-manipulation/minimum-bit-flips-to-convert-number solution 2

--- bit-manipulation/minimum-bit-flips-to-convert-number.js
+++ bit-manipulation/minimum-bit-flips-to-convert-number.js
@@ -14,8 +14,30 @@ function minBitFlips(start, goal) {
 }
 
 // ----------------------
+// SOLUTION 2
+
+function minBitFlips2(start, goal) {
+  let count = 0
+  let num1 = start.toString(2)
+  let num2 = goal.toString(2)
+
+  if (num1.length > num2.length) {
+    num2 = num2.padStart(num1.length, '0')
+  } else {
+    num1 = num1.padStart(num2.length, '0')
+  }
+
+  for (let i = 0; i < num1.length; i++) {
+    num1[i] !== num2[i] && count++
+  }
+
+  return count
+}
+
+// ----------------------
 // TESTS
 
+// 1
 console.log(minBitFlips(10, 7)) // 3
 /*
 Explanation: The binary representation of 10 and 7 are 1010 and 0111 respectively. We can convert 10 to 7 in 3 steps:
@@ -24,3 +46,6 @@ Explanation: The binary representation of 10 and 7 are 1010 and 0111 respectivel
 - Flip the fourth bit from the right: 1111 -> 0111.
 It can be shown we cannot convert 10 to 7 in less than 3 steps. Hence, we return 3.
 */
+
+// 2
+console.log(minBitFlips2(10, 7)) // 3
