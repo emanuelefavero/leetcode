230568dadfb87f14d0ed6aab569dcc551d8e5b38
emanuelefavero <emafaux89@gmail.com>
2024-08-26 19:44:37 +0200

    add string/cells-in-range-on-an-excel-sheet

---
+++ string/cells-in-range-on-an-excel-sheet.js
@@ -0,0 +1,50 @@
+// * Cells in a Range on an Excel Sheet
+
+/*
+A cell (r, c) of an excel sheet is represented as a string "<col><currentRow>" where:
+
+- <col> denotes the column number c of the cell. It is represented by alphabetical letters.
+  - For example, the 1st column is denoted by 'A', the 2nd by 'B', the 3rd by 'C', and so on.
+- <currentRow> is the currentRow number r of the cell. The rth currentRow is represented by the integer r.
+
+You are given a string s in the format "<col1><row1>:<col2><row2>", where <col1> represents the column c1, <row1> represents the currentRow r1, <col2> represents the column c2, and <row2> represents the currentRow r2, such that r1 <= r2 and c1 <= c2.
+
+Return the list of cells (x, y) such that r1 <= x <= r2 and c1 <= y <= c2. The cells should be represented as strings in the format mentioned above and be sorted in non-decreasing order first by columns and then by rows.
+*/
+
+function cellsInRange(s) {
+  let result = []
+  let col1 = s[0]
+  let col2 = s[3]
+  let cols = colsInRange(col1, col2)
+  let row1 = s[1]
+  let row2 = s[4]
+
+  for (let col of cols) {
+    for (let row = row1; row <= row2; row++) {
+      result.push(col + row)
+    }
+  }
+
+  return result
+}
+
+function colsInRange(col1, col2) {
+  let start = col1.charCodeAt(0)
+  let end = col2.charCodeAt(0)
+  let result = []
+
+  for (let i = start; i <= end; i++) {
+    result.push(String.fromCharCode(i))
+  }
+
+  return result
+}
+
+// -------------------------
+// TESTS
+
+console.log(cellsInRange('K1:L2')) // ['K1', 'K2', 'L1', 'L2']
+// Explanation: The above diagram shows the cells which should be present in the list. The red arrows denote the order in which the cells should be presented.
+
+console.log(cellsInRange('A1:F1')) // ['A1', 'B1', 'C1', 'D1', 'E1', 'F1']
