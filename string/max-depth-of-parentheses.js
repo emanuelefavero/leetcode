cae40b56508162fe8d247d4f9da7cac334062c33
emanuelefavero <emafaux89@gmail.com>
2024-08-28 06:32:49 +0200

    add string/max-depth-of-parentheses solution 2

--- string/max-depth-of-parentheses.js
+++ string/max-depth-of-parentheses.js
@@ -19,7 +19,32 @@ function maxDepth(s) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solutions uses a stack to keep track of the depth of the parentheses
+
+function maxDepth2(s) {
+  let max = 0
+  let stack = []
+
+  for (let char of s) {
+    if (char === '(') {
+      stack.push(char)
+    } else if (char === ')') {
+      if (stack.length > max) max = stack.length
+      stack.pop()
+    }
+  }
+
+  return max
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(maxDepth('(1+(2*3)+((8)/4))+1')) // 3
 // Explanation: Digit 8 is inside of 3 nested parentheses in the string.
+
+// 2
+console.log(maxDepth2('(1+(2*3)+((8)/4))+1')) // 3
