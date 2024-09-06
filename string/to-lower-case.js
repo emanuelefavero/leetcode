476493b332f3d120110b969ada153015b7bee0a9
emanuelefavero <emafaux89@gmail.com>
2024-09-06 08:31:49 +0200

    add string/to-lower-case solution 2

--- string/to-lower-case.js
+++ string/to-lower-case.js
@@ -13,6 +13,30 @@ function toLowerCase(s) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution uses ASCII values to convert uppercase to lowercase. It is slower than the first solution
+
+function toLowerCase2(s) {
+  let result = ''
+
+  for (let char of s) {
+    let charCode = char.charCodeAt(0)
+    if (charCode >= 65 && charCode <= 90) {
+      charCode += 32
+    }
+
+    result += String.fromCharCode(charCode)
+  }
+
+  return result
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(toLowerCase('Hello')) // hello
+
+// 2
+console.log(toLowerCase2('Hello')) // hello
