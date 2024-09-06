87b66277f013b9e8a4fc211a729e6d7f3e924822
emanuelefavero <emafaux89@gmail.com>
2024-09-06 08:25:37 +0200

    add string/to-lower-case jsDoc

--- string/to-lower-case.js
+++ string/to-lower-case.js
@@ -2,6 +2,12 @@
 
 // Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
 
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+
+// O(n) time | O(n) space
 function toLowerCase(s) {
   return s.toLowerCase()
 }
