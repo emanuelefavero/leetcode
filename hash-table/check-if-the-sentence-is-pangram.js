1641f0e1330b78d1d04a1911746b0f7e3f39cb63
emanuelefavero <emafaux89@gmail.com>
2024-09-10 12:18:52 +0200

    add hash-table/check-if-sentence-is-panagram

---
+++ hash-table/check-if-the-sentence-is-pangram.js
@@ -0,0 +1,28 @@
+// * Check if the Sentence Is Pangram
+
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+function checkIfPangram(sentence) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let map = new Map()
+
+  for (let char of sentence) {
+    map.set(char, true)
+  }
+
+  for (let char of alphabet) {
+    if (!map.has(char)) return false
+  }
+
+  return true
+}
+
+// -------------------------
+// TESTS
+
+console.log(checkIfPangram('thequickbrownfoxjumpsoverthelazydog')) // true
+// Explanation: sentence contains at least one of every letter of the English alphabet.
+
+console.log(checkIfPangram('hello')) // false
