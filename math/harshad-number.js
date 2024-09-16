6f6853b4dd4e5dc337d5c1479487237567995f5e
emanuelefavero <emafaux89@gmail.com>
2024-09-16 21:54:00 +0200

    add math/harshad-number solution 2

--- math/harshad-number.js
+++ math/harshad-number.js
@@ -18,7 +18,27 @@ function sumOfTheDigitsOfHarshadNumber(x) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution is more efficient than the previous one
+
+function sumOfTheDigitsOfHarshadNumber2(x) {
+  let sum = 0
+
+  for (let i of String(x)) {
+    sum += Number(i)
+  }
+
+  if (x % sum === 0) return sum
+  else return -1
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(sumOfTheDigitsOfHarshadNumber(18)) // 9
 // Explanation: The sum of digits of x is 9. 18 is divisible by 9. So 18 is a Harshad number and the answer is 9.
+
+// 2
+console.log(sumOfTheDigitsOfHarshadNumber2(18)) // 9
