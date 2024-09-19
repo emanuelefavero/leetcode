3e7bb0501d5157672590b1a640c2fdc80c68f711
emanuelefavero <emafaux89@gmail.com>
2024-09-19 10:08:19 +0200

    add string/count-asterisks solution 2

--- string/count-asterisks.js
+++ string/count-asterisks.js
@@ -32,8 +32,29 @@ function countAsterisks(s) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution does not use split method. Instead, it uses a boolean variable to keep track of whether the current character is inside a pair of '|'. the `!insidePair` inside the if statement also ensures that the '*' is counted only alternate pairs of '|' (as required by the problem).
+
+function countAsterisks2(s) {
+  let count = 0
+  let insidePair = false
+
+  for (let char of s) {
+    if (char === '|') {
+      insidePair = !insidePair
+    } else if (char === '*' && !insidePair) {
+      count++
+    }
+  }
+
+  return count
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(countAsterisks('l|*e*et|c**o|*de|')) // 2
 /*
 Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
@@ -41,6 +62,8 @@ The characters between the first and second '|' are excluded from the answer.
 Also, the characters between the third and fourth '|' are excluded from the answer.
 There are 2 asterisks considered. Therefore, we return 2.
 */
-
 console.log(countAsterisks('iamprogrammer')) // 0
 console.log(countAsterisks('yo|uar|e**|b|e***au|tifu|l')) // 5
+
+// 2
+console.log(countAsterisks2('l|*e*et|c**o|*de|')) // 2
