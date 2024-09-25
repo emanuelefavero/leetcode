a009cfc8b23c828ca51f2e5c8fa11d8780d4f93e
emanuelefavero <emafaux89@gmail.com>
2024-09-25 12:43:05 +0200

    add array/number-of-senior-citizens

---
+++ array/number-of-senior-citizens.js
@@ -0,0 +1,31 @@
+// * Number of Senior Citizens
+
+/*
+You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+- The first ten characters consist of the phone number of passengers.
+- The next character denotes the gender of the person.
+- The following two characters are used to indicate the age of the person.
+- The last two characters determine the seat allotted to that person.
+
+Return the number of passengers who are strictly more than 60 years old.
+*/
+
+function countSeniors(details) {
+  let count = 0
+
+  for (let detail of details) {
+    let age = +(detail[11] + detail[12])
+    if (age > 60) count++
+  }
+
+  return count
+}
+
+// -------------------------
+// TESTS
+
+console.log(
+  countSeniors(['7868190130M7522', '5303914400F9211', '9273338290F4010'])
+) // 2
+// Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
