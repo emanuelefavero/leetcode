a61a6fdf79c2b676b133639e8a817f031640fecb
emanuelefavero <emafaux89@gmail.com>
2024-09-25 12:44:42 +0200

    add array/number-of-senior-citizens solution 2

--- array/number-of-senior-citizens.js
+++ array/number-of-senior-citizens.js
@@ -14,9 +14,23 @@ Return the number of passengers who are strictly more than 60 years old.
 function countSeniors(details) {
   let count = 0
 
-  for (let detail of details) {
-    let age = +(detail[11] + detail[12])
+  for (let d of details) {
+    let age = parseInt(d[11] + d[12])
     if (age > 60) count++
+  }
+
+  return count
+}
+
+// -------------------------
+// SOLUTION 2
+
+function countSeniors2(details) {
+  let count = 0
+
+  for (let d of details) {
+    let age = +(d[11] + d[12])
+    count += age > 60
   }
 
   return count
@@ -25,7 +39,13 @@ function countSeniors(details) {
 // -------------------------
 // TESTS
 
+// 1
 console.log(
   countSeniors(['7868190130M7522', '5303914400F9211', '9273338290F4010'])
 ) // 2
 // Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+
+// 2
+console.log(
+  countSeniors2(['7868190130M7522', '5303914400F9211', '9273338290F4010'])
+) // 2
