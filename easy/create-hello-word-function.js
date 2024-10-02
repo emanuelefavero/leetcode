c88ed428ef89f1c3ed19fdfb62304ef52e876e84
emanuelefavero <emafaux89@gmail.com>
2024-10-02 16:53:17 +0200

    add easy/create-hello-world-function solution 2

--- easy/create-hello-word-function.js
+++ easy/create-hello-word-function.js
@@ -12,6 +12,17 @@ function createHelloWorld() {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution uses the classic function syntax
+
+function createHelloWorld2() {
+  return function (...args) {
+    return 'Hello World'
+  }
+}
+
+// -------------------------
 // TESTS
 
 const f = createHelloWorld()
@@ -19,3 +30,7 @@ console.log(f()) // "Hello World"
 console.log(f({}, null, 42)) // "Hello World"
 
 // Explanation: The function returned by createHelloWorld should always return "Hello World"
+
+const f2 = createHelloWorld()
+console.log(f2()) // "Hello World"
+console.log(f2({}, null, 42)) // "Hello World"
