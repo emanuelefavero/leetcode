11b8ee0a5dcc263959dc217d5120589b153d2965
emanuelefavero <emafaux89@gmail.com>
2024-10-04 19:06:41 +0200

    add easy/counter jsDoc

--- easy/counter.js
+++ easy/counter.js
@@ -2,6 +2,12 @@
 
 // Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 
+/**
+ * @param {number} n
+ * @returns {Function}
+ */
+
+// O(1) time | O(1) space
 function createCounter(n) {
   return () => n++
 }
@@ -13,3 +19,10 @@ const counter = createCounter(10)
 console.log(counter()) // 10
 console.log(counter()) // 11
 console.log(counter()) // 12
+
+/*
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+*/
