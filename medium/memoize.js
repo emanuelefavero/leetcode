dd540a854ae19c78f2aa2824c0946be5be4c7c1e
emanuelefavero <emafaux89@gmail.com>
2024-10-16 14:14:26 +0200

    refactor medium/memoize

--- medium/memoize.js
+++ medium/memoize.js
@@ -16,14 +16,12 @@ function memoize(fn) {
   const cache = {}
 
   return function (...args) {
-    const key = String(args)
+    const key = JSON.stringify(args)
 
     if (key in cache) return cache[key]
 
     // callCount++
-    const result = fn(...args)
-    cache[key] = result
-    return result
+    return (cache[key] = fn(...args))
   }
 }
 
