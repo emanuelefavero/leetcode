fa7eb65f640b261fa5d136a861c3b84210232be6
emanuelefavero <emafaux89@gmail.com>
2024-10-17 12:02:27 +0200

    add easy/is-object-empty solution 2

--- easy/is-object-empty.js
+++ easy/is-object-empty.js
@@ -13,10 +13,27 @@ function isEmpty(obj) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution seems more efficient than the previous one
+
+function isEmpty2(obj) {
+  for (let key in obj) return false
+  return true
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(isEmpty({ x: 5, y: 42 })) // false
 console.log(isEmpty({})) // true
-
 console.log(isEmpty([null, false, 0])) // false
 console.log(isEmpty([])) // true
+
+// 2
+console.log('Solution 2:')
+console.log(isEmpty2({ x: 5, y: 42 })) // false
+console.log(isEmpty2({})) // true
+console.log(isEmpty2([null, false, 0])) // false
+console.log(isEmpty2([])) // true
