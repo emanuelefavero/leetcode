84a81122fe580c49804f8d32b0ae67ac55fac518
emanuelefavero <emafaux89@gmail.com>
2024-10-18 15:42:19 +0200

    add easy/array-prototype-last jsDoc

--- easy/array-prototype-last.js
+++ easy/array-prototype-last.js
@@ -4,6 +4,11 @@
 
 // You may assume the array is the output of JSON.parse.
 
+/**
+ * @returns {null|boolean|number|string|Array|Object}
+ */
+
+// O(1) time | O(1) space
 Array.prototype.last = function () {
   if (!this.length) return -1
   return this[this.length - 1]
