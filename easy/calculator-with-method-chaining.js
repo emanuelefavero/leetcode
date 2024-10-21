1334b919711cc009783f2f3797fc7308e68051e4
emanuelefavero <emafaux89@gmail.com>
2024-10-21 13:06:27 +0200

    add easy/calculator-with-method-chaining jsDoc

--- easy/calculator-with-method-chaining.js
+++ easy/calculator-with-method-chaining.js
@@ -18,36 +18,62 @@ Solutions within 10-5 of the actual result are considered correct.
 */
 
 class Calculator {
+  /**
+   * @param {number} value
+   */
   constructor(value) {
     this.result = value
   }
 
+  /**
+   * @param {number} value
+   * @returns {Calculator}
+   */
   add(value) {
     this.result += value
     return this
   }
 
+  /**
+   * @param {number} value
+   * @returns {Calculator}
+   */
   subtract(value) {
     this.result -= value
     return this
   }
 
+  /**
+   * @param {number} value
+   * @returns {Calculator}
+   */
   multiply(value) {
     this.result *= value
     return this
   }
 
+  /**
+   * @param {number} value
+   * @returns {Calculator}
+   */
   divide(value) {
     if (value === 0) throw new Error('Division by zero is not allowed')
     this.result /= value
     return this
   }
 
+  /**
+   * @param {number} value
+   * @returns {Calculator}
+   */
   power(value) {
     this.result **= value
     return this
   }
 
+  /**
+   * @returns {number}
+   */
   getResult() {
     return this.result
   }
