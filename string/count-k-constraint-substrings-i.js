ff3f013c942c571c8aafd5f61bd6028ce6e5c986
emanuelefavero <emafaux89@gmail.com>
2024-10-25 21:15:43 +0200

    add string/count-k-constraint-substrings-i

---
+++ string/count-k-constraint-substrings-i.js
@@ -0,0 +1,48 @@
+// * Count Substrings That Satisfy K-Constraint I
+
+/*
+You are given a binary string s and an integer k.
+
+A binary string satisfies the k-constraint if either of the following conditions holds:
+
+- The number of 0's in the string is at most k.
+- The number of 1's in the string is at most k.
+
+Return an integer denoting the number of 
+substrings of s that satisfy the k-constraint.
+*/
+
+function countKConstraintSubstrings(s, k) {
+  let count = 0
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j <= s.length; j++) {
+      let substring = s.slice(i, j)
+      if (isSatisfied(substring, k)) count++
+    }
+  }
+
+  return count
+}
+
+function isSatisfied(substring, k) {
+  let count0 = 0
+  let count1 = 0
+
+  for (let char of substring) {
+    if (char === '0') count0++
+    else if (char === '1') count1++
+  }
+
+  if (count0 <= k || count1 <= k) return true
+  else return false
+}
+
+// -------------------------
+// TESTS
+
+console.log(countKConstraintSubstrings('10101', 1)) // 12
+// Explanation: Every substring of s except the substrings "1010", "10101", and "0101" satisfies the k-constraint.
+
+console.log(countKConstraintSubstrings('1010101', 2)) // 25
+// Explanation: Every substring of s except the substrings with a length greater than 5 satisfies the k-constraint.
