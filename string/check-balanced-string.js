17d8001fb5a1f9bd58a63e1a41582ca9d54601c5
emanuelefavero <emafaux89@gmail.com>
2024-11-06 16:23:43 +0100

    add string/check-balanced-string

---
+++ string/check-balanced-string.js
@@ -0,0 +1,32 @@
+// * Check Balanced String
+
+// You are given a string num consisting of only digits. A string of digits is called balanced if the sum of the digits at even indices is equal to the sum of digits at odd indices.
+
+// Return true if num is balanced, otherwise return false.
+
+function isBalanced(num) {
+  let evenSum = 0
+  let oddSum = 0
+
+  for (let i = 0; i < num.length; i++) {
+    let digit = Number(num[i])
+
+    if (i % 2 === 0) evenSum += digit
+    else oddSum += digit
+  }
+
+  return evenSum === oddSum
+}
+
+// -------------------------
+// TESTS
+
+console.log(isBalanced('1234')) // false
+/*
+Explanation:
+
+The sum of digits at even indices is 1 + 3 == 4, and the sum of digits at odd indices is 2 + 4 == 6.
+Since 4 is not equal to 6, num is not balanced.
+*/
+
+console.log(isBalanced('24123')) // true
