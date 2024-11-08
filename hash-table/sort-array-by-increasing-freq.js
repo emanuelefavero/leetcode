d9554d2b017ab834fd3e46eb40cf64e84a694e79
emanuelefavero <emafaux89@gmail.com>
2024-11-08 19:03:04 +0100

    add hash-table/sort-array-by-increasing-freq jsDoc

--- hash-table/sort-array-by-increasing-freq.js
+++ hash-table/sort-array-by-increasing-freq.js
@@ -2,8 +2,14 @@
 
 // Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
 
-// Return the sorted array.
+// Return the sorted array
 
+/**
+ * @param {number[]} nums
+ * @returns {number[]}
+ */
+
+// O(n log n) time  | O(n) space
 function frequencySort(nums) {
   let map = new Map()
 
