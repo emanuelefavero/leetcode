5d2725f792c3bd54898a56076ef5441797a0d61c
emanuelefavero <emafaux89@gmail.com>
2024-11-08 19:06:58 +0100

    add hash-table/sort-array-by-increasing-freq solution 2

--- hash-table/sort-array-by-increasing-freq.js
+++ hash-table/sort-array-by-increasing-freq.js
@@ -29,9 +29,32 @@ function frequencySort(nums) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: This solution uses an object instead of a Map and returns the sorted method directly
+
+function frequencySort2(nums) {
+  let map = {}
+
+  for (let num of nums) {
+    map[num] = (map[num] || 0) + 1
+  }
+
+  return nums.sort((a, b) => {
+    if (map[a] === map[b]) return b - a
+    return map[a] - map[b]
+  })
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 console.log(frequencySort([1, 1, 2, 2, 2, 3])) // [3,1,1,2,2,2]
 // Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
 console.log(frequencySort([2, 3, 1, 3, 2])) // [1,3,3,2,2]
 // Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+
+// 2
+console.log(frequencySort2([1, 1, 2, 2, 2, 3])) // [3,1,1,2,2,2]
+console.log(frequencySort2([2, 3, 1, 3, 2])) // [1,3,3,2,2]
