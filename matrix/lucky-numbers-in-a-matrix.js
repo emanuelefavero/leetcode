cb2728b04023133e797c7aeed60272b57f269d44
emanuelefavero <emafaux89@gmail.com>
2024-11-12 15:26:38 +0100

    add matrix/lucky-numbers-in-a-matrix link solution 2

--- matrix/lucky-numbers-in-a-matrix.js
+++ matrix/lucky-numbers-in-a-matrix.js
@@ -30,6 +30,23 @@ function luckyNumbers(matrix) {
 }
 
 // --------------------------
+// SOLUTION 2
+
+// TIP: This solution uses the .every() method to check if the min value is the max value in its column
+
+function luckyNumbers2(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    let min = Math.min(...matrix[i]) // get the min value in the row
+    let minIndex = matrix[i].indexOf(min)
+
+    // check if the min value is the max value in its column
+    if (matrix.every((arr) => arr[minIndex] <= min)) return [min]
+  }
+
+  return []
+}
+
+// --------------------------
 // TESTS
 
 console.log(
@@ -40,3 +57,12 @@ console.log(
   ])
 ) // [15]
 // Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+// 2
+console.log(
+  luckyNumbers2([
+    [3, 7, 8],
+    [9, 11, 13],
+    [15, 16, 17],
+  ])
+) // [15]
