b445f87c3e1f637260fb18af2dc78dd5f2ab5da2
emanuelefavero <emafaux89@gmail.com>
2024-11-14 22:55:49 +0100

    add string/remove-trailing-zeros-from-a-string jsDoc

--- string/remove-trailing-zeros-from-a-string.js
+++ string/remove-trailing-zeros-from-a-string.js
@@ -2,6 +2,12 @@
 
 // Given a positive integer num represented as a string, return the integer num without trailing zeros as a string.
 
+/**
+ * @param {string} num
+ * @returns {string}
+ */
+
+// O(n) time | O(1) space
 function removeTrailingZeros(num) {
   for (let i = num.length - 1; i >= 0; i--) {
     if (num[i] === '0') {
