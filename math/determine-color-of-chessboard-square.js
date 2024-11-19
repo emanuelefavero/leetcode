b2385cd12f982f143f60a4d47979350480736fe8
emanuelefavero <emafaux89@gmail.com>
2024-11-19 16:38:13 +0100

    add math/determine-color-of-chessboard-square jsDoc

--- math/determine-color-of-chessboard-square.js
+++ math/determine-color-of-chessboard-square.js
@@ -18,8 +18,14 @@ Return true if the square is white, and false if the square is black.
 The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
 */
 
+/**
+ * @param {string} coordinates
+ * @returns {boolean}
+ */
+
 // TIP: The square is white if the letter's ASCII code is odd (e.g. 'a' = 97) and the number is even (e.g. 2)
 
+// O(1) time | O(1) space
 function squareIsWhite(coordinates) {
   return coordinates[0].charCodeAt(0) % 2 !== parseInt(coordinates[1]) % 2
 }
