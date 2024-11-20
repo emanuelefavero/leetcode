1fa0027b76b598b0bd7597b3276dc4f33188290a
emanuelefavero <emafaux89@gmail.com>
2024-11-20 17:06:50 +0100

    add array/defuse-the-bomb solution 2

--- array/defuse-the-bomb.js
+++ array/defuse-the-bomb.js
@@ -47,8 +47,47 @@ function decrypt(code, k) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This is a sliding window solution
+
+function decrypt2(code, k) {
+  const decode = (code, k) => {
+    let length = 0
+    let result = []
+    let sum = 0
+
+    // Calculate the sum of the first k elements
+    for (let i = 0; i < k; i++) {
+      sum += code[i]
+    }
+
+    // Calculate the sum of the next k elements
+    for (let i = 0; i < code.length; i++) {
+      sum -= code[i]
+      sum += code[(i + k) % code.length] // Use modulo to handle circular array
+      result.push(sum)
+    }
+
+    return result
+  }
+
+  if (k === 0) return new Array(code.length).fill(0)
+  if (k > 0) return decode(code, k)
+
+  // Reverse the array to handle negative k
+  if (k < 0) return decode(code.reverse(), -k).reverse()
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(decrypt([5, 7, 1, 4], 3)) // [12, 10, 16, 13]
 console.log(decrypt([1, 2, 3, 4], 0)) // [0, 0, 0, 0]
 console.log(decrypt([2, 4, 9, 3], -2)) // [12, 5, 6, 13]
+
+// 2
+console.log(decrypt2([5, 7, 1, 4], 3)) // [12, 10, 16, 13]
+console.log(decrypt2([1, 2, 3, 4], 0)) // [0, 0, 0, 0]
+console.log(decrypt2([2, 4, 9, 3], -2)) // [12, 5, 6, 13]
