c0267f78a2bce8f12b0e12a86511bca7d60d49c4
emanuelefavero <emafaux89@gmail.com>
2024-12-02 07:50:26 +0100

    add array/find-greatest-common-divisor

---
+++ array/find-greatest-common-divisor.js
@@ -0,0 +1,33 @@
+// * Find Greatest Common Divisor of Array
+
+// Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+// The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+function findGCD(nums) {
+  const largest = Math.max(...nums)
+  const smallest = Math.min(...nums)
+  let divisor = 1
+
+  for (let i = smallest; i >= 1; i--) {
+    if (smallest % i === 0 && largest % i === 0) {
+      divisor = i
+      break
+    }
+  }
+
+  return divisor
+}
+
+// -------------------------
+// TESTS
+
+console.log(findGCD([2, 5, 6, 9, 10])) // 2
+/*
+Explanation:
+The smallest number in nums is 2.
+The largest number in nums is 10.
+The greatest common divisor of 2 and 10 is 2.
+*/
+console.log(findGCD([7, 5, 6, 8, 3])) // 1
+console.log(findGCD([3, 3])) // 3
