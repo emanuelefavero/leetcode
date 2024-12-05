4e76046f048027542d05262756fe9aa622def51b
Emanuele Favero <emafaux89@gmail.com>
2024-12-05 10:06:02 +0100

    add string/generate-odd-counts-string solution 2

--- string/generate-odd-counts-string.js
+++ string/generate-odd-counts-string.js
@@ -19,9 +19,29 @@ function generateTheString(n) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: We can use the repeat method to repeat a string n times
+
+function generateTheString2(n) {
+  if (n % 2 === 0) {
+    return 'a'.repeat(n - 1) + 'b'
+  } else return 'a'.repeat(n)
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(generateTheString(4)) // aaab
 console.log(generateTheString(2)) // ab
 console.log(generateTheString(3)) // aaa
 console.log(generateTheString(1)) // a
+
+// Explanation: "aaab" is a valid string since the character 'a' occurs three times and the character 'b' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+
+// 2
+console.log(generateTheString2(4)) // aaab
+console.log(generateTheString2(2)) // ab
+console.log(generateTheString2(3)) // aaa
+console.log(generateTheString2(1)) // a
