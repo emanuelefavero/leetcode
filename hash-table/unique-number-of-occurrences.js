6e1b88d80d679403d3007fbc89ccd7b8c11738d5
Emanuele Favero <emafaux89@gmail.com>
2024-12-07 06:07:29 +0100

    add hash-table/unique-number-of-occurrences solution 2

--- hash-table/unique-number-of-occurrences.js
+++ hash-table/unique-number-of-occurrences.js
@@ -19,9 +19,31 @@ function uniqueOccurrences(arr) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: We could also check the size of the set
+
+function uniqueOccurrences2(arr) {
+  let map = new Map()
+
+  for (let num of arr) {
+    map.set(num, (map.get(num) || 0) + 1)
+  }
+
+  let set = new Set(map.values())
+
+  return map.size === set.size
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3])) // true
 // Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 
 console.log(uniqueOccurrences([1, 2])) // false
+
+// 2
+console.log(uniqueOccurrences2([1, 2, 2, 1, 1, 3])) // true
+console.log(uniqueOccurrences2([1, 2])) // false
