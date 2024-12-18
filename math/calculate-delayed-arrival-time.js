ca6322c3d4eb771ba95c641eeb72d9670785a6c4
Emanuele Favero <emafaux89@gmail.com>
2024-12-18 03:43:49 +0100

    add math/calculate-delayed-arrival-time solution 2

--- math/calculate-delayed-arrival-time.js
+++ math/calculate-delayed-arrival-time.js
@@ -20,9 +20,26 @@ function findDelayedArrivalTime(arrivalTime, delayedTime) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution does not use the modulo operator
+
+function findDelayedArrivalTime2(arrivalTime, delayedTime) {
+  let result = arrivalTime + delayedTime
+
+  if (result >= 24) return result - 24
+  return result
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(findDelayedArrivalTime(15, 5)) // 20
 // Explanation: Arrival time of the train was 15:00 hours. It is delayed by 5 hours. Now it will reach at 15+5 = 20 (20:00 hours).
 console.log(findDelayedArrivalTime(13, 11)) // 0
 // Explanation: Arrival time of the train was 13:00 hours. It is delayed by 11 hours. Now it will reach at 13+11=24 (Which is denoted by 00:00 in 24 hours format so return 0).
+
+// 2
+console.log(findDelayedArrivalTime2(15, 5)) // 20
+console.log(findDelayedArrivalTime2(13, 11)) // 0
