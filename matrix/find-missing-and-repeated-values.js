dae0e163f81a9bbf5520d9b8b81d0095343299a3
Emanuele Favero <emafaux89@gmail.com>
2025-02-03 23:41:57 +0100

    add matrix/find-missing-and-repeated-values solution 2

--- matrix/find-missing-and-repeated-values.js
+++ matrix/find-missing-and-repeated-values.js
@@ -47,8 +47,41 @@ function findMissingAndRepeatedValues(grid) {
 }
 
 // --------------------------
+// SOLUTION 2
+
+// TIP: Since the numbers are in the range [1, n2], we can use an array to count the frequency of each number.
+
+function findMissingAndRepeatedValues2(grid) {
+  let length = grid.length
+  let count = new Array(length * length + 1).fill(0)
+
+  // Count the frequency of each number
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      count[grid[i][j]]++
+    }
+  }
+
+  let repeated = -1
+  let missing = -1
+  let numsLength = length * length
+
+  // Find repeated and missing numbers
+  for (let i = 1; i <= numsLength; i++) {
+    if (count[i] === 2) repeated = i
+    if (count[i] === 0) missing = i
+
+    // If both repeated and missing numbers are found, return them
+    if (repeated !== -1 && missing !== -1) return [repeated, missing]
+  }
+
+  return [repeated, missing]
+}
+
+// --------------------------
 // TESTS
 
+// 1
 console.log(
   findMissingAndRepeatedValues([
     [1, 3],
@@ -56,3 +89,11 @@ console.log(
   ])
 ) // [2, 4]
 // Explanation: Number 2 is repeated and number 4 is missing so the answer is [2,4]
+
+// 2
+console.log(
+  findMissingAndRepeatedValues2([
+    [1, 3],
+    [2, 2],
+  ])
+) // [2, 4]
