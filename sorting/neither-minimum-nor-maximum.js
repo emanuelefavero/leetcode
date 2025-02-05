74afe1ee69947e86088e058646abe152c0f41e99
Emanuele Favero <emafaux89@gmail.com>
2025-02-05 23:32:59 +0100

    add sorting/neither-minimum-nor-maximum solution 2

--- sorting/neither-minimum-nor-maximum.js
+++ sorting/neither-minimum-nor-maximum.js
@@ -19,8 +19,32 @@ function findNonMinOrMax(nums) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: We can also solve this problem without sorting in O(n) time complexity
+
+// O(n) time | O(1) space
+function findNonMinOrMax2(nums) {
+  if (nums.length <= 2) return -1
+
+  let max = Math.max(...nums)
+  let min = Math.min(...nums)
+
+  for (let num of nums) {
+    if (num !== max && num !== min) return num
+  }
+
+  return -1
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(findNonMinOrMax([3, 2, 1, 4])) // 2
 // Explanation: In this example, the minimum value is 1 and the maximum value is 4. Therefore, either 2 or 3 can be valid answers.
 console.log(findNonMinOrMax([1, 2])) // -1
+
+// 2
+console.log(findNonMinOrMax2([3, 2, 1, 4])) // 2
+console.log(findNonMinOrMax2([1, 2])) // -1
