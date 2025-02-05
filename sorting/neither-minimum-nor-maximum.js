94cfe105db244d1e697b578ec1f9af534f38e4b1
Emanuele Favero <emafaux89@gmail.com>
2025-02-05 23:29:10 +0100

    add sorting/neither-minimum-nor-maximum

---
+++ sorting/neither-minimum-nor-maximum.js
@@ -0,0 +1,20 @@
+// * Neither Minimum nor Maximum
+
+// Given an integer array nums containing distinct positive integers, find and return any number from the array that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+
+// Return the selected integer.
+
+function findNonMinOrMax(nums) {
+  if (nums.length <= 2) return -1
+
+  nums.sort((a, b) => a - b)
+
+  return nums[1]
+}
+
+// -------------------------
+// TESTS
+
+console.log(findNonMinOrMax([3, 2, 1, 4])) // 2
+// Explanation: In this example, the minimum value is 1 and the maximum value is 4. Therefore, either 2 or 3 can be valid answers.
+console.log(findNonMinOrMax([1, 2])) // -1
