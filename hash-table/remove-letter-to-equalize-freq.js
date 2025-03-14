45ffb393bcfea4f461161696077e4b3f4fc02985
Emanuele Favero <emafaux89@gmail.com>
2025-03-14 09:16:13 +0100

    add hash-table/remove-letter-to-equalize-freq

---
+++ hash-table/remove-letter-to-equalize-freq.js
@@ -0,0 +1,45 @@
+// * Remove Letter To Equalize Frequency
+
+/*
+You are given a 0-indexed string word, consisting of lowercase English letters. You need to select one index and remove the letter at that index from word so that the frequency of every letter present in word is equal.
+
+Return true if it is possible to remove one letter so that the frequency of all letters in word are equal, and false otherwise.
+
+Note:
+
+- The frequency of a letter x is the number of times it occurs in the string.
+- You must remove exactly one letter and cannot choose to do nothing.
+*/
+
+function equalFrequency(word) {
+  const map = new Map()
+
+  // 1. Count the frequency of each character
+  for (const char of word) {
+    map.set(char, (map.get(char) || 0) + 1)
+  }
+
+  // 2. Remove each character and check if all frequencies are equal
+  for (const char of word) {
+    map.set(char, map.get(char) - 1)
+
+    // If frequency becomes 0, remove the key
+    if (map.get(char) === 0) map.delete(char)
+
+    // Check if all remaining frequencies are equal
+    if (new Set(map.values()).size === 1) return true
+
+    // Reset the frequency by adding 1
+    map.set(char, (map.get(char) || 0) + 1)
+  }
+
+  return false
+}
+
+// -----------------------------
+// TESTS
+
+console.log(equalFrequency('abcc')) // true
+console.log(equalFrequency('aazz')) // false
+console.log(equalFrequency('ab')) // false
+console.log(equalFrequency('bac')) // true
