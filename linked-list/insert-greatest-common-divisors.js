1bad04e559d8ea6800e080873756f0a8d2dea100
Emanuele Favero <emafaux89@gmail.com>
2025-03-14 08:34:50 +0100

    add linked-list/insert-greatest-common-divisors solution 2

--- linked-list/insert-greatest-common-divisors.js
+++ linked-list/insert-greatest-common-divisors.js
@@ -55,18 +55,53 @@ function insertGreatestCommonDivisors(head) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: To solve this problem we can also use the Euclidean Algorithm (@see https://en.wikipedia.org/wiki/Euclidean_algorithm) to find the greatest common divisor. We keep dividing the dividend by the remainder until the remainder is 0. This way we can find the greatest common divisor in O(log n) time
+
+// O(log n) time | O(1) space
+function gcd(a, b) {
+  if (a % b === 0) return b
+  return gcd(b, a % b)
+}
+
+// O(n) time | O(1) space
+function insertGreatestCommonDivisors2(head) {
+  let current = head
+
+  while (current && current.next) {
+    let currentVal = current.val
+    let nextVal = current.next.val
+
+    let gcdVal = gcd(currentVal, nextVal)
+
+    const newNode = new ListNode(gcdVal)
+    newNode.next = current.next
+    current.next = newNode
+
+    current = newNode.next
+  }
+
+  return head
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 let head = new ListNode(18)
 head.next = new ListNode(6)
 head.next.next = new ListNode(10)
 head.next.next.next = new ListNode(3)
 
-// Print the result
-let resultHead = insertGreatestCommonDivisors(head)
-let current = resultHead
-while (current) {
-  console.log(current.val)
-  current = current.next
-}
+console.log(insertGreatestCommonDivisors(head))
+// 18 -> 6 -> 6 -> 2 -> 10 -> 1 -> 3
+
+// 2
+head = new ListNode(18)
+head.next = new ListNode(6)
+head.next.next = new ListNode(10)
+head.next.next.next = new ListNode(3)
+
+console.log(insertGreatestCommonDivisors2(head))
 // 18 -> 6 -> 6 -> 2 -> 10 -> 1 -> 3
