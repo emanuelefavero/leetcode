2ee13d03d0304884c4891abe6c745a6bf9a4f362
Emanuele Favero <emafaux89@gmail.com>
2025-03-19 22:43:38 +0100

    add hash-table/the-two-sneaky-numbers

---
+++ hash-table/the-two-sneaky-numbers.js
@@ -0,0 +1,25 @@
+// * The Two Sneaky Numbers of Digitville
+
+/*
+In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+
+As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+*/
+
+function getSneakyNumbers(nums) {
+  let set = new Set()
+  let result = []
+
+  for (let num of nums) {
+    if (set.has(num)) result.push(num)
+    else set.add(num)
+  }
+
+  return result
+}
+
+// -----------------------------
+// TESTS
+
+console.log(getSneakyNumbers([0, 1, 1, 0])) // [0, 1]
+// Explanation: The numbers 0 and 1 each appear twice in the array
