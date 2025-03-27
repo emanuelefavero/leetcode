88dfc25eeb1736ac890ccbae6bacbf5297ef51ff
Emanuele Favero <emafaux89@gmail.com>
2025-03-27 23:07:33 +0100

    add sorting/transform-array-by-parity solution 2

--- sorting/transform-array-by-parity.js
+++ sorting/transform-array-by-parity.js
@@ -29,11 +29,28 @@ function transformArray(nums) {
 }
 
 // -----------------------
+// SOLUTION 2
+
+// O(n log n) time | O(1) space
+function transformArray2(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) nums[i] = 0
+    else nums[i] = 1
+  }
+
+  return nums.sort((a, b) => a - b)
+}
+
+// -----------------------
 // TESTS
 
+// 1
 console.log(transformArray([4, 3, 2, 1])) //  [0,0,1,1]
 /*
 Explanation:
 - Replace the even numbers (4 and 2) with 0 and the odd numbers (3 and 1) with 1. Now, nums = [0, 1, 0, 1].
 - After sorting nums in non-descending order, nums = [0, 0, 1, 1].
 */
+
+// 2
+console.log(transformArray2([4, 3, 2, 1])) //  [0,0,1,1]
