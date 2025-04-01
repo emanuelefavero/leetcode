26fa30b49c0e154b85a7627b5d4f577859b196ba
Emanuele Favero <emafaux89@gmail.com>
2025-04-01 14:06:14 +0200

    add hash-table/uncommon-words-from-two-sentences solution 2

--- hash-table/uncommon-words-from-two-sentences.js
+++ hash-table/uncommon-words-from-two-sentences.js
@@ -45,8 +45,44 @@ function uncommonFromSentences(s1, s2) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: This solution is the same as the first one, but uses a DRY approach
+
+function uncommonFromSentences2(s1, s2) {
+  let arr1 = s1.split(' ')
+  let arr2 = s2.split(' ')
+
+  const countWords = (arr) => {
+    let map = new Map()
+    for (let word of arr) {
+      map.set(word, (map.get(word) || 0) + 1)
+    }
+    return map
+  }
+
+  let map1 = countWords(arr1)
+  let map2 = countWords(arr2)
+
+  const checkUncommon = (map1, map2) => {
+    let result = []
+    for (let [key, value] of map1) {
+      if (value === 1 && !map2.has(key)) result.push(key)
+    }
+    return result
+  }
+
+  return [...checkUncommon(map1, map2), ...checkUncommon(map2, map1)]
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 console.log(uncommonFromSentences('this apple is sweet', 'this apple is sour')) // ['sweet', 'sour']
 // Explanation: The word "sweet" appears only in s1, while the word "sour" appears only in s2.
-console.log(uncommonFromSentences('apple apple', 'banana'))
+console.log(uncommonFromSentences('apple apple', 'banana')) // ['banana']
+
+// 2
+console.log(uncommonFromSentences2('this apple is sweet', 'this apple is sour')) // ['sweet', 'sour']
+console.log(uncommonFromSentences2('apple apple', 'banana')) // ['banana']
