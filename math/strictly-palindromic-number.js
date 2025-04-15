918fcfa06db3d4097f41d70fcb2207b33260984a
Emanuele Favero <emafaux89@gmail.com>
2025-04-15 23:54:00 +0200

    add math/strictly-palindromic-number

---
+++ math/strictly-palindromic-number.js
@@ -0,0 +1,30 @@
+// * Strictly Palindromic Number
+
+/*
+An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), the string representation of the integer n in base b is palindromic.
+
+Given an integer n, return true if n is strictly palindromic and false otherwise.
+
+A string is palindromic if it reads the same forward and backward.
+*/
+
+function isStrictlyPalindromic(n) {
+  let length = n - 2
+  for (let i = 2; i <= length; i++) {
+    let base = n.toString(i)
+    let reversed = base.split('').reverse().join('')
+    if (base !== reversed) return false
+  }
+  return true
+}
+
+// -------------------------
+// TESTS
+
+console.log(isStrictlyPalindromic(9)) // false
+/*
+Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
+In base 3: 9 = 100 (base 3), which is not palindromic.
+Therefore, 9 is not strictly palindromic so we return false.
+Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
+*/
