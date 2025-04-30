8c4db94fcb7392164f4913252103f9ac9720eb23
Emanuele Favero <emafaux89@gmail.com>
2025-04-30 13:18:37 +0200

    add hash-table/find-the-prefix-common-array solution 2

--- hash-table/find-the-prefix-common-array.js
+++ hash-table/find-the-prefix-common-array.js
@@ -35,8 +35,38 @@ function findThePrefixCommonArray(A, B) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: This solution is similar to the first one but does not use the pre increment operator
+
+function findThePrefixCommonArray2(A, B) {
+  let n = A.length
+  let freq = new Array(n + 1).fill(0)
+  let result = []
+  let count = 0
+
+  for (let i = 0; i < n; i++) {
+    let numA = A[i]
+    let numB = B[i]
+
+    // Increment the frequency of the current numbers in A and B
+    // If the frequency becomes 2 (the number is present in both arrays), increment the count
+    freq[numA]++
+    if (freq[A[i]] === 2) count++
+
+    freq[numB]++
+    if (freq[B[i]] === 2) count++
+
+    result.push(count)
+  }
+
+  return result
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 console.log(findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4])) // [0,2,3,4]
 /*
 Explanation: At i = 0: no number is common, so C[0] = 0.
@@ -44,3 +74,6 @@ At i = 1: 1 and 3 are common in A and B, so C[1] = 2.
 At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
 At i = 3: 1, 2, 3, and 4 are common in A and B, so C[3] = 4.
 */
+
+// 2
+console.log(findThePrefixCommonArray2([1, 3, 2, 4], [3, 1, 2, 4])) // [0,2,3,4]
