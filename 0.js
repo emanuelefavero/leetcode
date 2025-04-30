e797e1c7a1ed7a22dd2401bee35a95870fdbf13a
Emanuele Favero <emafaux89@gmail.com>
2025-04-30 13:36:37 +0200

    remove unused 0.js file

--- 0.js
+++
@@ -1,8 +0,0 @@
-let map = new Map()
-let nums = [1, 2, 3, 2, 4]
-
-for (let num of nums) {
-  map.set(num, (map.get(num) || 0) + 1)
-}
-
-console.log(map) // Map(4) { 1 => 1, 2 => 2, 3 => 1, 4 => 1 }
