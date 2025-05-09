34d71e2ee6a3fa87eb09b6a8cad4641e8dda5df9
Emanuele Favero <emafaux89@gmail.com>
2025-05-09 23:25:05 +0200

    feat: add find-indices-of-stable-mountains solution 2

--- array/find-indices-of-stable-mountains.js
+++ array/find-indices-of-stable-mountains.js
@@ -26,12 +26,31 @@ function stableMountains(height, threshold) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution checks the current mountain and pushes the next mountain index. Either solutions are valid.
+
+function stableMountains2(height, threshold) {
+  let result = []
+
+  for (let i = 0; i < height.length - 1; i++) {
+    if (height[i] > threshold) result.push(i + 1)
+  }
+
+  return result
+}
+
+// -------------------------
 // TESTS
 
-console.log(stableMountains([1, 2, 3, 4, 5], 2)) // [3,4]
+// 1
+console.log(stableMountains([1, 2, 3, 4, 5], 2)) // [3, 4]
 /*
 Explanation:
 
 Mountain 3 is stable because height[2] == 3 is greater than threshold == 2.
 Mountain 4 is stable because height[3] == 4 is greater than threshold == 2.
 */
+
+// 2
+console.log(stableMountains([1, 2, 3, 4, 5], 2)) // [3, 4]
