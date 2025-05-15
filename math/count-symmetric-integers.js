ff9617779e2de9eced87571a4755bd731ffecca6
Emanuele Favero <emafaux89@gmail.com>
2025-05-15 23:48:51 +0200

    feat: add math/count-symmetric-integers

---
+++ math/count-symmetric-integers.js
@@ -0,0 +1,36 @@
+// * Count Symmetric Integers
+
+/*
+You are given two positive integers low and high.
+
+An integer x consisting of 2 * n digits is symmetric if the sum of the first n digits of x is equal to the sum of the last n digits of x. Numbers with an odd number of digits are never symmetric.
+
+Return the number of symmetric integers in the range [low, high].
+*/
+
+function countSymmetricIntegers(low, high) {
+  let result = 0
+
+  if (high < 10) return result
+  if (low < 10 && high > 9) low = 10
+
+  for (let i = low; i <= high; i++) {
+    let array = i.toString().split('')
+    if (array.length % 2 !== 0) continue
+
+    let middle = Math.floor(array.length / 2)
+    let left = array.slice(0, middle)
+    let right = array.slice(middle)
+    let leftSum = left.reduce((a, acc) => Number(a) + Number(acc), 0)
+    let rightSum = right.reduce((a, acc) => Number(a) + Number(acc), 0)
+    if (leftSum === rightSum) result++
+  }
+
+  return result
+}
+
+// -------------------------
+// TESTS
+
+console.log(countSymmetricIntegers(1, 100)) // 9
+// Explanation: There are 9 symmetric integers between 1 and 100: 11, 22, 33, 44, 55, 66, 77, 88, and 99.
