699f48ecbb13179d58e99afc4ddceb716753a6e6
Emanuele Favero <emafaux89@gmail.com>
2025-05-22 23:59:46 +0200

    feat: add hash-table/merge-two-2d-arrays solution 2

--- hash-table/merge-two-2d-arrays.js
+++ hash-table/merge-two-2d-arrays.js
@@ -49,8 +49,44 @@ function mergeArrays(nums1, nums2) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This is a two pointer approach
+
+function mergeArrays2(nums1, nums2) {
+  let result = []
+  let i = 0
+  let j = 0
+
+  while (i < nums1.length && j < nums2.length) {
+    // if the id of nums1 is less than the id of nums2, add nums1 to the result
+    if (nums1[i][0] < nums2[j][0]) {
+      result.push(nums1[i])
+      i++
+      // if the id of nums2 is less than the id of nums1, add nums2 to the result
+    } else if (nums1[i][0] > nums2[j][0]) {
+      result.push(nums2[j])
+      j++
+      // if the ids are equal, sum the values
+    } else {
+      result.push([nums1[i][0], nums1[i][1] + nums2[j][1]])
+      i++
+      j++
+    }
+  }
+
+  // add the remaining elements of nums1 to the result
+  while (i < nums1.length) result.push(nums1[i++])
+  while (j < nums2.length) result.push(nums2[j++])
+  // ? increment i and j after pushing the element to the result
+
+  return result
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(
   mergeArrays(
     [
@@ -72,3 +108,19 @@ Explanation: The resulting array contains the following:
 - id = 3, the value of this id is 2.
 - id = 4, the value of this id is 5 + 1 = 6.
 */
+
+// 2
+console.log(
+  mergeArrays2(
+    [
+      [1, 2],
+      [2, 3],
+      [4, 5],
+    ],
+    [
+      [1, 4],
+      [3, 2],
+      [4, 1],
+    ]
+  )
+) // [[1,6],[2,3],[3,2],[4,6]]
