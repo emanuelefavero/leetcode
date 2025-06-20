a0dfa25c3c2ba0ffcec535f6b145a91f99e989b5
Emanuele Favero <emafaux89@gmail.com>
2025-06-20 21:58:29 +0200

    feat: add math/smallest-index-with-digit-sum

---
+++ math/smallest-index-with-digit-sum.js
@@ -0,0 +1,32 @@
+// * Smallest Index With Digit Sum Equal to Index
+
+// You are given an integer array nums.
+
+// Return the smallest index i such that the sum of the digits of nums[i] is equal to i.
+
+// If no such index exists, return -1.
+
+function smallestIndex(nums) {
+  let result = []
+
+  for (let i = 0; i < nums.length; i++) {
+    let digits = nums[i].toString().split('')
+    let sum = digits.reduce((acc, digit) => acc + Number(digit), 0)
+
+    if (sum === i) result.push(sum)
+  }
+
+  if (!result.length) return -1
+
+  result.sort((a, b) => a - b)
+
+  return result[0]
+}
+
+// -------------------------
+// TESTS
+
+console.log(smallestIndex([1, 3, 2])) // 2
+// Explanation: For nums[2] = 2, the sum of digits is 2, which is equal to index i = 2. Thus, the output is 2.
+console.log(smallestIndex([1, 10, 11])) // 1
+console.log(smallestIndex([1, 2, 3])) // -1
