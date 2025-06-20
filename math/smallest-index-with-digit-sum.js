84b2620407719d9ad492227070cea6d14d7ac6d3
Emanuele Favero <emafaux89@gmail.com>
2025-06-20 22:05:16 +0200

    feat: math/smallest-index-with-digit-sum solution 2

--- math/smallest-index-with-digit-sum.js
+++ math/smallest-index-with-digit-sum.js
@@ -18,6 +18,26 @@ function smallestIndex(nums) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+// TIP: This solution uses the remainder operator to get the last digit of the number and the floor division to remove the last digit
+
+function smallestIndex2(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0
+
+    while (nums[i] > 0) {
+      sum += nums[i] % 10 // Get the last digit
+      nums[i] = Math.floor(nums[i] / 10) // Remove the last digit
+    }
+
+    if (sum === i) return i
+  }
+
+  return -1
+}
+
+// -------------------------
 // TESTS
 
 // 1
@@ -25,3 +45,8 @@ console.log(smallestIndex([1, 3, 2])) // 2
 // Explanation: For nums[2] = 2, the sum of digits is 2, which is equal to index i = 2. Thus, the output is 2.
 console.log(smallestIndex([1, 10, 11])) // 1
 console.log(smallestIndex([1, 2, 3])) // -1
+
+// 2
+console.log(smallestIndex2([1, 3, 2])) // 2
+console.log(smallestIndex2([1, 10, 11])) // 1
+console.log(smallestIndex2([1, 2, 3])) // -1
