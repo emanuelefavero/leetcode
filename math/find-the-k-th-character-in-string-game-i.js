957fc874de68a0c5e9fb0f6bda947f9224e72e68
Emanuele Favero <emafaux89@gmail.com>
2025-07-17 20:13:16 +0200

    feat: add find-the-k-th-character-in-string-game-i

---
+++ math/find-the-k-th-character-in-string-game-i.js
@@ -0,0 +1,46 @@
+// * Find the K-th Character in String Game I
+
+/*
+Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+
+You are given a positive integer k.
+
+Now Bob will ask Alice to perform the following operation forever:
+
+- Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+
+For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+
+Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+*/
+
+function kthCharacter(k) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let word = 'a'
+
+  while (word.length < k) {
+    let newWord = ''
+    for (let i = 0; i < word.length; i++) {
+      let charIndex = alphabet.indexOf(word[i])
+
+      newWord += alphabet[(charIndex + 1) % alphabet.length]
+    }
+
+    word += newWord
+  }
+
+  return word[k - 1]
+}
+
+// -------------------------
+// TESTS
+
+console.log(kthCharacter(5)) // "b"
+/* Explanation:
+Initially, word = "a". We need to do the operation three times:
+
+Generated string is "b", word becomes "ab".
+Generated string is "bc", word becomes "abbc".
+Generated string is "bccd", word becomes "abbcbccd".
+*/
+console.log(kthCharacter(10)) // "c"
