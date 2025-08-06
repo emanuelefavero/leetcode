96ac9d9d5041f5098978040ede5e102ed266849a
Emanuele Favero <emafaux89@gmail.com>
2025-08-06 02:56:50 +0200

    feat: add string/generate-binary-strings solution 2

--- string/generate-binary-strings.js
+++ string/generate-binary-strings.js
@@ -30,7 +30,42 @@ function validStrings(n) {
 }
 
 // -------------------------
+// SOLUTION 2
+
+function validStrings2(n) {
+  const result = []
+
+  const generate = (s) => {
+    // base case: if the string length is n, add it to the result
+    if (s.length === n) {
+      result.push(s)
+      return
+    }
+
+    // If the last character in the string is '0', we can only add '1' to avoid adjacent zeros
+    if (s[s.length - 1] === '0') {
+      generate(s + '1')
+    }
+
+    // If the last character is '1' or if the string is empty, we can add both '0' and '1'
+    else {
+      generate(s + '0')
+      generate(s + '1')
+    }
+  }
+
+  generate('')
+
+  return result
+}
+
+// -------------------------
 // TESTS
 
+// 1
 console.log(validStrings(3)) // ["010","011","101","110","111"]
 console.log(validStrings(1)) // ["0","1"]
+
+// 2
+console.log(validStrings2(3)) // ["010","011","101","110","111"]
+console.log(validStrings2(1)) // ["0","1"]
