ac45431a2fb28ec1549a8c313c86beeb7b1f8c1f
Emanuele Favero <emafaux89@gmail.com>
2025-08-06 02:43:02 +0200

    feat: add string/generate-binary-strings

---
+++ string/generate-binary-strings.js
@@ -0,0 +1,28 @@
+// * Generate Binary Strings Without Adjacent Zeros
+
+/*
+You are given a positive integer n.
+
+A binary string x is valid if all substrings of x of length 2 contain at least one "1".
+
+Return all valid strings with length n, in any order.
+*/
+
+function validStrings(n) {
+  if (n === 1) return ['0', '1']
+
+  let result = []
+
+  for (let s of validStrings(n - 1)) {
+    result.push(`1${s}`)
+    if (s[0] === '1') result.push(`0${s}`)
+  }
+
+  return result
+}
+
+// -------------------------
+// TESTS
+
+console.log(validStrings(3)) // ["010","011","101","110","111"]
+console.log(validStrings(1)) // ["0","1"]
