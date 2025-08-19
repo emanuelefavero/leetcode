7a56d6a9ba8903e417354aedd06d8f10d46f46fa
Emanuele Favero <emafaux89@gmail.com>
2025-08-19 19:44:26 +0200

    feat: add array/divide-array-into-equal-pairs solution 2

--- array/divide-array-into-equal-pairs.js
+++ array/divide-array-into-equal-pairs.js
@@ -34,13 +34,44 @@ function divideArray(nums) {
   let n = nums.length / 2
   return sum % n === 0
 }
+// -------------------------
+// SOLUTION 2
+
+// TIP: If the frequency of all numbers is even, we can form pairs no matter the arrangement of the numbers, so we can simplify the solution.
+
+function divideArray2(nums) {
+  let map = new Map()
+
+  // Populate the map with counts of each number
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1)
+  }
+
+  // Check if any num occurrence is odd, if so, return false
+  for (const [_, value] of map) {
+    if (value % 2 !== 0) return false
+  }
+
+  // Get the sum of num occurrences
+  let sum = 0
+  for (const [_, value] of map) {
+    sum += value
+  }
+
+  return true
+}
 
 // -------------------------
 // TESTS
 
+// 1
 console.log(divideArray([3, 2, 3, 2, 2, 2])) // true
 /* Explanation:
 There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
 If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
 */
 console.log(divideArray([1, 2, 3, 4])) // false
+
+// 2
+console.log(divideArray2([3, 2, 3, 2, 2, 2])) // true
+console.log(divideArray2([1, 2, 3, 4])) // false
