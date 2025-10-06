a93412e9a659a55211d8e60f6c1b3c5ac296d507
Emanuele Favero <emafaux89@gmail.com>
2025-10-06 14:09:18 +0200

    feat: add array/earliest-time-to-finish-one-task

---
+++ array/earliest-time-to-finish-one-task.js
@@ -0,0 +1,43 @@
+// * Earliest Time to Finish One Task
+
+/*
+You are given a 2D integer array tasks where tasks[i] = [si, ti].
+
+Each [si, ti] in tasks represents a task with start time si that takes ti units of time to finish.
+
+Return the earliest time at which at least one task is finished.
+*/
+
+function earliestTime(tasks) {
+  let min = Infinity
+
+  for (const task of tasks) {
+    let sum = task[0] + task[1]
+    min = Math.min(min, sum)
+  }
+
+  return min
+}
+
+// -------------------------
+// TESTS
+
+console.log(
+  earliestTime([
+    [1, 6],
+    [2, 3],
+  ])
+) // 5
+/* Explanation:
+The first task starts at time t = 1 and finishes at time 1 + 6 = 7. The second task finishes at time 2 + 3 = 5. You can finish one task at time 5.
+*/
+console.log(
+  earliestTime([
+    [100, 100],
+    [100, 100],
+    [100, 100],
+  ])
+) // 200
+/* Explanation:
+All three tasks finish at time 100 + 100 = 200.
+*/
