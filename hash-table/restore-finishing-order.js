cc3edc5efaf83781ed8ca1c7be161d7b3809229f
Emanuele Favero <emafaux89@gmail.com>
2025-10-10 21:42:37 +0200

    feat: add hash-table/restore-finishing-order solution 2

--- hash-table/restore-finishing-order.js
+++ hash-table/restore-finishing-order.js
@@ -30,9 +30,35 @@ function recoverOrder(order, friends) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: We can also solve the problem with nested loops, but it would be less efficient (although in this case is fine since the constraints are small)
+
+// O(n^2) time | O(1) space
+function recoverOrder2(order, friends) {
+  const result = []
+
+  for (const id of order) {
+    for (const friend of friends) {
+      if (id === friend) {
+        result.push(id)
+        break
+      }
+    }
+  }
+
+  return result
+}
+
+// -----------------------------
 // TESTS
 
+// 1
 console.log(recoverOrder([3, 1, 2, 5, 4], [1, 3, 4])) // [3, 1, 4]
 // Explanation: The finishing order is [3, 1, 2, 5, 4]. Therefore, the finishing order of your friends is [3, 1, 4].
 console.log(recoverOrder([1, 4, 5, 3, 2], [2, 5])) // [5, 2]
 // Explanation: The finishing order is [1, 4, 5, 3, 2]. Therefore, the finishing order of your friends is [5, 2].
+
+// 2
+console.log(recoverOrder2([3, 1, 2, 5, 4], [1, 3, 4])) // [3, 1, 4]
+console.log(recoverOrder2([1, 4, 5, 3, 2], [2, 5])) // [5, 2]
