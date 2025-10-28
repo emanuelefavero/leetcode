391250f1593e6ba6e61e73ce1101045d9ceffdea
Emanuele Favero <emafaux89@gmail.com>
2025-10-28 19:59:01 +0100

    feat: add bit-manipulation/bitwise-or-of-even-numbers-in-an-array

---
+++ bit-manipulation/bitwise-or-of-even-numbers-in-an-array.js
@@ -0,0 +1,24 @@
+// * Bitwise OR of Even Numbers in an Array
+
+// You are given an integer array nums.
+
+// Return the bitwise OR of all even numbers in the array.
+
+// If there are no even numbers in nums, return 0.
+
+function evenNumberBitwiseORs(nums) {
+  let result = 0
+
+  for (const num of nums) {
+    if (num % 2 === 0) result |= num
+  }
+
+  return result
+}
+
+// ----------------------
+// TESTS
+
+console.log(evenNumberBitwiseORs([1, 2, 3, 4, 5, 6])) // 6
+// Explanation: The even numbers are 2, 4, and 6. Their bitwise OR equals 6.
+console.log(evenNumberBitwiseORs([7, 9, 11])) // 0
