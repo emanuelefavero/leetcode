5e3fc8a773f25e2abd851b2f106973f48beaed6a
Emanuele Favero <emafaux89@gmail.com>
2025-11-15 00:11:02 +0100

    fix: k is not defined in array/move-zeroes

--- array/move-zeroes.js
+++ array/move-zeroes.js
@@ -8,7 +8,7 @@
 
 // O(n) time | O(1) space - in-place
 function moveZeroes(nums) {
-  numberOfZeroes = 0
+  let numberOfZeroes = 0
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 0) {
