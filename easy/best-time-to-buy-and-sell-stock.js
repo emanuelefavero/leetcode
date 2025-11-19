63cc56370c52944cfa6b9540f53dde308a792b37
Emanuele Favero <emafaux89@gmail.com>
2025-11-19 20:22:49 +0100

    feat: add easy/best-time-to-buy-and-sell-stock solution 2

--- easy/best-time-to-buy-and-sell-stock.js
+++ easy/best-time-to-buy-and-sell-stock.js
@@ -26,12 +26,29 @@ function maxProfit(prices) {
   return profit
 }
 
+function maxProfit2(prices) {
+  let minPrice = prices[0]
+  let profit = 0
+
+  for (let i = 1; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i])
+    profit = Math.max(profit, prices[i] - minPrice)
+  }
+
+  return profit
+}
+
 // -----------------------------
 // TESTS
 
+// 1
 console.log(maxProfit([7, 1, 5, 3, 6, 4])) // 5
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
 console.log(maxProfit([7, 6, 4, 3, 1])) // 0
 // Explanation: In this case, no transactions are done and the max profit = 0.
+
+// 2
+console.log(maxProfit([7, 1, 5, 3, 6, 4])) // 5
+console.log(maxProfit([7, 6, 4, 3, 1])) // 0
