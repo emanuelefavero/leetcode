8d6640b98d671f043058310fd9f7e964837e93b1
Emanuele Favero <emafaux89@gmail.com>
2025-12-17 22:36:43 +0100

    docs: add hash-table/maximum-substrings-with-distinct-start jsDoc

--- hash-table/maximum-substrings-with-distinct-start.js
+++ hash-table/maximum-substrings-with-distinct-start.js
@@ -6,6 +6,12 @@ You are given a string s consisting of lowercase English letters.
 Return an integer denoting the maximum number of substrings you can split s into such that each substring starts with a distinct character (i.e., no two substrings start with the same character).
 */
 
+/**
+ * @param {string} s
+ * @returns {number}
+ */
+
+// O(n) time | O(n) space
 function maxDistinct(s) {
   return new Set(s).size
 }
