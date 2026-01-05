d48f522c24ed94ba1b6c90429899ae2627741ada
Emanuele Favero <emafaux89@gmail.com>
2026-01-06 00:08:47 +0100

    feat: add hash-table/encode-and-decode-tiny-url

---
+++ hash-table/encode-and-decode-tiny-url.js
@@ -0,0 +1,38 @@
+// * Encode and Decode TinyURL
+
+/*
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
+
+There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+Implement the Solution class:
+
+- Solution() Initializes the object of the system.
+- String encode(String longUrl) Returns a tiny URL for the given longUrl.
+- String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+*/
+
+const map = new Map()
+let count = 0
+const baseUrl = 'http://tinyurl.com/'
+
+function encode(longUrl) {
+  count += 1
+  map.set(count, longUrl)
+  return baseUrl + count
+}
+
+function decode(shortUrl) {
+  const id = parseInt(shortUrl.replace(baseUrl, ''))
+  return map.get(id)
+}
+
+// -----------------------------
+// TESTS
+
+const url = 'https://leetcode.com/problems/design-tinyurl'
+const shortUrl = encode(url)
+console.log(decode(shortUrl)) // https://leetcode.com/problems/design-tinyurl
+console.log(shortUrl) // http://tinyurl.com/1
