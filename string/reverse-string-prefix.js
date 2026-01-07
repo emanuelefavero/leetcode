36ffaf7d9bcc8b1c06d8a19b98544f016f3a82b3
Emanuele Favero <emafaux89@gmail.com>
2026-01-07 15:39:50 +0100

    docs: add string/reverse-string-prefix jsDoc

--- string/reverse-string-prefix.js
+++ string/reverse-string-prefix.js
@@ -4,6 +4,13 @@
 
 // Reverse the first k characters of s and return the resulting string.
 
+/**
+ * @param {string} s
+ * @param {number} k
+ * @returns {string}
+ */
+
+// O(n) time | O(n) space
 function reversePrefix(s, k) {
   let reversed = ''
 
@@ -18,5 +25,6 @@ function reversePrefix(s, k) {
 // TESTS
 
 console.log(reversePrefix('abcd', 2)) // bacd
+// Explanation:​​​​​​​ The first k = 2 characters "ab" are reversed to "ba". The final resulting string is "bacd".
 console.log(reversePrefix('xyz', 3)) // zyx
 console.log(reversePrefix('hey', 1)) // hey
