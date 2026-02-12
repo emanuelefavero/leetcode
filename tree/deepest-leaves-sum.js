175a975b6e4a09f541ba70c259c06abc223190d9
Emanuele Favero <emafaux89@gmail.com>
2026-02-12 23:16:14 +0100

    feat: add tree/deepest-leaves-sum solution 2

--- tree/deepest-leaves-sum.js
+++ tree/deepest-leaves-sum.js
@@ -41,6 +41,37 @@ function deepestLeavesSum(root) {
 }
 
 // -----------------------------
+// SOLUTION 2
+
+// TIP: In theory this solution should be more efficient, as it uses a breadth-first search (BFS) approach, which allows us to find the deepest leaves in a single pass through the tree.
+
+function deepestLeavesSum2(root) {
+  if (!root) return 0
+
+  const queue = [root]
+  let sum = 0
+
+  while (queue.length) {
+    // Reset sum for each level, so that at the end of the loop, sum will contain the sum of the deepest leaves
+    sum = 0
+
+    const length = queue.length // number of nodes at the current level
+
+    // Iterate through the current level
+    for (let i = 0; i < length; i++) {
+      const current = queue.shift() // remove first element
+
+      sum += current.val
+
+      if (current.left) queue.push(current.left)
+      if (current.right) queue.push(current.right)
+    }
+  }
+
+  return sum
+}
+
+// ------------------------------
 // TESTS
 
 const root = new TreeNode(1)
@@ -53,3 +84,4 @@ root.left.left.left = new TreeNode(7)
 root.right.right.right = new TreeNode(8)
 
 console.log(deepestLeavesSum(root)) // 15 (7 + 8)
+console.log(deepestLeavesSum2(root)) // 15 (7 + 8)
